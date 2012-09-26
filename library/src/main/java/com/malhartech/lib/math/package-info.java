3c9e508762e6e0d7de096e9b5dc15bdae4a3ea39
Amol Kekre <amol@datatorrent.com>
2012-09-25 21:16:14 -0700

    Added more info

--- library/src/main/java/com/malhartech/lib/math/package-info.java
+++ library/src/main/java/com/malhartech/lib/math/package-info.java
@@ -11,6 +11,8 @@
  * <b>{@link com.malhartech.lib.math.ArithmeticMargin}</b>: For every window computes margins of sums of values of a key in two streams<br>
  * <b>{@link com.malhartech.lib.math.ArithmeticQuotient}</b>: For every window computes quotient of sum of values of a key in two streams<br>
  * <b>{@link com.malhartech.lib.math.ArithmeticSum}</b>: For every window adds all the values of a key in a stream. For string values users can choose to appends with a delimiter<br>
+ * <b>{@link com.malhartech.lib.math.SearchInvertIndex}</b>: Takes in a stream via input port "data". Inverts the index and sends out the tuple on output port "index"<p>
+ * <b>{@link com.malhartech.lib.math.UniqueCounter}</b>: Counts payloads and send unique count for each on end of window<br>
  * <br>
  *
  */
