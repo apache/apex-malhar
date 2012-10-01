7165012d29b794853dabd6124795f265bdd8ca74
Amol Kekre <amol@datatorrent.com>
2012-10-01 10:28:26 -0700

    Added Count Port

--- library/src/main/java/com/malhartech/lib/math/ArithmeticSum.java
+++ library/src/main/java/com/malhartech/lib/math/ArithmeticSum.java
@@ -8,6 +8,8 @@ import com.malhartech.annotation.ModuleAnnotation;
 import com.malhartech.annotation.PortAnnotation;
 import com.malhartech.dag.AbstractModule;
 import com.malhartech.dag.ModuleConfiguration;
+import com.malhartech.dag.Sink;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
@@ -28,14 +30,33 @@ import org.slf4j.LoggerFactory;
 @ModuleAnnotation(
         ports = {
   @PortAnnotation(name = ArithmeticSum.IPORT_DATA, type = PortAnnotation.PortType.INPUT),
-  @PortAnnotation(name = ArithmeticSum.OPORT_SUM, type = PortAnnotation.PortType.OUTPUT)
+  @PortAnnotation(name = ArithmeticSum.OPORT_SUM, type = PortAnnotation.PortType.OUTPUT),
+  @PortAnnotation(name = ArithmeticSum.OPORT_COUNT, type = PortAnnotation.PortType.OUTPUT)
 })
 public class ArithmeticSum extends AbstractModule
 {
   public static final String IPORT_DATA = "data";
   public static final String OPORT_SUM = "sum";
+  public static final String OPORT_COUNT = "count";
   private static Logger LOG = LoggerFactory.getLogger(ArithmeticSum.class);
-  HashMap<String, Number> sum = new HashMap<String, Number>();
+  HashMap<String, Object> sum = new HashMap<String, Object>();
+
+  boolean count_connected = false;
+  boolean sum_connected = true;
+
+  @Override
+  public void connected(String id, Sink dagpart)
+  {
+    if (id.equals(OPORT_COUNT)) {
+      count_connected = (dagpart != null);
+    }
+    else if (id.equals(OPORT_SUM)) {
+      sum_connected = (dagpart != null);
+    }
+  }
+
+
+
 
   /**
    * Process each tuple
@@ -46,14 +67,35 @@ public class ArithmeticSum extends AbstractModule
   public void process(Object payload)
   {
       for (Map.Entry<String, Number> e: ((HashMap<String, Number>)payload).entrySet()) {
-        Number val = sum.get(e.getKey());
-        if (val != null) {
-          val = new Double(val.doubleValue() + e.getValue().doubleValue());
+        Object hval = sum.get(e.getKey());
+        if (sum_connected && !count_connected) {
+          if (hval != null) {
+            hval = new Double(((Number) hval).doubleValue() + e.getValue().doubleValue());
+          }
+          else {
+            hval = new Double(e.getValue().doubleValue());
+          }
         }
-        else {
-          val = new Double(e.getValue().doubleValue());
+        else if (sum_connected && count_connected) {
+          if (hval != null) {
+            ((ArrayList) hval).set(0, new Double(((Number) ((ArrayList) hval).get(0)).doubleValue() + e.getValue().doubleValue()));
+            ((ArrayList) hval).set(1, (Integer) ((ArrayList) hval).get(0) + 1);
+          }
+          else {
+            hval = new ArrayList();
+            ((ArrayList) hval).add(new Double(e.getValue().doubleValue()));
+            ((ArrayList) hval).add(new Integer(1));
+          }
         }
-        sum.put(e.getKey(), val);
+        else if (count_connected) {
+          if (hval != null) {
+            hval = ((Integer) hval) + 1;
+          }
+          else {
+            hval = new Integer(1);
+          }
+        } // If neither of the above are true, emit error
+        sum.put(e.getKey(), hval);
       }
   }
 
@@ -68,15 +110,37 @@ public class ArithmeticSum extends AbstractModule
   @Override
   public void endWindow()
   {
-    HashMap<String, Number> tuples = new HashMap<String, Number>();
-    for (Map.Entry<String, Number> e: sum.entrySet()) {
-      tuples.put(e.getKey(), e.getValue());
-    }
 
     // Should allow users to send each key as a separate tuple to load balance
     // This is an aggregate node, so load balancing would most likely not be needed
-    if (!tuples.isEmpty()) {
-      emit(OPORT_SUM, tuples);
+
+    HashMap<String, Object> stuples = null;
+    if (sum_connected) {
+      stuples = new HashMap<String, Object>();
+    }
+    HashMap<String, Object> ctuples = null;
+    if (count_connected) {
+      ctuples = new HashMap<String, Object>();
+    }
+
+    for (Map.Entry<String, Object> e: sum.entrySet()) {
+      if (sum_connected && !count_connected) {
+        stuples.put(e.getKey(), e.getValue());
+      }
+      else if (sum_connected && count_connected) {
+        stuples.put(e.getKey(), ((ArrayList) e.getValue()).get(0));
+        ctuples.put(e.getKey(), ((ArrayList) e.getValue()).get(1));
+      }
+      else if (count_connected) {
+        ctuples.put(e.getKey(), e.getValue());
+      }
+    }
+
+    if ((stuples != null) && !stuples.isEmpty()) {
+      emit(OPORT_SUM, stuples);
+    }
+    if ((ctuples != null) && !ctuples.isEmpty()) {
+      emit(OPORT_COUNT, ctuples);
     }
     sum.clear();
   }
