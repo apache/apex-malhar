ac01d0a20222a76acab9794eb172753bc6525fe3
Amol Kekre <amol@datatorrent.com>
2012-10-01 21:38:59 -0700

    added more nodes

--- demos/src/main/java/com/malhartech/demos/mobile/Application.java
+++ demos/src/main/java/com/malhartech/demos/mobile/Application.java
@@ -12,7 +12,9 @@ import com.malhartech.dag.DAG.Operator;
 import com.malhartech.lib.io.ConsoleOutputModule;
 import com.malhartech.lib.io.HttpInputModule;
 import com.malhartech.lib.io.HttpOutputModule;
+import com.malhartech.lib.testbench.LoadRandomGenerator;
 import com.malhartech.lib.testbench.LoadSeedGenerator;
+import com.malhartech.lib.testbench.SeedClassifier;
 
 
 /**
@@ -38,15 +40,35 @@ public class Application implements ApplicationFactory {
             .setProperty(ConsoleOutputModule.P_STRING_FORMAT, operatorName + ": %s");
   }
 
-  public Operator getSeedGeneratorOperator(String name, DAG b) {
+  public Operator getSeedGenerator(String name, DAG b) {
     Operator oper = b.addOperator(name, LoadSeedGenerator.class);
     oper.setProperty(LoadSeedGenerator.KEY_STRING_SCHEMA, "false");
     oper.setProperty(LoadSeedGenerator.KEY_EMITKEY, "false");
     oper.setProperty(LoadSeedGenerator.KEY_KEYS, "x,y");
+    oper.setProperty(LoadSeedGenerator.KEY_SEED_START, "8000000");
+    oper.setProperty(LoadSeedGenerator.KEY_SEED_END    , "9999999");
 
     return oper;
   }
 
+  public Operator getRandomGenerator(String name, DAG b) {
+    Operator oper = b.addOperator(name, LoadRandomGenerator.class);
+    oper.setProperty(LoadRandomGenerator.KEY_MAX_VALUE, "99");
+    oper.setProperty(LoadRandomGenerator.KEY_MIN_VALUE, "0");
+    oper.setProperty(LoadRandomGenerator.KEY_TUPLES_BLAST, "1000");
+    oper.setProperty(LoadRandomGenerator.KEY_STRING_SCHEMA, "false");
+    return oper;
+  }
+
+  public Operator getSeedClassifier(String name, DAG b) {
+    Operator oper = b.addOperator(name, SeedClassifier.class);
+    oper.setProperty(SeedClassifier.KEY_SEED_END    , "9999999");
+    oper.setProperty(SeedClassifier.KEY_SEED_START, "8000000");
+    oper.setProperty(SeedClassifier.KEY_IN_DATA1_CLASSIFIER, "x");
+    oper.setProperty(SeedClassifier.KEY_IN_DATA2_CLASSIFIER, "y");
+    oper.setProperty(SeedClassifier.KEY_STRING_SCHEMA, "false");
+    return oper;
+  }
 
   @Override
   public DAG getApplication(Configuration conf) {
@@ -56,8 +78,11 @@ public class Application implements ApplicationFactory {
     Operator phoneLocationQuery = dag.addOperator("phoneLocationQuery", HttpInputModule.class);
     phoneLocationQuery.setProperty(HttpInputModule.P_RESOURCE_URL, "http://localhost:8080/channel/mobile/phoneLocationQuery");
 
+    Operator seedGen = getSeedGenerator("seedGen", dag);
+    Operator randomXGen = getRandomGenerator("xgen", dag);
+    Operator randomYGen = getRandomGenerator("ygen", dag);
+    Operator seedClassify = getSeedClassifier("seedclassify", dag);
 
-    Operator seedGen = getSeedGeneratorOperator("seedGen", dag);
 
     //dag.addStream("views", viewGen.getOutput(LoadGenerator.OPORT_DATA), adviews.getInput(LoadClassifier.IPORT_IN_DATA)).setInline(true);
 
