9747a36da18f80fc32852e46a4fd8de946aef83c
Amol Kekre <amol@datatorrent.com>
2012-10-04 11:45:40 -0700

    beautified it

--- demos/src/main/java/com/malhartech/demos/twitter/Application.java
+++ demos/src/main/java/com/malhartech/demos/twitter/Application.java
@@ -43,8 +43,7 @@ public class Application implements ApplicationFactory
             .getInput(ConsoleOutputModule.INPUT);
   }
 
-  @Override
-  public DAG getApplication(Configuration conf)
+  public Operator getTwitterFeed(String name, DAG b, int multby)
   {
     final String propertyBase = "twitter4j";
     Properties properties = new Properties();
@@ -55,64 +54,57 @@ public class Application implements ApplicationFactory
       logger.error("Could not read the much needed credentials file because of {}", e.getLocalizedMessage());
       return null;
     }
-
-    DAG b = new DAG(conf);
     /*
      * Setup the operator to get the data from twitter sample stream injected into the system.
      */
-    Operator twitterFeed = b.addOperator("TweetSampler", TwitterSampleInput.class);
-    twitterFeed.setProperty("FeedMultiplier", String.valueOf(100));
+    Operator oper = b.addOperator(name, TwitterSampleInput.class);
+    oper.setProperty("FeedMultiplier", String.valueOf(multby));
     for (Entry<Object, Object> property: properties.entrySet()) {
       String key = propertyBase.concat(".").concat(property.getKey().toString());
 //      logger.info("remove this: setting property {} = {}", key, property.getValue().toString());
-      twitterFeed.setProperty(key, property.getValue().toString());
+      oper.setProperty(key, property.getValue().toString());
     }
+    return oper;
+  }
 
-    /*
-     * Setup the operator to get the URLs extracted from the twitter statuses.
-     */
-    Operator urlExtractor = b.addOperator("URLExtractor", TwitterStatusURLExtractor.class);
+  public Operator getTwitterUrlExtractor(String name, DAG b)
+  {
+    Operator oper = b.addOperator(name, TwitterStatusURLExtractor.class);
+    return oper;
+  }
 
-    /*
-     * Feed the statuses from feed into the input of the url extractor.
-     */
-    b.addStream("tweetStream")
-            .setSource(twitterFeed.getOutput(TwitterSampleInput.OPORT_STATUS))
-            .addSink(urlExtractor.getInput(TwitterStatusURLExtractor.INPUT))
-            .setInline(true);
+  public Operator getUniqueCounter(String name, DAG b) {
+    Operator oper = b.addOperator(name, UniqueCounter.class);
+    return oper;
+  }
 
-    /*
-     * Let's setup a node to count the unique urls within a window.
-     */
-    Operator uniqueCounter = b.addOperator("UniqueURLCounter", UniqueCounter.class);
+  public Operator getTopCounter(String name, DAG b, int count) {
+    Operator oper = b.addOperator(name, WindowedTopCounter.class).setProperty("topCount", String.valueOf(count));
+    return oper;
+  }
 
-    /*
-     * Start counting the urls coming out of URL extractor
-     */
-    b.addStream("twittedURLs")
-            .setSource(urlExtractor.getOutput(TwitterStatusURLExtractor.OUTPUT))
-            .addSink(uniqueCounter.getInput(UniqueCounter.INPUT))
-            .setInline(inline);
+  @Override
+  public DAG getApplication(Configuration conf)
+  {
+    DAG b = new DAG(conf);
 
-    /*
-     * Get the aggregated url counts and count them over the timeframe.
-     */
-    Operator topCounts = b.addOperator("TopCounter", WindowedTopCounter.class);
-    topCounts.setProperty("topCount", String.valueOf(10));
+    Operator twitterFeed = getTwitterFeed("TweetSampler", b, 100); // Setup the operator to get the data from twitter sample stream injected into the system.
+    Operator urlExtractor = getTwitterUrlExtractor("URLExtractor", b); //  Setup the operator to get the URLs extracted from the twitter statuses
+    Operator uniqueCounter = getUniqueCounter("UniqueURLCounter", b); // Setup a node to count the unique urls within a window.
+    Operator topCounts = getTopCounter("TopCounter", b, 10);  // Get the aggregated url counts and count them over the timeframe
 
-    b.addStream("uniqueURLCounts")
-            .setSource(uniqueCounter.getOutput(UniqueCounter.OUTPUT))
-            .addSink(topCounts.getInput(WindowedTopCounter.INPUT))
-            .setInline(inline);
-
-    b.addStream("TopURLs")
-            .setSource(topCounts.getOutput(WindowedTopCounter.OUTPUT))
-            .addSink(consoleOutput(b, "topURLs"))
-            .setInline(inline);
-
-    // these settings only affect distributed mode
-    b.getConf().setInt(DAG.STRAM_CONTAINER_MEMORY_MB, 512);
-    b.getConf().setInt(DAG.STRAM_MASTER_MEMORY_MB, 512);
+    // Feed the statuses from feed into the input of the url extractor.
+    b.addStream("tweetStream").setSource(twitterFeed.getOutput(TwitterSampleInput.OPORT_STATUS))
+            .addSink(urlExtractor.getInput(TwitterStatusURLExtractor.INPUT)).setInline(true);
+    //  Start counting the urls coming out of URL extractor
+    b.addStream("twittedURLs").setSource(urlExtractor.getOutput(TwitterStatusURLExtractor.OUTPUT))
+            .addSink(uniqueCounter.getInput(UniqueCounter.INPUT)).setInline(inline);
+    // Count unique urls
+    b.addStream("uniqueURLCounts").setSource(uniqueCounter.getOutput(UniqueCounter.OUTPUT))
+            .addSink(topCounts.getInput(WindowedTopCounter.INPUT)).setInline(inline);
+    // Count top 10
+    b.addStream("TopURLs").setSource(topCounts.getOutput(WindowedTopCounter.OUTPUT))
+            .addSink(consoleOutput(b, "topURLs")).setInline(inline);
 
     return b;
   }
