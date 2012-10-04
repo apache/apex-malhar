2d5af8e5644bb9969a12ce18b58adbf9cbf407fa
Thomas Weise <thomas@datatorrent.com>
2012-10-04 10:35:22 -0700

    make range and server configurable

--- demos/src/main/java/com/malhartech/demos/mobile/Application.java
+++ demos/src/main/java/com/malhartech/demos/mobile/Application.java
@@ -4,20 +4,22 @@
  */
 package com.malhartech.demos.mobile;
 
+import org.apache.hadoop.conf.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.malhartech.dag.ApplicationFactory;
 import com.malhartech.dag.DAG;
 import com.malhartech.dag.DAG.Operator;
 import com.malhartech.lib.algo.InvertIndexMap;
 import com.malhartech.lib.algo.TupleQueue;
 import com.malhartech.lib.io.ConsoleOutputModule;
+import com.malhartech.lib.io.HttpInputModule;
 import com.malhartech.lib.io.HttpOutputModule;
 import com.malhartech.lib.testbench.LoadIncrementer;
 import com.malhartech.lib.testbench.LoadRandomGenerator;
 import com.malhartech.lib.testbench.LoadSeedGenerator;
 import com.malhartech.lib.testbench.SeedClassifier;
-import org.apache.hadoop.conf.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 /**
@@ -28,17 +30,19 @@ public class Application implements ApplicationFactory {
 
   private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
-  public static final String P_generatorVTuplesBlast = com.malhartech.demos.mobile.Application.class.getName() + ".generatorVTuplesBlast";
-  public static final String P_generatorMaxWindowsCount = com.malhartech.demos.mobile.Application.class.getName() + ".generatorMaxWindowsCount";
-  public static final String P_allInline = com.malhartech.demos.mobile.Application.class.getName() + ".allInline";
+  public static final String P_phoneRangeStart = com.malhartech.demos.mobile.Application.class.getName() + ".phoneRangeStart";
+  public static final String P_phoneRangeEnd = com.malhartech.demos.mobile.Application.class.getName() + ".phoneRangeEnd";
 
   // adjust these depending on execution mode (junit, cli-local, cluster)
   private int generatorMaxWindowsCount = 100;
-  private boolean allInline = true;
-
+  private String ajaxServerAddr = null;
+  private int phoneRangeStart = 9000000;
+  private int phoneRangeEnd = 9999999;
 
   public void setUnitTestMode() {
      generatorMaxWindowsCount = 20;
+     this.phoneRangeStart = 9000000;
+     this.phoneRangeEnd = 9000010;
   }
 
   public void setLocalMode() {
@@ -47,8 +51,10 @@ public class Application implements ApplicationFactory {
 
    private void configure(Configuration conf) {
 
+     this.ajaxServerAddr = System.getenv("MALHAR_AJAXSERVER_ADDRESS");
+     LOG.debug(String.format("\n******************* Server address was %s", this.ajaxServerAddr));
+
     if (LAUNCHMODE_YARN.equals(conf.get(DAG.STRAM_LAUNCH_MODE))) {
-      setLocalMode();
       // settings only affect distributed mode
       conf.setIfUnset(DAG.STRAM_CONTAINER_MEMORY_MB, "2048");
       conf.setIfUnset(DAG.STRAM_MASTER_MEMORY_MB, "1024");
@@ -56,18 +62,16 @@ public class Application implements ApplicationFactory {
     } else if (LAUNCHMODE_LOCAL.equals(conf.get(DAG.STRAM_LAUNCH_MODE))) {
       setLocalMode();
     }
-
-    this.generatorMaxWindowsCount = conf.getInt(P_generatorMaxWindowsCount, this.generatorMaxWindowsCount);
-    this.allInline = conf.getBoolean(P_allInline, this.allInline);
+    this.phoneRangeStart = conf.getInt(P_phoneRangeStart, this.phoneRangeStart);
+    this.phoneRangeEnd = conf.getInt(P_phoneRangeEnd, this.phoneRangeEnd);
   }
 
   private Operator getConsoleOperator(DAG b, String operatorName)
   {
     // output to HTTP server when specified in environment setting
-    String serverAddr = System.getenv("MALHAR_AJAXSERVER_ADDRESS");
-    if (serverAddr != null) {
+    if (this.ajaxServerAddr != null) {
       return b.addOperator(operatorName, HttpOutputModule.class)
-              .setProperty(HttpOutputModule.P_RESOURCE_URL, "http://" + serverAddr + "/channel/mobile/" + operatorName);
+              .setProperty(HttpOutputModule.P_RESOURCE_URL, "http://" + ajaxServerAddr + "/channel/mobile/" + operatorName);
     }
     return b.addOperator(operatorName, ConsoleOutputModule.class)
             //.setProperty(ConsoleOutputModule.P_DEBUG, "true")
@@ -79,8 +83,8 @@ public class Application implements ApplicationFactory {
     oper.setProperty(LoadSeedGenerator.KEY_STRING_SCHEMA, "false");
     oper.setProperty(LoadSeedGenerator.KEY_EMITKEY, "false");
     oper.setProperty(LoadSeedGenerator.KEY_KEYS, "x:0,500;y:0,500");
-    oper.setProperty(LoadSeedGenerator.KEY_SEED_START, "9000000");
-    oper.setProperty(LoadSeedGenerator.KEY_SEED_END    , "9999999");
+    oper.setProperty(LoadSeedGenerator.KEY_SEED_START, String.valueOf(this.phoneRangeStart));
+    oper.setProperty(LoadSeedGenerator.KEY_SEED_END  , String.valueOf(this.phoneRangeEnd));
 
     return oper;
   }
@@ -96,8 +100,8 @@ public class Application implements ApplicationFactory {
 
   public Operator getSeedClassifier(String name, DAG b) {
     Operator oper = b.addOperator(name, SeedClassifier.class);
-    oper.setProperty(SeedClassifier.KEY_SEED_START, "9000000");
-    oper.setProperty(SeedClassifier.KEY_SEED_END    , "9999999");
+    oper.setProperty(SeedClassifier.KEY_SEED_START, String.valueOf(this.phoneRangeStart));
+    oper.setProperty(SeedClassifier.KEY_SEED_END  , String.valueOf(this.phoneRangeEnd));
     oper.setProperty(SeedClassifier.KEY_IN_DATA1_CLASSIFIER, "x");
     oper.setProperty(SeedClassifier.KEY_IN_DATA2_CLASSIFIER, "y");
     oper.setProperty(SeedClassifier.KEY_STRING_SCHEMA, "false");
@@ -127,10 +131,7 @@ public class Application implements ApplicationFactory {
   public DAG getApplication(Configuration conf) {
 
     DAG dag = new DAG(conf);
-
-    // Waiting for local server to be set up. For now I hardcoded the phones to be dumped
-    // Operator phoneLocationQuery = dag.addOperator("phoneLocationQuery", HttpInputModule.class);
-    // phoneLocationQuery.setProperty(HttpInputModule.P_RESOURCE_URL, "http://localhost:8080/channel/mobile/phoneLocationQuery");
+    configure(conf);
 
     Operator seedGen = getSeedGenerator("seedGen", dag);
     Operator randomXGen = getRandomGenerator("xgen", dag);
@@ -146,7 +147,14 @@ public class Application implements ApplicationFactory {
     dag.addStream("ydata", randomYGen.getOutput(LoadRandomGenerator.OPORT_DATA), seedClassify.getInput(SeedClassifier.IPORT_IN_DATA2)).setInline(true);
     dag.addStream("incrdata", seedClassify.getOutput(SeedClassifier.OPORT_OUT_DATA), incrementer.getInput(LoadIncrementer.IPORT_INCREMENT)).setInline(true);
     dag.addStream("mobilelocation", incrementer.getOutput(LoadIncrementer.OPORT_DATA), indexMap.getInput(InvertIndexMap.IPORT_DATA)).setInline(true);
-    //dag.addStream("mobilequery", phoneLocationQuery.getOutput(HttpInputModule.OUTPUT, indexMap.getInput(InvertIndexMap.IPORT_QUERY));
+
+    if (this.ajaxServerAddr != null) {
+    // Waiting for local server to be set up. For now I hardcoded the phones to be dumped
+       Operator phoneLocationQuery = dag.addOperator("phoneLocationQuery", HttpInputModule.class);
+       phoneLocationQuery.setProperty(HttpInputModule.P_RESOURCE_URL, "http://" + ajaxServerAddr + "/channel/mobile/phoneLocationQuery");
+       dag.addStream("mobilequery", phoneLocationQuery.getOutput(HttpInputModule.OUTPUT), indexMap.getInput(InvertIndexMap.IPORT_QUERY));
+    }
+
     dag.addStream("consoledata", indexMap.getOutput(InvertIndexMap.OPORT_CONSOLE), phoneconsole.getInput(HttpOutputModule.INPUT)).setInline(true);
 
     return dag;
