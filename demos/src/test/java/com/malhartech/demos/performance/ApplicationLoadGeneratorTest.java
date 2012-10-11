c045134de985a595b45ddfb1a8511d3c72c6e893
Amol Kekre <amol@datatorrent.com>
2012-10-10 18:39:36 -0700

    Created a loadgen->devnull demo. Getting 8M/sec on a DAG. YAY!!!

---
+++ demos/src/test/java/com/malhartech/demos/performance/ApplicationLoadGeneratorTest.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ */
+package com.malhartech.demos.performance;
+
+import com.malhartech.stram.StramLocalCluster;
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Test;
+
+/**
+ * Test the DAG declaration in local mode.
+ */
+public class ApplicationLoadGeneratorTest
+{
+  @Test
+  public void testApplication() throws IOException, Exception
+  {
+    ApplicationLoadGenerator app = new ApplicationLoadGenerator();
+    final StramLocalCluster lc = new StramLocalCluster(app.getApplication(new Configuration(false)));
+    lc.setHeartbeatMonitoringEnabled(false);
+    lc.run();
+  }
+}
