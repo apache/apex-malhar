af43b642346656ca5f7c8e78da737f3767ed0a1e
Amol Kekre <amol@datatorrent.com>
2012-10-12 10:03:03 -0700

    Added a module to split streams

---
+++ library/src/main/java/com/malhartech/lib/testbench/StreamSplitter.java
@@ -0,0 +1,102 @@
+/*
+ *  Copyright (c) 2012 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.malhartech.lib.testbench;
+
+import com.malhartech.annotation.ModuleAnnotation;
+import com.malhartech.annotation.PortAnnotation;
+import com.malhartech.dag.AbstractModule;
+import com.malhartech.dag.FailedOperationException;
+import com.malhartech.dag.ModuleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Takes one stream and emits exactly same tuple on both the output ports. Needed to allow separation of listeners into two streamsr<p>
+ * <br>
+ * <br>
+ * Benchmarks: This node has been benchmarked at over ?? million tuples/second in local/inline mode<br>
+ *
+ * <b>Tuple Schema</b>: All tuples were treated as Object
+ * <b>Port Interface</b><br>
+ * <b>data</b>: Input port<br>
+ * <b>out_data1</b>: Output port 1<br>
+ * <b>out_data2</b>: Output port 2<br>
+ * <br>
+ * <b>Properties</b>:
+ * None
+ * <br>
+ * Compile time checks are:<br>
+ * no checks are done. Schema check is compile/instantiation time. Not runtime
+ * <br>
+ *
+ * @author amol
+ */
+@ModuleAnnotation(
+        ports = {
+  @PortAnnotation(name = StreamSplitter.IPORT_DATA, type = PortAnnotation.PortType.INPUT),
+  @PortAnnotation(name = StreamSplitter.OPORT_OUT_DATA1, type = PortAnnotation.PortType.OUTPUT),
+  @PortAnnotation(name = StreamSplitter.OPORT_OUT_DATA2, type = PortAnnotation.PortType.OUTPUT)
+})
+public class StreamSplitter extends AbstractModule
+{
+  public static final String IPORT_DATA = "data";
+  public static final String OPORT_OUT_DATA1 = "out_data1";
+  public static final String OPORT_OUT_DATA2 = "out_data2";
+  private static Logger LOG = LoggerFactory.getLogger(StreamSplitter.class);
+
+  final static int num_oport = 2;
+  /**
+   * Allows usage of StreamSplitter in a automated way
+   *
+   * @param i port index
+   * @return returns the proper input port name
+   */
+
+  static public String getOutputName(int i) {
+    String ret = "illegal_portnumber";
+    if ((i != 0) && (i <= num_oport)) {
+      ret = "out_data";
+      ret += Integer.toString(i);
+    }
+    return ret;
+  }
+
+  public int getNumOutputPorts(){
+    return num_oport;
+  }
+
+  /**
+   * Code to be moved to a proper base method name
+   *
+   * @param config
+   * @return boolean
+   */
+  public boolean myValidation(ModuleConfiguration config)  {
+    return true;
+  }
+
+
+  /**
+   * Sets up all the config parameters. Assumes checking is done and has passed
+   *
+   * @param config
+   */
+  @Override
+  public void setup(ModuleConfiguration config) throws FailedOperationException {
+    if (!myValidation(config)) {
+      throw new FailedOperationException("Did not pass validation");
+    }
+  }
+
+  /**
+   * Emit each tuple on both the output ports
+   * @param payload
+   */
+  @Override
+  public void process(Object payload) {
+    emit(OPORT_OUT_DATA1, payload);
+    emit(OPORT_OUT_DATA2, payload);
+  }
+}
