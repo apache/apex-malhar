abc4fafd2a44d1bb140ad2b1ed3e3ceb43dcd5f2
Amol Kekre <amol@datatorrent.com>
2012-10-23 21:56:00 -0700

    cleaned up

--- library/src/test/java/com/malhartech/lib/testbench/RandomEventGeneratorTest.java
+++ library/src/test/java/com/malhartech/lib/testbench/RandomEventGeneratorTest.java
@@ -3,11 +3,16 @@
  */
 package com.malhartech.lib.testbench;
 
+import com.malhartech.api.BaseOperator;
+import com.malhartech.api.DAG;
+import com.malhartech.api.DefaultInputPort;
+import com.malhartech.api.Operator;
 import com.malhartech.api.Sink;
 import com.malhartech.dag.StreamConfiguration;
 import com.malhartech.dag.Tuple;
 import com.malhartech.dag.WindowGenerator;
 import com.malhartech.stram.ManualScheduledExecutorService;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import junit.framework.Assert;
@@ -29,122 +34,89 @@ import org.slf4j.LoggerFactory;
  * DRC validation is done<br>
  * <br>
  */
+public class RandomEventGeneratorTest
+{
+  private static Logger LOG = LoggerFactory.getLogger(RandomEventGenerator.class);
 
-public class RandomEventGeneratorTest {
+  public static class CollectorInputPort<T> extends DefaultInputPort<T>
+  {
+    ArrayList<T> list;
+    final String id;
 
-    private static Logger LOG = LoggerFactory.getLogger(RandomEventGenerator.class);
-
-    class TestSink implements Sink {
-
-        HashMap<Object, Object> collectedTuples = new HashMap<Object, Object>();
-
-
-        //DefaultSerDe serde = new DefaultSerDe();
-        int count = 0;
-        boolean isstring = false;
-
-        /**
-         *
-         * @param payload
-         */
-        @Override
-        public void process(Object payload) {
-            if (payload instanceof Tuple) {
-                // LOG.debug(payload.toString());
-            }
-            else {
-                if (isstring) {
-                  collectedTuples.put((String) payload, null);
-                }
-                else {
-                  collectedTuples.put((Integer) payload, null);
-                }
-                count++;
-             }
-        }
+    public CollectorInputPort(String id, Operator module)
+    {
+      super(module);
+      this.id = id;
     }
+
+    @Override
+    public void process(T tuple)
+    {
+      list.add(tuple);
+    }
+  }
+
+  class TestSink implements Sink
+  {
+    HashMap<Object, Object> collectedTuples = new HashMap<Object, Object>();
+    //DefaultSerDe serde = new DefaultSerDe();
+    int count = 0;
+    boolean isstring = false;
 
     /**
-     * Test node logic emits correct results
+     *
+     * @param payload
      */
-    @Test
-    public void testNodeProcessing () throws Exception {
-      testSchemaNodeProcessing(true);
-      testSchemaNodeProcessing(false);
+    @Override
+    public void process(Object payload)
+    {
+      if (payload instanceof Tuple) {
+        // LOG.debug(payload.toString());
+      }
+      else {
+        if (isstring) {
+          collectedTuples.put((String)payload, null);
+        }
+        else {
+          collectedTuples.put((Integer)payload, null);
+        }
+        count++;
+      }
     }
+  }
 
+  /**
+   * Test node logic emits correct results
+   */
+  @Test
+  public void testNodeProcessing() throws Exception
+  {
+    testSchemaNodeProcessing(true);
+    testSchemaNodeProcessing(false);
+  }
 
-  @SuppressWarnings("SleepWhileInLoop")
-    public void testSchemaNodeProcessing(boolean isstring) throws Exception {
+    public static class CollectorOperator<T> extends BaseOperator
+  {
+    public final transient CollectorInputPort<T> sdata = new CollectorInputPort<T>("sdata", this);
+    public final transient CollectorInputPort<T> vdata = new CollectorInputPort<T>("vdata", this);
+    public final transient CollectorInputPort<T> vlist = new CollectorInputPort<T>("vlist", this);
+    public final transient CollectorInputPort<T> kvpair = new CollectorInputPort<T>("kvpair", this);
+  }
 
-        final RandomEventGenerator node = new RandomEventGenerator();
-        final ManualScheduledExecutorService mses = new ManualScheduledExecutorService(1);
-        final WindowGenerator wingen = new WindowGenerator(mses);
+  public void testSchemaNodeProcessing(boolean isstring) throws Exception
+  {
+    DAG dag = new DAG();
+    RandomEventGenerator node = dag.addOperator("randomgen", RandomEventGenerator.class);
 
-      StreamConfiguration config = new StreamConfiguration();
-      config.setLong(WindowGenerator.FIRST_WINDOW_MILLIS, 0);
-      config.setInt(WindowGenerator.WINDOW_WIDTH_MILLIS, 1);
-      wingen.setup(config);
+    TestSink lgenSink = new TestSink();
+    node.connect(RandomEventGenerator.OPORT_DATA, lgenSink);
 
-        Sink input = node.connect(Component.INPUT, wingen);
-        wingen.connect("mytestnode", input);
+    lgenSink.isstring = isstring;
 
-        TestSink lgenSink = new TestSink();
-        node.connect(LoadRandomGenerator.OPORT_DATA, lgenSink);
-        OperatorConfiguration conf = new OperatorConfiguration("mynode", new HashMap<String, String>());
-        lgenSink.isstring = isstring;
+    node.setMinvalue(0);
+    node.setMaxvalue(1000);
+    node.setTuplesblast(50000000);
 
-        conf.set(RandomEventGenerator.KEY_MIN_VALUE, "0");
-        conf.set(RandomEventGenerator.KEY_MAX_VALUE, "1000");
-        conf.setInt(RandomEventGenerator.KEY_TUPLES_BLAST, 50000000);
-        conf.set(RandomEventGenerator.KEY_STRING_SCHEMA, isstring ? "true" : "false");
-        conf.setInt(RandomEventGenerator.KEY_SLEEP_TIME, 1);
-
-        conf.setInt("SpinMillis", 2);
-        conf.setInt("BufferCapacity", 1024 * 1024);
-
-        node.setup(conf);
-
-        final AtomicBoolean inactive = new AtomicBoolean(true);
-        new Thread() {
-            @Override
-            public void run() {
-                inactive.set(false);
-                node.activate(new OperatorContext("LoadRandomGeneratorTestNode", this));
-            }
-        }.start();
-
-        /**
-         * spin while the node gets activated.
-         */
-        try {
-            do {
-                Thread.sleep(20);
-            } while (inactive.get());
-        } catch (InterruptedException ex) {
-            LOG.debug(ex.getLocalizedMessage());
-        }
-
-        wingen.activated(null);
-        for (int i = 0; i < 1000; i++) {
-            mses.tick(1);
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                LOG.error("Unexpected error while sleeping for 1 s", e);
-            }
-        }
-        node.deactivate();
-
-        // Let the reciever get the tuples from the queue
-        for (int i = 0; i < 20; i++) {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                LOG.error("Unexpected error while sleeping for 1 s", e);
-            }
-        }
-
-        LOG.debug("Processed {} tuples and {} unique strings", lgenSink.count, lgenSink.collectedTuples.size());
-    }
+    LOG.debug("Processed {} tuples and {} unique strings", lgenSink.count, lgenSink.collectedTuples.size());
+  }
 }
