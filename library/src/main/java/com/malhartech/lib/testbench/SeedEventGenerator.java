855e3bb6b3bb3f4afff74701727ffe7a40f853a8
Thomas Weise <thomas@datatorrent.com>
2012-11-01 19:12:54 -0700

    seed generator exit w/o thread interrupt

--- library/src/main/java/com/malhartech/lib/testbench/SeedEventGenerator.java
+++ library/src/main/java/com/malhartech/lib/testbench/SeedEventGenerator.java
@@ -106,7 +106,7 @@ public class SeedEventGenerator extends BaseOperator implements InputOperator
     }
     // done generating data
     LOG.info("Finished generating data.");
-    Thread.currentThread().interrupt();
+    throw new RuntimeException(new InterruptedException("Finished generating data."));
   }
 
   /**
