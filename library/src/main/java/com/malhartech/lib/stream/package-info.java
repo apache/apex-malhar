7184e9b3ea3337e543305e27f902265129362a19
Amol Kekre <amol@datatorrent.com>
2012-11-08 15:12:50 -0800

    added comments for faq

--- library/src/main/java/com/malhartech/lib/stream/package-info.java
+++ library/src/main/java/com/malhartech/lib/stream/package-info.java
@@ -9,7 +9,7 @@
  * <br>
  * <br> The classes are<br>
  * <b>{@link com.malhartech.lib.stream.ArrayListToItem}</b>: Takes in an ArrayList and emits each item in the array. Used for breaking up a ArrayList into Objects<br>
- * <b>{@link com.malhartech.lib.stream.DevNullCounter}</b>: Takes a in stream <b>data</b> and just drops the tuple. Used for logging. Increments a count and writes the net number (rate) to console. Mainly to be used to benchmark other modules<br>
+ * <b>{@link com.malhartech.lib.stream.DevNullCounter}</b>: Counts and then drops the tuple. Used for logging. Increments a count and writes the net number (rate) to console. Mainly to be used to benchmark other modules<br>
  * <b>{@link com.malhartech.lib.stream.HashMapToKey}</b>: Takes a HashMap and emits its keys, keyvals, vals. Used for breaking up a HashMap ito objects (keys, vals, or key/val pairs)<br>
  * <b>{@link com.malhartech.lib.stream.StreamDuplicater}</b>: Takes one stream and emits exactly same tuple on both the output ports. Needed to allow separation of listeners into two streams<br>
  * <b>{@link com.malhartech.lib.stream.StreamMerger}</b>: Merges two streams with identical schema and emits the tuples to the output port in order<br>
