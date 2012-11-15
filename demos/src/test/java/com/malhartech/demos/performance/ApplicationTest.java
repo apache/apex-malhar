6d95f36e48d30fcb3d39ed9571833c76a83e4320
Chetan Narsude <chetan@datatorrent.com>
2012-11-14 20:03:34 -0800

    enable heartbeatmonitoring.

--- demos/src/test/java/com/malhartech/demos/performance/ApplicationTest.java
+++ demos/src/test/java/com/malhartech/demos/performance/ApplicationTest.java
@@ -19,22 +19,21 @@ public class ApplicationTest
   {
     Application app = new Application();
     final StramLocalCluster lc = new StramLocalCluster(app.getApplication(new Configuration(false)));
-    lc.setHeartbeatMonitoringEnabled(false);
 
-//    new Thread("LocalClusterController")
-//    {
-//      @Override
-//      public void run()
-//      {
-//        try {
-//          Thread.sleep(10000);
-//        }
-//        catch (InterruptedException ex) {
-//        }
-//
-//        lc.shutdown();
-//      }
-//    }.start();
+    new Thread("LocalClusterController")
+    {
+      @Override
+      public void run()
+      {
+        try {
+          Thread.sleep(60000);
+        }
+        catch (InterruptedException ex) {
+        }
+
+        lc.shutdown();
+      }
+    }.start();
 
     lc.run();
   }
