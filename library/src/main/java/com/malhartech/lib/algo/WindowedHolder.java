9fc659c2aab1f2fe99cf1927a024a044b3adfcbd
Chetan Narsude <chetan@datatorrent.com>
2012-11-21 20:06:54 -0800

    remove the clone method and transient fields.

--- library/src/main/java/com/malhartech/lib/algo/WindowedHolder.java
+++ library/src/main/java/com/malhartech/lib/algo/WindowedHolder.java
@@ -6,7 +6,6 @@ package com.malhartech.lib.algo;
 
 import java.util.Comparator;
 
-
 /**
  *
  * @author Chetan Narsude <chetan@malhar-inc.com>
@@ -15,14 +14,11 @@ class WindowedHolder<T>
 {
   T identifier;
   int totalCount;
-  transient int position;
-  final transient int windowedCount[];
+  int position;
+  int windowedCount[];
 
   WindowedHolder()
   {
-    this.totalCount = 0;
-    this.position = 0;
-    windowedCount = null;
   }
 
   public WindowedHolder(T identifier, int windowCount)
@@ -51,30 +47,12 @@ class WindowedHolder<T>
   {
     return identifier + " => " + totalCount;
   }
-
-  @Override
-  public WindowedHolder clone()
-  {
-    WindowedHolder clone;
-    if (windowedCount == null) {
-      clone = new WindowedHolder();
-      clone.identifier = identifier;
-    }
-    else {
-      clone = new WindowedHolder(identifier, windowedCount.length);
-    }
-
-    clone.totalCount = totalCount;
-    clone.position = position;
-
-    return clone;
-  }
 }
 
-class TopSpotComparator implements Comparator<WindowedHolder>
+class TopSpotComparator implements Comparator<WindowedHolder<?>>
 {
   @Override
-  public int compare(WindowedHolder o1, WindowedHolder o2)
+  public int compare(WindowedHolder<?> o1, WindowedHolder<?> o2)
   {
     if (o1.totalCount > o2.totalCount) {
       return 1;
