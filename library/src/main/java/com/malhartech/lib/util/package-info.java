8dcdc2021842bee91621e6515433588eb9143bbe
Amol Kekre <amol@datatorrent.com>
2012-11-29 15:54:58 -0800

    Added more operators

--- library/src/main/java/com/malhartech/lib/util/package-info.java
+++ library/src/main/java/com/malhartech/lib/util/package-info.java
@@ -14,16 +14,20 @@
  * <b>{@link com.malhartech.lib.util.AbstractBaseNNonUniqueOperator}</b>: Tuples are ordered by key, and bottom N of the ordered tuples per key are emitted at the end of window<br>
  * <b>{@link com.malhartech.lib.util.AbstractBaseNUniqueOperator}</b>: Abstract class for sorting NUnique key, val pairs, emit is done at end of window<<br>
  * <b>{@link com.malhartech.lib.util.AbstractBaseSortOperator}</b>: Takes a stream of key value pairs via input port "data"; The incoming tuple is merged into already existing sorted list. At the end of the window the entire sorted list is emitted on output port "sort"<br>
- * <b>{@link com.malhartech.lib.util.BaseKeyOperator}</b>: Base class for operators that allows cloneKey for enabling users to use mutable objects<br>
- * <b>{@link com.malhartech.lib.util.BaseKeyValueOperator}</b>: Base class for operators that allows cloneValue and cloneKey for enabling users to use mutable objects<br>
+ * <b>{@link com.malhartech.lib.util.BaseKeyOperator}</b>: Base class for operators that allows cloneKey for mutable key objects<br>
+ * <b>{@link com.malhartech.lib.util.BaseKeyValueOperator}</b>: extends {@link com.malhartech.lib.util.BaseKeyOperator} and enables cloneValue for mutable value objects<br>
+ * <b>{@link com.malhartech.lib.util.BaseFilteredKeyValueOperator}</b>: extends {@link com.malhartech.lib.util.BaseKeyValueOperator} and enables filtering of keys<br>
  * <b>{@link com.malhartech.lib.util.BaseLineTokenizer}</b>: Base class for splitting lines into tokens and tokens into sub-tokens<br>
  * <b>{@link com.malhartech.lib.util.BaseMatchOperator}</b>:  * Base class that sets up the operator functionality needed for match based operators<br>
  * <b>{@link com.malhartech.lib.util.BaseNumberKeyValueOperator}</b>: Base class for operators that take in V extends Number. Provides basic methods for value conversion, and cloning of keys and values<br>
  * <b>{@link com.malhartech.lib.util.BaseNumberValueOperator}</b>: Base class for operators that take in K,V extends Number. Provides basic methods for value conversion and key cloning<br>
  * <b>{@link com.malhartech.lib.util.BaseUniqueKeyCounter}</b>: Count unique occurrences of keys within a window<<br>
  * <b>{@link com.malhartech.lib.util.BaseUniqueKeyValueCounter}</b>: Count unique occurrences of key,val pairs within a window<<br>
- * <b>{@link com.malhartech.lib.util.MutableDouble}</b>: A mutable double for basic operations. Makes things easy for incrementing etc.<br>
- * <b>{@link com.malhartech.lib.util.MutableInteger}</b>: A mutable integer for basic operations. Makes things easy for incrementing etc.<br>
+ * <b>{@link com.malhartech.lib.util.MutableDouble}</b>: A mutable double for basic operations. Makes things faster for incrementing etc. To be used for operations internal to operators<br>
+ * <b>{@link com.malhartech.lib.util.MutableInteger}</b>: A mutable integer for basic operations. Makes things fasterfor incrementing etc. To be used for operations internal to operators<br>
+ * <b>{@link com.malhartech.lib.util.MutableLong}</b>: A mutable long for basic operations. Makes things easy faster incrementing etc. To be used for operations internal to operators<br>
+ * <b>{@link com.malhartech.lib.util.MutableShort}</b>: A mutable short for basic operations. Makes things faster for incrementing etc. To be used for operations internal to operators<br>
+ * <b>{@link com.malhartech.lib.util.MutableFloat}</b>: A mutable float for basic operations. Makes things faster for incrementing etc. To be used for operations internal to operators<br>
  * <b>{@link com.malhartech.lib.util.OneKeyValPair}</b>: A single KeyValPair for basic data passing. It is a write once, and read often model<br>
  * <b>{@link com.malhartech.lib.util.ReversibleComparator}</b>: A comparator for ascending and descending lists<br>
  * <b>{@link com.malhartech.lib.util.TopNSort}</b>: Gives top N objects in ascending or descending order<br>
