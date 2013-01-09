febfbf57517c2f729df1d6f3ff9ff1f6380bd0c1
Chetan Narsude <chetan@datatorrent.com>
2013-01-08 20:05:36 -0800

    remove unused import.

--- library/src/test/java/com/malhartech/lib/codec/JavaSerializationStreamCodecTest.java
+++ library/src/test/java/com/malhartech/lib/codec/JavaSerializationStreamCodecTest.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.Serializable;
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
