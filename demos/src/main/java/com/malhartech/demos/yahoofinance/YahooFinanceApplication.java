273f2875008c1c0d47271e8d9e3509598b182be2
Locknath Shil <locknath@datatorrent.com>
2013-01-24 21:59:41 -0800

    disable multiwindow sum for now.

--- demos/src/main/java/com/malhartech/demos/yahoofinance/YahooFinanceApplication.java
+++ demos/src/main/java/com/malhartech/demos/yahoofinance/YahooFinanceApplication.java
@@ -58,7 +58,7 @@ public class YahooFinanceApplication implements ApplicationFactory
   {
     SumKeyVal<String, Long> oper = dag.addOperator(name, SumKeyVal.class);
     oper.setType(Long.class);
-    oper.setResetAtEndWindow(false);
+    //oper.setResetAtEndWindow(false);
     return oper;
   }
 
