0612a5d5c7722a22b97d60b621e79abda02c7ca5
Amol Kekre <amol@datatorrent.com>
2013-01-28 22:44:31 -0800

    Added UniqueValueMap

--- library/src/main/java/com/malhartech/lib/algo/UniqueValueMap.java
+++ library/src/main/java/com/malhartech/lib/algo/UniqueValueMap.java
@@ -20,8 +20,8 @@ import org.apache.commons.lang3.mutable.MutableInt;
  * This is an end of window operator. It uses sticky key partition and default unifier<br>
  * <br>
  * <b>Ports</b>:<br>
- * <b>data</b>: expects KeyValPair&lt;K,V&gt;<br>
- * <b>count</b>: emits KeyValPair&lt;K,Integer&gt;<br>
+ * <b>data</b>: expects Map&lt;K,V&gt;<br>
+ * <b>count</b>: emits HashMap&lt;K,Integer&gt;<br>
  * <br>
  * <b>Properties</b>: None<br>
  * <br>
@@ -29,7 +29,7 @@ import org.apache.commons.lang3.mutable.MutableInt;
  * <b>Specific run time checks</b>:<br>
  * <br>
  * <b>Benchmarks</b>: Blast as many tuples as possible in inline mode<br>
- * <table border="1" cellspacing=1 cellpadding=1 summary="Benchmark table for UniqueValueKeyVal&lt;K,V&gt; operator template">
+ * <table border="1" cellspacing=1 cellpadding=1 summary="Benchmark table for UniqueValueMap&lt;K,V&gt; operator template">
  * <tr><th>In-Bound</th><th>Out-bound</th><th>Comments</th></tr>
  * <tr><td><b>&gt; processes 3 Million K,V pairs/s</b></td><td>Emits one tuple per key per window</td><td>In-bound throughput
  * and number of unique vals per key are the main determinant of performance. Tuples are assumed to be immutable. If you use mutable tuples and have lots of keys,
@@ -37,7 +37,7 @@ import org.apache.commons.lang3.mutable.MutableInt;
  * </table><br>
  * <p>
  * <b>Function Table (K=String)</b>: The order of the K,V pairs in the tuple is undeterminable
- * <table border="1" cellspacing=1 cellpadding=1 summary="Function table for UniqueValueKeyVal&lt;K,V&gt; operator template">
+ * <table border="1" cellspacing=1 cellpadding=1 summary="Function table for UniqueValueMap&lt;K,V&gt; operator template">
  * <tr><th rowspan=2>Tuple Type (api)</th><th>In-bound (process)</th><th>Out-bound (emit)</th></tr>
  * <tr><th><i>data</i>KeyValPair(K,V)</th><th><i>count</i>(KeyValPair(K,Integer)</th></tr>
  * <tr><td>Begin Window (beginWindow())</td><td>N/A</td><td>N/A</td></tr>
@@ -55,27 +55,29 @@ import org.apache.commons.lang3.mutable.MutableInt;
  * <br>
  * @author Amol Kekre <amol@malhar-inc.com>
  */
-public class UniqueValueKeyVal<K,V> extends BaseKeyValueOperator<K,V>
+public class UniqueValueMap<K,V> extends BaseKeyValueOperator<K,V>
 {
   @InputPortFieldAnnotation(name = "data")
-  public final transient DefaultInputPort<KeyValPair<K,V>> data = new DefaultInputPort<KeyValPair<K,V>>(this)
+  public final transient DefaultInputPort<Map<K,V>> data = new DefaultInputPort<Map<K,V>>(this)
   {
     /**
      * Reference counts tuples
      */
     @Override
-    public void process(KeyValPair<K,V> tuple)
+    public void process(Map<K,V> tuple)
     {
-      HashSet<V> vals = map.get(tuple.getKey());
-      if (vals == null) {
-        vals = new HashSet<V>();
-        map.put(cloneKey(tuple.getKey()), vals);
+      for (Map.Entry<K, V> e: tuple.entrySet()) {
+        HashSet<V> vals = map.get(e.getKey());
+        if (vals == null) {
+          vals = new HashSet<V>();
+          map.put(cloneKey(e.getKey()), vals);
+        }
+        vals.add(e.getValue());
       }
-      vals.add(tuple.getValue());
     }
   };
   @OutputPortFieldAnnotation(name = "count")
-  public final transient DefaultOutputPort<KeyValPair<K,Integer>> count = new DefaultOutputPort<KeyValPair<K,Integer>>(this);
+  public final transient DefaultOutputPort<HashMap<K,Integer>> count = new DefaultOutputPort<HashMap<K,Integer>>(this);
 
   /**
    * Bucket counting mechanism.
@@ -84,15 +86,19 @@ public class UniqueValueKeyVal<K,V> extends BaseKeyValueOperator<K,V>
 
 
   /**
-   * Emits key,Integer pairs
+   * Emits one HashMap as tuple
    */
   @Override
   public void endWindow()
   {
-    for (Map.Entry<K,HashSet<V>> e: map.entrySet()) {
-      count.emit(new KeyValPair(e.getKey(), e.getValue().size()));
+    if (!map.isEmpty()) {
+      HashMap<K, Integer> tuple = new HashMap<K, Integer>(map.size());
+      for (Map.Entry<K, HashSet<V>> e: map.entrySet()) {
+        tuple.put(e.getKey(), e.getValue().size());
+      }
+      count.emit(tuple);
+      clearCache();
     }
-    clearCache();
   }
 
   public void clearCache()
