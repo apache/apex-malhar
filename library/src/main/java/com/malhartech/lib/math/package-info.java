9ca6247c8dd207bdf0f93a637fd997661a7a5c61
Amol Kekre <amol@datatorrent.com>
2013-01-28 21:03:36 -0800

    added docs

--- library/src/main/java/com/malhartech/lib/math/package-info.java
+++ library/src/main/java/com/malhartech/lib/math/package-info.java
@@ -5,7 +5,16 @@
  */
 
 /**
- * <b>{@link com.malhartech.lib.math}</b> is a library of arithmetic operators for reuse<p>
+ * <b>{@link com.malhartech.lib.math}</b> is a library of arithmetic operators for reuse.<p>
+ * <br>
+ * Most of the arithmetic operators come in three types based on their schema<br>
+ * The operators whose names ends with "Map" (eg SumMap, MaxMap, MinMap) take in Map on input ports and emit HashMap. These operators use
+ * round robin partitioning and would merge as per their functionality<br>
+ * The operators whose names ends with "KeyVal" (eg SumKeyVal, MaxKeyVal, MinKeyVal) take in KeyValPair and emit KeyValPair. These operators use
+ * sticky key partitioning and would merge using default pass through merge operator<br>
+ * The operators whose names are just their function name (eg Sum, Min, Max) operate on same objects and emit a final result. These operators have no keys.
+ * They partition in roundrobin and would merge as per their functionality<br>
+ * Average, Quotient operators are non-partitionable<br>
  * <br>
  * <br>
  */
