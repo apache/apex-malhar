8ce2df300434175534224621e34e34419188fb60
David Yan <david@datatorrent.com>
2013-01-31 19:37:57 -0800

    Apache log demo application

--- contrib/src/main/java/com/malhartech/demos/apachelog/Application.java
+++ contrib/src/main/java/com/malhartech/demos/apachelog/Application.java
@@ -2,43 +2,62 @@
  *  Copyright (c) 2012-2013 Malhar, Inc.
  *  All Rights Reserved.
  */
-package com.malhartech.demos.wordcount;
+package com.malhartech.demos.apachelog;
 
 import com.malhartech.api.ApplicationFactory;
+import com.malhartech.api.Context.OperatorContext;
 import com.malhartech.api.DAG;
-import com.malhartech.lib.algo.UniqueCounterEach;
+import com.malhartech.contrib.zmq.SimpleSinglePortZeroMQPullStringInputOperator;
+import com.malhartech.lib.algo.UniqueCounter;
 import com.malhartech.lib.io.ConsoleOutputOperator;
+import com.malhartech.lib.logs.ApacheLogParseOperator;
+import com.malhartech.lib.math.Sum;
+import com.malhartech.lib.stream.ArrayListAggregator;
 import org.apache.hadoop.conf.Configuration;
 
 /**
- * To run the test, you need to generate a data file as sample file.
- * default location is 
- * This program will count the number of words.
  *
- * @author Zhongjian Wang <zhongjian@malhar-inc.com>
+ * @author David Yan <davidyan@malhar-inc.com>
  */
-public class Application implements ApplicationFactory
-{
-  private boolean allInline =  false;
+public class Application implements ApplicationFactory {
 
-  @Override
-  public DAG getApplication(Configuration conf)
-  {
-    allInline = true;
+    private boolean allInline = false;
+    private String addr = "tcp://127.0.0.1:5555";
 
-    DAG dag = new DAG(conf);
-    
-    WordCountInputOperator input = dag.addOperator("wordinput", new WordCountInputOperator());
-    UniqueCounterEach<String> wordCount = dag.addOperator("count", new UniqueCounterEach<String>());
+    @Override
+    public DAG getApplication(Configuration conf) {
+        allInline = true;
 
-    dag.addStream("wordinput-count", input.outputPort, wordCount.data).setInline(allInline);
+        DAG dag = new DAG(conf);
 
-    ConsoleOutputOperator consoleOperator = dag.addOperator("console", new ConsoleOutputOperator());
-    dag.addStream("count-console",wordCount.count, consoleOperator.input);
+        dag.getAttributes().attr(DAG.STRAM_WINDOW_SIZE_MILLIS).set(1000);
+        SimpleSinglePortZeroMQPullStringInputOperator input = dag.addOperator("input", new SimpleSinglePortZeroMQPullStringInputOperator(addr));
+        ApacheLogParseOperator parse = dag.addOperator("parse", new ApacheLogParseOperator());
+        UniqueCounter ipAddrCount = dag.addOperator("ipAddrCount", new UniqueCounter<String>());
+        UniqueCounter urlCount = dag.addOperator("urlCount", new UniqueCounter<String>());
+        UniqueCounter httpStatusCount = dag.addOperator("httpStatusCount", new UniqueCounter<String>());
+        Sum<Long> numOfBytesSum = dag.addOperator("numOfBytesSum", new Sum<Long>());
+        ArrayListAggregator<Long> agg = dag.addOperator("agg", new ArrayListAggregator<Long>());
+        
+        dag.getOperatorWrapper(agg).getAttributes().attr(OperatorContext.APPLICATION_WINDOW_COUNT).set(3);
 
+        dag.addStream("input-parse", input.outputPort, parse.data).setInline(allInline);
+        dag.addStream("parse-ipAddrCount", parse.outputIPAddress, ipAddrCount.data).setInline(allInline);
+        dag.addStream("parse-urlCount", parse.outputUrl, urlCount.data).setInline(allInline);
+        dag.addStream("parse-httpStatusCount", parse.outputStatusCode, httpStatusCount.data).setInline(allInline);
+        dag.addStream("parse-numOfBytesSum", parse.outputBytes, numOfBytesSum.data).setInline(allInline);
+        dag.addStream("numOfBytesSum-agg", numOfBytesSum.sumLong, agg.input);
 
-    return dag;
-  }
+        ConsoleOutputOperator consoleOperator1 = dag.addOperator("console1", new ConsoleOutputOperator());
+        ConsoleOutputOperator consoleOperator2 = dag.addOperator("console2", new ConsoleOutputOperator());
+        ConsoleOutputOperator consoleOperator3 = dag.addOperator("console3", new ConsoleOutputOperator());
+        ConsoleOutputOperator consoleOperator4 = dag.addOperator("console4", new ConsoleOutputOperator());
 
+        dag.addStream("ipAddrCount-console", ipAddrCount.count, consoleOperator1.input);
+        dag.addStream("urlCount-console", urlCount.count, consoleOperator2.input);
+        dag.addStream("httpStatusCount-console", httpStatusCount.count, consoleOperator3.input);
+        dag.addStream("agg-console", agg.output, consoleOperator4.input);
 
+        return dag;
+    }
 }
