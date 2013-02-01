f9332775792a0a1de2c7144743b70182e6d91163
David Yan <david@datatorrent.com>
2013-01-31 19:37:34 -0800

    Added simple string pulling from zero MQ

--- contrib/src/main/java/com/malhartech/contrib/zmq/SimpleSinglePortZeroMQPullStringInputOperator.java
+++ contrib/src/main/java/com/malhartech/contrib/zmq/SimpleSinglePortZeroMQPullStringInputOperator.java
@@ -2,14 +2,9 @@
  *  Copyright (c) 2012-2013 Malhar, Inc.
  *  All Rights Reserved.
  */
-package com.malhartech.demos.wordcount;
+package com.malhartech.contrib.zmq;
 
-import com.malhartech.lib.io.SimpleSinglePortInputOperator;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.malhartech.contrib.zmq.SimpleSinglePortZeroMQPullInputOperator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,59 +12,21 @@ import org.slf4j.LoggerFactory;
  *
  * @author David Yan <davidyan@malhar-inc.com>
  */
-public class WordCountInputOperator extends SimpleSinglePortInputOperator<String> implements Runnable {
+public class SimpleSinglePortZeroMQPullStringInputOperator extends SimpleSinglePortZeroMQPullInputOperator<String> {
 
-    private static final Logger logger = LoggerFactory.getLogger(WordCountInputOperator.class);
-    protected long averageSleep = 300;
-    protected long sleepPlusMinus = 100;
-    protected String fileName = "src/main/resources/com/malhartech/demos/wordcount/samplefile.txt";
-    private transient BufferedReader br;
-    private transient DataInputStream in;
+    private static final Logger logger = LoggerFactory.getLogger(SimpleSinglePortZeroMQPullStringInputOperator.class);
 
-    public void setAverageSleep(long as) {
-        averageSleep = as;
+    private SimpleSinglePortZeroMQPullStringInputOperator() {
+        super("INVALID");
     }
-
-    public void setSleepPlusMinus(long spm) {
-        sleepPlusMinus = spm;
+    
+    public SimpleSinglePortZeroMQPullStringInputOperator(String addr) {
+        super(addr);
     }
-
-    public void setFileName(String fn) {
-        fileName = fn;
-    }
-
+    
     @Override
-    public void run() {
-        while (true) {
-            try {
-                String line;
-                FileInputStream fstream = new FileInputStream(fileName);
-
-                in = new DataInputStream(fstream);
-                br = new BufferedReader(new InputStreamReader(in));
-
-                while ((line = br.readLine()) != null) {
-                    String[] words = line.trim().split("[\\p{Punct}\\s\\\"\\'“”]+");
-                    for (String word : words) {
-                        word = word.trim().toLowerCase();
-                        if (!word.isEmpty()) {
-                            //System.out.println("Sending "+word);
-                            outputPort.emit(word);
-                        }
-                    }
-                    try {
-                        Thread.sleep(averageSleep + (new Double(sleepPlusMinus * (Math.random() * 2 - 1))).longValue());
-                    } catch (InterruptedException ex) {
-                        
-                    }
-                }
-                br.close();
-                in.close();
-                fstream.close();
-
-            } catch (IOException ex) {
-                logger.debug(ex.toString());
-            }
-        }
+    protected String convertFromBytesToTuple(byte[] bytes) {
+        return new String(bytes);
     }
+
 }
