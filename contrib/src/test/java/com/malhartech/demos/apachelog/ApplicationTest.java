16c10b8d4f95c0796e03fd7971c75f572cf428e4
Amol Kekre <amol@datatorrent.com>
2013-02-01 20:00:37 -0800

    Removed import for its own lib. Test package automatically imports source package

--- contrib/src/test/java/com/malhartech/demos/apachelog/ApplicationTest.java
+++ contrib/src/test/java/com/malhartech/demos/apachelog/ApplicationTest.java
@@ -4,7 +4,6 @@
  */
 package com.malhartech.demos.apachelog;
 
-import com.malhartech.demos.apachelog.*;
 import com.malhartech.stram.StramLocalCluster;
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
