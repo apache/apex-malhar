17e45036b5f6e9f335d50f820ce224a1a4b5cc6a
Chetan Narsude <chetan@datatorrent.com>
2013-02-05 15:14:53 -0800

    debugging.

--- library/src/main/java/com/malhartech/lib/io/AbstractHDFSInputOperator.java
+++ library/src/main/java/com/malhartech/lib/io/AbstractHDFSInputOperator.java
@@ -69,6 +69,7 @@ public abstract class AbstractHDFSInputOperator extends BaseOperator implements 
   @Override
   public void setup(OperatorContext context)
   {
+    logger.debug("trying to reopen file {}", filePath);
     try {
       fs = FileSystem.get(new Configuration());
     }
@@ -82,6 +83,8 @@ public abstract class AbstractHDFSInputOperator extends BaseOperator implements 
     catch (IOException ex) {
       throw new RuntimeException(ex);
     }
+
+    logger.debug("reopened file {}", filePath);
   }
 
   @Override
