9fb3b3ed3c5b55a052c6845401a3fcb9847f0354
David Yan <david@datatorrent.com>
2013-02-20 12:20:55 -0800

    made some members accessible for testings

--- demos/src/test/java/com/malhartech/demos/pi2/ApplicationTest.java
+++ demos/src/test/java/com/malhartech/demos/pi2/ApplicationTest.java
@@ -4,7 +4,9 @@
  */
 package com.malhartech.demos.pi2;
 
+import com.malhartech.api.DAG;
 import com.malhartech.demos.pi.*;
+import com.malhartech.stram.PhysicalPlan.PTOperator;
 import com.malhartech.stram.StramLocalCluster;
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
@@ -16,7 +18,7 @@ import static org.junit.Assert.*;
  */
 public class ApplicationTest
 {
-  @Test
+  //@Test
   public void testSomeMethod() throws Exception
   {
     Application topology = new Application();
@@ -39,5 +41,41 @@ public class ApplicationTest
     }/*.start()*/;
 
     lc.run();
+  }
+
+  @Test
+  public void testTupleRecorder() throws Exception
+  {
+    Application topology = new Application();
+    DAG dag = topology.getApplication(new Configuration(false));
+    final StramLocalCluster lc = new StramLocalCluster(dag);
+
+    new Thread("LocalClusterController")
+    {
+      @Override
+      public void run()
+      {
+        try {
+          Thread.sleep(10000);
+        }
+        catch (InterruptedException ex) {
+        }
+
+        lc.shutdown();
+      }
+
+    }/*.start()*/;
+
+    lc.runAsync();
+
+    PTOperator ptOp = lc.findByLogicalNode(dag.getOperatorWrapper("picalc"));
+    Thread.sleep(5000);
+
+    lc.getStreamingContainerManager().startRecording(ptOp.getId(), "doesNotMatter");
+
+    Thread.sleep(10000);
+    lc.getStreamingContainerManager().stopRecording(ptOp.getId());
+    Thread.sleep(5000);
+    lc.shutdown();
   }
 }
