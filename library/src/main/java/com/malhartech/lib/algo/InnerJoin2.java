97a86c9a868c0de0a4c49650bef6709e2c2f5fe2
Zhongjian Wang <zhongjian@datatorrent.com>
2013-02-25 16:20:57 -0800

    created new InnerJoin2 operator and test

---
+++ library/src/main/java/com/malhartech/lib/algo/InnerJoin2.java
@@ -0,0 +1,112 @@
+/*
+ *  Copyright (c) 2012-2013 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.malhartech.lib.algo;
+
+import com.malhartech.annotation.InputPortFieldAnnotation;
+import com.malhartech.annotation.OutputPortFieldAnnotation;
+import com.malhartech.api.DefaultInputPort;
+import com.malhartech.api.DefaultOutputPort;
+import com.malhartech.lib.util.BaseKeyValueOperator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import org.apache.commons.collections.map.MultiValueMap;
+
+/**
+ *
+ * @author Zhongjian Wang <zhongjian@malhar-inc.com>
+ */
+public class InnerJoin2 <K,V> extends BaseKeyValueOperator<K,V>
+{
+  public transient HashMap<K, ArrayList<HashMap<K,V>>> map1 = new HashMap<K, ArrayList<HashMap<K,V>>>();
+  public transient HashMap<K, ArrayList<HashMap<K,V>>> map2 = new HashMap<K, ArrayList<HashMap<K,V>>>();
+  @OutputPortFieldAnnotation(name = "result")
+  public final transient DefaultOutputPort<MultiValueMap> result = new DefaultOutputPort<MultiValueMap>(this);
+
+  @InputPortFieldAnnotation(name = "data1")
+  public final transient DefaultInputPort<HashMap<K,V>> data1 = new DefaultInputPort<HashMap<K,V>>(this)
+  {
+    /**
+     * Checks if key exists. If so emits all current combinations with matching tuples received on port "data2"
+     */
+    @Override
+    public void process(HashMap<K,V> tuple)
+    {
+      Iterator it = tuple.entrySet().iterator();
+      if( it.hasNext() ) {
+        Entry<K,V> e = (Entry)it.next();
+        emitTuples(tuple, map2.get(e.getKey()));
+        registerTuple(tuple, map1, e.getKey());
+      }
+    }
+  };
+  @InputPortFieldAnnotation(name = "data2")
+  public final transient DefaultInputPort<HashMap<K,V>> data2 = new DefaultInputPort<HashMap<K,V>>(this)
+  {
+    /**
+     * Checks if key exists. If so emits all current combinations with matching tuples received on port "data1"
+     */
+    @Override
+    public void process(HashMap<K,V> tuple)
+    {
+      Iterator it = tuple.entrySet().iterator();
+      if( it.hasNext() ) {
+        Entry<K,V> e = (Entry)it.next();
+        ArrayList<HashMap<K,V>> list = map1.get(e.getKey());
+
+        emitTuples(tuple, list);
+        registerTuple(tuple, map2, e.getKey());
+      }
+    }
+  };
+
+  public void emitTuples(HashMap<K,V> row, ArrayList<HashMap<K,V>> list) {
+    if( list == null )
+      return;
+    Iterator it = row.entrySet().iterator();
+    Entry<K,V> e = (Entry)it.next();
+    for( HashMap<K,V> map : list ) {
+      MultiValueMap multimap = new MultiValueMap(); // original K,V pair
+      multimap.put(e.getKey(), e.getValue());
+
+      Iterator it1 = map.entrySet().iterator(); // the other side K,V pair
+      Entry<K,V> e1 = (Entry)it1.next();
+      multimap.put(e1.getKey(), e1.getValue());
+      result.emit(multimap);
+      System.out.println("emitting:"+multimap.toString()+" emited "+emitedTuples+" list:"+list.toString());
+      emitedTuples++;
+    }
+  }
+
+  public void registerTuple(HashMap<K,V> tuple, HashMap<K, ArrayList<HashMap<K,V>>> map, K key) {
+    ArrayList<HashMap<K,V>> list = map.get(key);
+    if( list == null ) {
+      list = new ArrayList<HashMap<K,V>>();
+      map.put(key, list);
+    }
+    if( !list.contains(tuple) )
+      list.add(tuple);
+//    map.put(key, list);
+//    System.out.println("3map size:"+map.size()+" list size:"+list.size()+" tuple:"+tuple.toString()+" list:"+list.toString()+" map:"+map.toString());
+  }
+
+  @Override
+  public void beginWindow(long windowId) {
+    System.out.println("Join beginWindow "+windowId);
+  }
+  /**
+   * Clears cache/hash for both ports
+   */
+  @Override
+  public void endWindow()
+  {
+    map1.clear();
+    map2.clear();
+    System.out.println("Join Endwindow");
+  }
+  public int emitedTuples = 0;
+
+}
---
+++ library/src/test/java/com/malhartech/lib/algo/InnerJoin2Test.java
@@ -0,0 +1,183 @@
+/*
+ *  Copyright (c) 2012-2013 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.malhartech.lib.algo;
+
+import com.malhartech.api.BaseOperator;
+import com.malhartech.api.Context.OperatorContext;
+import com.malhartech.api.DAG;
+import com.malhartech.api.DefaultOutputPort;
+import com.malhartech.api.InputOperator;
+import com.malhartech.lib.io.ConsoleOutputOperator;
+import com.malhartech.stram.StramLocalCluster;
+import com.malhartech.stream.StramTestSupport;
+import com.malhartech.stream.StramTestSupport.WaitCondition;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Zhongjian Wang <zhongjian@malhar-inc.com>
+ */
+public class InnerJoin2Test
+{
+  public static class SourceModule1 extends BaseOperator
+  implements InputOperator
+  {
+
+    public final transient DefaultOutputPort<HashMap<String, String>> outPort = new DefaultOutputPort<HashMap<String, String>>(this);
+    int testNum=6;
+    int start;
+
+    @Override
+    public void setup(OperatorContext context)
+    {
+    }
+
+    @Override
+    public void beginWindow(long windowId) {
+      System.out.println("Source1 beginWindow:"+windowId);
+    }
+    @Override
+    public void endWindow() {
+      System.out.println("Source1 endWindow");
+    }
+
+    public void emitTuple(HashMap<String, String> message)
+    {
+      outPort.emit(message);
+//      System.out.println("emitting "+message.toString());
+    }
+
+    @Override
+    public void emitTuples()
+    {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(Integer.toString(31), "Rafferty");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(33), "Jones");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(33), "Steinberg");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(34), "Robinson");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(34), "Smith");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(-1), "John");
+        emitTuple(map);
+//      for( int i=start; i<start+testNum; i++ ) {
+//        HashMap<String, String> map = new HashMap<String, String>();
+//        map.put(Integer.toString(i), "1 number"+i);
+//        emitTuple(map);
+//      }
+//        System.out.println("emitting Souce1");
+    }
+
+    public void setTestNum(int testNum)
+    {
+      this.testNum = testNum;
+    }
+
+    public void setStart(int start) {
+      this.start = start;
+    }
+  }
+
+  public static class SourceModule2 extends BaseOperator
+  implements InputOperator
+  {
+    public final transient DefaultOutputPort<HashMap<String, String>> outPort = new DefaultOutputPort<HashMap<String, String>>(this);
+    int testNum=5;
+    int start;
+
+    @Override
+    public void setup(OperatorContext context)
+    {
+    }
+    @Override
+    public void beginWindow(long windowId) {
+      System.out.println("Source2 beginWindow:"+windowId);
+    }
+    @Override
+    public void endWindow() {
+      System.out.println("Source2 endWindow");
+    }
+
+    public void emitTuple(HashMap<String, String> message)
+    {
+      outPort.emit(message);
+    }
+
+    @Override
+    public void emitTuples()
+    {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(Integer.toString(31), "Sales");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(33), "Engineering");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(34), "Clerical");
+        emitTuple(map);
+        map.clear();
+        map.put(Integer.toString(35), "Marketing");
+        emitTuple(map);
+        map.clear();
+//      for( int i=start; i<start+testNum; i++ ) {
+//        HashMap<String, String> map = new HashMap<String, String>();
+//        map.put(Integer.toString(i), "2 number"+i);
+//        emitTuple(map);
+//      }
+//        System.out.println("emitting Souce2");
+    }
+
+    public void setTestNum(int testNum)
+    {
+      this.testNum = testNum;
+    }
+
+    public void setStart(int start) {
+      this.start = start;
+    }
+  }
+
+  @Test
+  public void testDag() throws Exception
+  {
+    DAG dag = new DAG();
+    SourceModule1 input1 = dag.addOperator("input1", SourceModule1.class);
+    SourceModule2 input2 = dag.addOperator("input2", SourceModule2.class);
+//    input1.setStart(0);
+//    input2.setStart(3);
+    final InnerJoin2<String, String> join = dag.addOperator("join", new InnerJoin2<String, String>());
+
+    ConsoleOutputOperator output = dag.addOperator("console", ConsoleOutputOperator.class);
+
+    dag.addStream("stream1", input1.outPort, join.data1);
+    dag.addStream("stream2", input2.outPort, join.data2);
+    dag.addStream("output", join.result, output.input);
+
+    final StramLocalCluster localCluster = new StramLocalCluster(dag);
+    localCluster.runAsync();
+    WaitCondition c = new WaitCondition()
+    {
+      @Override
+      public boolean isComplete()
+      {
+        return false;
+      }
+    };
+     StramTestSupport.awaitCompletion(c, 3000);
+//    Assert.assertTrue("Tuple recorder shouldn't exist any more after stopping", StramTestSupport.awaitCompletion(c, 4000));
+
+  }
+}
