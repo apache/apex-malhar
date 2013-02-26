77a4ded6f447bb848cc4534f1839c711d75d21a9
David Yan <david@datatorrent.com>
2013-02-25 16:28:08 -0800

    added escalation levels

--- library/src/main/java/com/malhartech/lib/util/Alert.java
+++ library/src/main/java/com/malhartech/lib/util/Alert.java
@@ -14,29 +14,85 @@ import com.malhartech.api.DefaultOutputPort;
  *
  * @author David Yan <davidyan@malhar-inc.com>
  */
-public class Alert extends BaseOperator
+public class Alert<T> extends BaseOperator
 {
-  protected long lastAlert = -1;
+  protected long lastAlertTimeStamp = -1;
+  protected long inAlertSince = -1;
+  protected long timeout = 5000; // 5 seconds
   protected long alertFrequency = 0;
+  protected long levelOneTimeStamp = 0;
+  protected long levelTwoTimeStamp = 0;
+  protected long levelThreeTimeStamp = 0;
   @InputPortFieldAnnotation(name = "in", optional = false)
-  public final transient DefaultInputPort<Object> in = new DefaultInputPort<Object>(this)
+  public final transient DefaultInputPort<T> in = new DefaultInputPort<T>(this)
   {
     @Override
-    public void process(Object tuple)
+    public void process(T tuple)
     {
       long now = System.currentTimeMillis();
-      if (lastAlert + alertFrequency < now) {
-        alert.emit(tuple);
-        lastAlert = now;
+      if (inAlertSince < 0) {
+        inAlertSince = now;
+      }
+      if (lastAlertTimeStamp + alertFrequency < now) {
+        if (inAlertSince >= levelOneTimeStamp) {
+          alert1.emit(tuple);
+        }
+        if (inAlertSince >= levelTwoTimeStamp) {
+          alert2.emit(tuple);
+        }
+        if (inAlertSince >= levelThreeTimeStamp) {
+          alert3.emit(tuple);
+        }
+        lastAlertTimeStamp = now;
       }
     }
 
   };
-  @OutputPortFieldAnnotation(name = "alert", optional = false)
-  public final transient DefaultOutputPort<Object> alert = new DefaultOutputPort<Object>(this);
+  @OutputPortFieldAnnotation(name = "alert1", optional = false)
+  public final transient DefaultOutputPort<T> alert1 = new DefaultOutputPort<T>(this);
+  @OutputPortFieldAnnotation(name = "alert2", optional = false)
+  public final transient DefaultOutputPort<T> alert2 = new DefaultOutputPort<T>(this);
+  @OutputPortFieldAnnotation(name = "alert3", optional = false)
+  public final transient DefaultOutputPort<T> alert3 = new DefaultOutputPort<T>(this);
 
   public void setAlertFrequency(long millis)
   {
     alertFrequency = millis;
   }
+
+  public void setLevelOneAlertTimeStamp(long millis)
+  {
+    levelOneTimeStamp = millis;
+  }
+
+  public void setLevelTwoAlertTimeStamp(long millis)
+  {
+    levelTwoTimeStamp = millis;
+
+  }
+
+  public void setLevelThreeAlertTimeStamp(long millis)
+  {
+    levelThreeTimeStamp = millis;
+  }
+
+  public void setTimeout(long millis)
+  {
+    timeout = millis;
+  }
+
+  @Override
+  public void endWindow()
+  {
+    checkTimeout();
+  }
+
+  protected void checkTimeout()
+  {
+    if (System.currentTimeMillis() - inAlertSince > timeout) {
+      inAlertSince = -1;
+      lastAlertTimeStamp = -1;
+    }
+  }
+
 }
