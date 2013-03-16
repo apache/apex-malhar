0b7b5e29434789d8388bea58aa3d1baf68df46a7
David Yan <david@datatorrent.com>
2013-03-15 18:32:58 -0700

    WebSocketInputOperator done and tested. And some move-around of classes.

---
+++ library/src/main/java/com/malhartech/lib/io/WebSocketInputOperator.java
@@ -0,0 +1,118 @@
+/*
+ *  Copyright (c) 2012 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.malhartech.lib.io;
+
+import com.malhartech.annotation.ShipContainingJars;
+import com.malhartech.api.Context.OperatorContext;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import javax.validation.constraints.NotNull;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocket.Connection;
+import org.eclipse.jetty.websocket.WebSocketClient;
+import org.eclipse.jetty.websocket.WebSocketClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * Reads via GET from given URL as input stream (entities on stream delimited by leading length)<p>
+ * <br>
+ * Incoming data is interpreted as JSONObject and converted to {@link java.util.Map}.<br>
+ * <br>
+ *
+ */
+@ShipContainingJars(classes = {org.codehaus.jackson.JsonFactory.class,org.eclipse.jetty.websocket.WebSocket.class})
+public class WebSocketInputOperator extends SimpleSinglePortInputOperator<Map<String, String>> implements Runnable
+{
+  private static final Logger LOG = LoggerFactory.getLogger(WebSocketInputOperator.class);
+  /**
+   * Timeout interval for reading from server. 0 or negative indicates no timeout.
+   */
+  public int readTimeoutMillis = 0;
+  /**
+   * The URL of the web service resource for the POST request.
+   */
+  @NotNull
+  private WebSocketClientFactory factory = new WebSocketClientFactory();
+  private WebSocketClient client;
+  private URI channelUrl;
+  private JsonFactory jsonFactory = new JsonFactory();
+  private ObjectMapper mapper = new ObjectMapper(jsonFactory);
+
+  public void setUrl(URI u)
+  {
+    channelUrl = u;
+  }
+
+  @Override
+  public void setup(OperatorContext context)
+  {
+    try {
+      factory.setBufferSize(8192);
+      factory.start();
+
+      client = factory.newWebSocketClient();
+      LOG.info("URL: {}", channelUrl);
+    }
+    catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  @Override
+  public void teardown()
+  {
+    if (factory != null) {
+      factory.destroy();
+    }
+    super.teardown();
+  }
+
+  @Override
+  public void run()
+  {
+    try {
+      client.open(channelUrl, new WebSocket.OnTextMessage()
+      {
+        @Override
+        public void onMessage(String string)
+        {
+          LOG.debug("Got: " + string);
+          try {
+            Map<String, String> o = mapper.readValue(string, HashMap.class);
+            outputPort.emit(o);
+          }
+          catch (IOException ex) {
+            LOG.error("Got exception: ", ex);
+          }
+        }
+
+        @Override
+        public void onOpen(Connection cnctn)
+        {
+          LOG.debug("Connection opened");
+        }
+
+        @Override
+        public void onClose(int i, String string)
+        {
+          LOG.debug("Connection closed.");
+        }
+
+      }).get(5, TimeUnit.SECONDS);
+    }
+    catch (Exception ex) {
+      LOG.error("Error reading from " + channelUrl, ex);
+    }
+
+  }
+
+}
---
+++ library/src/test/java/com/malhartech/lib/io/WebSocketInputOperatorTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ */
+package com.malhartech.lib.io;
+
+import com.malhartech.daemon.Daemon;
+import com.malhartech.engine.TestSink;
+import com.malhartech.stream.SampleWebSocketPublisher;
+import java.net.URI;
+import java.util.Map;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class WebSocketInputOperatorTest
+{
+  @Test
+  @SuppressWarnings("SleepWhileInLoop")
+  public void testWebSocketInputModule() throws Exception
+  {
+
+    System.out.println("Starting Daemon...");
+    Daemon.setPort(9090);
+    Daemon.setLocalMode(true);
+    Daemon.setup();
+    Daemon.start();
+
+    SampleWebSocketPublisher sp = new SampleWebSocketPublisher();
+    sp.setChannelUrl("http://localhost:9090/channel/testChannel");
+    Thread t = new Thread(sp);
+    t.start();
+
+    String url = "ws://localhost:9090/channel/testChannel";
+    final WebSocketInputOperator operator = new WebSocketInputOperator();
+
+    TestSink<Map<String, String>> sink = new TestSink<Map<String, String>>();
+
+    operator.outputPort.setSink(sink);
+    operator.setName("testWebSocketInputNode");
+    operator.setUrl(new URI(url));
+
+    operator.setup(null);
+    operator.activate(null);
+
+    int timeoutMillis = 2000;
+    while (sink.collectedTuples.isEmpty() && timeoutMillis > 0) {
+      operator.emitTuples();
+      timeoutMillis -= 20;
+      Thread.sleep(20);
+    }
+
+    Assert.assertTrue("tuple emitted", sink.collectedTuples.size() > 0);
+
+    Map<String, String> tuple = sink.collectedTuples.get(0);
+    Assert.assertEquals("", tuple.get("hello"), "world");
+
+    operator.deactivate();
+    operator.teardown();
+    t.interrupt();
+    t.join();
+  }
+
+}
