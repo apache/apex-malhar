edeb4b391828b1a1ad08bf4b11d8e093d6d1c8b4
Amol Kekre <amol@datatorrent.com>
2013-06-02 14:24:30 -0700

    More doccs; AppDevGuide, StdLib, OpDevGuide first cut done

---
+++ library/src/main/java/com/malhartech/lib/samplecode/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ *
+ */
+
+/**
+ * <b>{@link com.malhartech.lib.samplecode}</b> is a collection of libraries that have examples of various operators<p>
+ * <br>
+ * <br>
+ */
+
+package com.malhartech.lib.samplecode;
