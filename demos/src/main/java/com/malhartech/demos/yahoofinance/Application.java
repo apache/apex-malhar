d00235646ccee79b644450c7aba9d7898afceac2
Dinesh Prasad <dinesh@datatorrent.com>
2013-06-14 11:27:46 -0700

    Added user doc in demo

--- demos/src/main/java/com/malhartech/demos/yahoofinance/Application.java
+++ demos/src/main/java/com/malhartech/demos/yahoofinance/Application.java
@@ -5,6 +5,7 @@
 package com.malhartech.demos.yahoofinance;
 
 import com.malhartech.api.ApplicationFactory;
+import com.malhartech.api.LocalMode;
 import com.malhartech.api.Context.OperatorContext;
 import com.malhartech.api.DAG;
 import com.malhartech.api.Operator.InputPort;
@@ -17,9 +18,171 @@ import com.malhartech.lib.util.HighLow;
 import org.apache.hadoop.conf.Configuration;
 
 /**
- * Yahoo! Finance application demo. <p>
- *
- * Get Yahoo finance feed and calculate minute price range, minute volume, simple moving average of 5 minutes.
+ * Yahoo! Finance Application Demo :<br>
+ * Get Yahoo finance feed and calculate minute price range, minute volume,
+ * simple moving average of 5 minutes. <br>
+ * <br>
+ * Functional Description : <br>
+ * Application samples yahoo finance ticker every 200ms. All data points in one
+ * second are streamed from input adapter. <br>
+ * <br>
+ * 
+ * Application calculates following Real Time Value(s):<br>
+ * <ul>
+ * <li>Quotes for IBM, Google, Apple, Yahoo stocks price/volume/time displayed
+ * every second.</li>
+ * <li>Charts for Stocks in terms for high/low price vs volume for last minute.</li>
+ * <li>Simple moving average over last 5 minutes for IBM, Google, Apple, Yahoo
+ * stocks.</li>
+ * </ul>
+ * <br>
+ * <br>
+ * 
+ * Custom Attribute : <br>
+ * <ul>
+ * <li>Application streaming window size(STREAMING_WINDOW_SIZE_MILLIS) = 1 sec,
+ * since we are only interested in quotes every second.</li>
+ * <li>Range/Minute Volume operator's window size(APPLICATION_WINDOW_COUNT) =
+ * 60, aggregate over one minute.</li>
+ * <li>Sum operator window length : 300, sliding average over last 5 minutes.</li>
+ * </ul>
+ * <br>
+ * 
+ * Input Adapter : <br>
+ * Stock Tick input operator get yahoo finance real time stock quotes data and
+ * pushes application. <br>
+ * <br>
+ * 
+ * Output Adapter : <br>
+ * Output values are written to console through ConsoleOutputOerator<br>
+ * if you need to change write to HDFS,HTTP .. instead of console, <br>
+ * Please refer to {@link com.malhartech.lib.io.HttpOutputOperator} or
+ * {@link com.malhartech.lib.io.HdfsOutputOperator}. <br>
+ * <br>
+ * 
+ * Run Sample Application : <br>
+ * Please consult Application Developer guide <a href=
+ * "https://docs.google.com/document/d/1WX-HbsGQPV_DfM1tEkvLm1zD_FLYfdLZ1ivVHqzUKvE/edit#heading=h.lfl6f68sq80m"
+ * > here </a>.
+ * <p>
+ * Running Java Test or Main app in IDE:
+ * 
+ * <pre>
+ * LocalMode lma = LocalMode.newInstance();
+ * new Application().populateDAG(lma.getDAG(), new Configuration(false));
+ * LocalMode.Controller lc = lma.getController();
+ * lc.run();
+ * </pre>
+ * 
+ * <br>
+ * <br>
+ * 
+ * Run Success : <br>
+ * For successful deployment and run, user should see following output on
+ * console:
+ * 
+ * <pre>
+ *  Price SMA: AAPL=435.965
+ * Price SMA: GOOG=877.0
+ * QUOTE: {YHOO=[26.37, 9760360, 4:00pm, null, null], IBM=[203.77, 2899698, 4:00pm, null, null], GOOG=[877.0, 2069614, 4:00pm, null, null], AAPL=[435.965, 10208099, 4:00pm, null, null]}
+ * Price SMA: YHOO=26.37
+ * </pre>
+ * 
+ * Scaling Options : <br>
+ * <ul>
+ * <li>Volume operator can be replicated using 'INITIAL_PARTITION_COUNT' options
+ * on operator.</li>
+ * <li>Range value operator can replicated but using proper unifier
+ * operator(read App Dev Guide).</li>
+ * <li>Slinging window operator can be replicated with proper unifier operator.</li>
+ * </ul>
+ * <br>
+ * 
+ * Application DAG : <br>
+ * <img src="../../../../resources/Application.gif" width=600px > <br>
+ * <br>
+ * 
+ * Streaming Window Size : 1000 ms(1 Sec) <br>
+ * Operator Details : <br>
+ * <ul>
+ * <li>
+ * <p>
+ * <b>The operator DailyVolume:</b> This operator reads from the input port,
+ * which contains the incremental volume tuples from StockTickInput, and
+ * aggregates the data to provide the cumulative volume. It just utilizes the
+ * library class SumKeyVal<K,V> provided in math package. In this case,
+ * SumKeyVal<String,Long>, where K is the stock symbol, V is the aggregated
+ * volume, with cumulative set to true. (Otherwise if cumulative was set to
+ * false, SumKeyVal would provide the sum for the application window.) Malhar
+ * provides a number of built-in operators for simple operations like this so
+ * that application developers do not have to write them. More examples to
+ * follow. This operator assumes that the application restarts before market
+ * opens every day.
+ * </p>
+ * Class : {@link com.malhartech.lib.math.SumKeyVal} <br>
+ * Operator Application Window Count : 1 <br>
+ * StateFull : YES, volume gets aggregated every window count.</li>
+ * 
+ * <li>
+ * <p>
+ * <b>The operator MinuteVolume:</b> This operator reads from the input port,
+ * which contains the volume tuples from StockTickInput, and aggregates the data
+ * to provide the sum of the volume within one minute. Like the operator
+ * DailyVolume, this operator is also SumKeyVal<String,Long>, but with
+ * cumulative set to false. Application Window is set to 1 minute. We will
+ * explain how to set this later. <br>
+ * Class : {@link com.malhartech.lib.math.SumKeyVal} <br>
+ * Operator App Window Count : 60 (1 Minute) <br>
+ * StateFull : YES, aggregate over last 60 windows.</li>
+ * 
+ * <li>
+ * <p>
+ * <b>The operator Quote:</b> This operator has three input ports, which are
+ * price (from StockTickInput), daily_vol (from Daily Volume), and time (from
+ * StockTickInput). This operator just consolidates the three data and and emits
+ * the consolidated data. It utilizes the class ConsolidatorKeyVal<K> from
+ * stream package.<br>
+ * Class : {@link com.malhartech.lib.stream.ConsolidatorKeyVal} <br>
+ * Operator App Window Count : 1 <br>
+ * State Less : YES</li>
+ * 
+ * <li>
+ * <p>
+ * <b>The operator Chart:</b> This operator is very similar to the operator
+ * Quote, except that it takes inputs from High Low and Minute Vol and outputs
+ * the consolidated tuples to the output port. <br>
+ * Class : {@link com.malhartech.lib.stream.ConsolidatorKeyVal} <br>
+ * StateLess : YES<br>
+ * Operator App Window Count : 1</li>
+ * 
+ * 
+ * <li>
+ * <p>
+ * <b>The operator PriceSMA:</b> SMA stands for - Simple Moving Average. It
+ * reads from the input port, which contains the price tuples from
+ * StockTickInput, and provides the moving average price of the stock. It
+ * utilizes SimpleMovingAverage<String,Double>, which is provided in multiwindow
+ * package. SimpleMovingAverage keeps track of the data of the previous N
+ * application windows in a sliding manner. For each end window event, it
+ * provides the average of the data in those application windows. <br>
+ * Class : {@link com.malhartech.lib.multiwindow.SimpleMovingAverage} <br>
+ * State full : YES, stores values across application window. <br>
+ * Operator App Window : 1 <br>
+ * Operator Sliding Window : 300 (5 mins).</li>
+ * 
+ * <li>
+ * <p>
+ * <b>The operator Console: </b> This operator just outputs the input tuples to
+ * the console (or stdout). In this example, there are four console operators,
+ * which connect to the output of Quote, Chart, PriceSMA and VolumeSMA. In
+ * practice, they should be replaced by operators which would do something about
+ * the data, like drawing charts. </li>
+ * 
+ * </ul>
+ * <br>
+ * 
+ * 
+ * @author Amol Kekre
  */
 public class Application implements ApplicationFactory
 {
@@ -29,10 +192,10 @@ public class Application implements ApplicationFactory
   protected String[] tickers = {"IBM", "GOOG", "AAPL", "YHOO"};
 
   /**
-   * Get actual Yahoo finance ticks of symbol, last price, total daily volume, and last traded price.
-   * @param name
-   * @param dag
-   * @return
+   * Instantiate stock input operator for actual Yahoo finance ticks of symbol, last price, total daily volume, and last traded price.
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @return StockTickInput instance.
    */
   public StockTickInput getStockTickInputOperator(String name, DAG dag)
   {
@@ -43,10 +206,11 @@ public class Application implements ApplicationFactory
   }
 
   /**
-   * This sends total daily volume by adding volumes from each ticks.
-   * @param name
-   * @param dag
-   * @return
+   * Instantiate {@link com.malhartech.lib.math.SumKeyVal} operator 
+   * to sends total daily volume by adding volumes from each ticks.
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @return SumKeyVal instance.
    */
   public SumKeyVal<String, Long> getDailyVolumeOperator(String name, DAG dag)
   {
@@ -58,11 +222,12 @@ public class Application implements ApplicationFactory
   }
 
   /**
+   * Instantiate {@link com.malhartech.lib.math.SumKeyVal} operator 
    * Get aggregated volume of 1 minute and send at the end window of 1 minute.
-   * @param name
-   * @param dag
-   * @param appWindowCount
-   * @return
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @param appWindowCount Operator aggregate window size.
+   * @return SumKeyVal instance.
    */
   public SumKeyVal<String, Long> getMinuteVolumeOperator(String name, DAG dag, int appWindowCount)
   {
@@ -74,11 +239,13 @@ public class Application implements ApplicationFactory
   }
 
   /**
+   * Instantiate {@link com.malhartech.lib.math.RangeKeyVal} operator to get high/low
+   * value for each key within given application window.
    * Get High-low range for 1 minute.
-   * @param name
-   * @param dag
-   * @param appWindowCount
-   * @return
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @param appWindowCount Operator aggregate window size.
+   * @return RangeKeyVal instance.
    */
   public RangeKeyVal<String, Double> getHighLowOperator(String name, DAG dag, int appWindowCount)
   {
@@ -89,10 +256,11 @@ public class Application implements ApplicationFactory
   }
 
   /**
+   * Instantiate {@link com.malhartech.lib.stream.ConsolidatorKeyVal} to send
    * Quote (Merge price, daily volume, time)
-   * @param name
-   * @param dag
-   * @return
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @return ConsolidatorKeyVal instance.
    */
   public ConsolidatorKeyVal<String,Double,Long,String,?,?> getQuoteOperator(String name, DAG dag)
   {
@@ -101,10 +269,11 @@ public class Application implements ApplicationFactory
   }
 
   /**
+   * Instantiate {@link com.malhartech.lib.stream.ConsolidatorKeyVal} to send
    * Chart (Merge minute volume and minute high-low)
-   * @param name
-   * @param dag
-   * @return
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @return ConsolidatorKeyVal instance.
    */
   public ConsolidatorKeyVal<String,HighLow,Long,?,?,?> getChartOperator(String name, DAG dag)
   {
@@ -113,11 +282,12 @@ public class Application implements ApplicationFactory
   }
 
   /**
-   * Get simple moving average of price.
-   * @param name
-   * @param dag
-   * @param appWindowCount
-   * @return
+   * Instantiate {@link com.malhartech.lib.multiwindow.SimpleMovingAverage} to calculate moving average for price
+   * over given window size. Sliding window size is 1.
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @param appWindowCount Operator aggregate window size.
+   * @return SimpleMovingAverage instance.
    */
   public SimpleMovingAverage<String, Double> getPriceSimpleMovingAverageOperator(String name, DAG dag, int appWindowCount)
   {
@@ -128,11 +298,10 @@ public class Application implements ApplicationFactory
   }
 
   /**
-   * Get console for output.
-   * @param name
-   * @param dag
-   * @param prefix
-   * @return
+   * Get console for output operator.
+   * @param name  Operator name 
+   * @param dag   Application DAG graph.
+   * @return input port for console output.
    */
   public InputPort<Object> getConsole(String name, /*String nodeName,*/ DAG dag, String prefix)
   {
@@ -152,9 +321,7 @@ public class Application implements ApplicationFactory
   }
 
   /**
-   * Create Yahoo Finance Application DAG.
-   * @param conf
-   * @return
+   * Populate Yahoo Finance Demo Application DAG.
    */
   @Override
   public void populateDAG(DAG dag, Configuration conf)
