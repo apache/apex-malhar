d1e10484de4028a5e72cd0a5122a14a678659bc5
Thomas Weise <thomas@datatorrent.com>
2013-06-15 19:34:45 -0700

    package rename for demos

---
+++ demos/src/test/java/com/datatorrent/demos/pi/ApplicationTest.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2012-2013 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.malhartech.demos.pi;
+
+import com.malhartech.api.LocalMode;
+import org.junit.Test;
+
+/**
+ *
+ * @author Zhongjian Wang <zhongjian@malhar-inc.com>
+ */
+public class ApplicationTest
+{
+  @Test
+  public void testSomeMethod() throws Exception
+  {
+    LocalMode.runApp(new Application(), 10000);
+  }
+}
