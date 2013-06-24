40e33ce3fad400f179191774cbb7609df4083d9f
Dinesh Prasad <dinesh@datatorrent.com>
2013-06-24 09:40:35 -0700

    demo code

---
+++ library/src/main/java/com/datatorrent/lib/testbench/KeyValSum.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2012 Malhar, Inc. All Rights Reserved.
+ */
+package com.datatorrent.lib.testbench;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.Operator;
+import com.datatorrent.api.Context.OperatorContext;
+
+/**
+ * @author Dinesh Prasad (dinesh@malhar-inc.com)
+ */
+public class KeyValSum extends BaseOperator
+{
+	private Map<String, Integer> collect;
+	public final transient DefaultInputPort<Map<String, Integer>> inport = new DefaultInputPort<Map<String, Integer>>(this) {
+    @Override
+    public void process(Map<String, Integer> s) {
+    	for(Map.Entry<String, Integer> entry : s.entrySet())
+    	{
+	    	if (collect.containsKey(entry.getKey()))
+	    	{
+	    		Integer value = (Integer)collect.remove(entry.getKey());
+	    		collect.put(entry.getKey(), value + entry.getValue());
+	    	} else {
+	    		collect.put(entry.getKey(), entry.getValue());
+	    	}
+    	}
+    }
+	};
+
+	@Override
+	public void setup(OperatorContext context)
+	{
+	}
+
+	@Override
+	public void teardown()
+	{
+	}
+
+	@Override
+	public void beginWindow(long windowId)
+	{
+		collect  = new HashMap<String, Integer>();
+	}
+	
+	// out port
+	public final transient DefaultOutputPort<Map<String, Integer>> outport = new DefaultOutputPort<Map<String, Integer>>(this);
+	
+	@Override
+	public void endWindow()
+	{
+		outport.emit(collect);
+	}
+}
---
+++ library/src/main/java/com/datatorrent/lib/testbench/RedisSumOper.java
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2012 Malhar, Inc. All Rights Reserved.
+ */
+package com.datatorrent.lib.testbench;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.Operator;
+import com.datatorrent.api.Context.OperatorContext;
+
+/**
+ * @author Dinesh Prasad (dinesh@malhar-inc.com)
+ */
+public class RedisSumOper extends BaseOperator
+{
+	private ArrayList<Integer> collect;
+	public final transient DefaultInputPort<Integer> inport = new DefaultInputPort<Integer>(this) {
+	    @Override
+	    public void process(Integer s) {
+	      collect.add(s);
+	    }
+	};
+
+	@Override
+	public void setup(OperatorContext context)
+	{
+	}
+
+	@Override
+	public void teardown()
+	{
+	}
+
+	@Override
+	public void beginWindow(long windowId)
+	{
+		collect  = new ArrayList<Integer>();
+	}
+	
+	// out port
+	public final transient DefaultOutputPort<Map<Integer, Integer>> outport = new DefaultOutputPort<Map<Integer, Integer>>(this);
+	
+	@Override
+	public void endWindow()
+	{
+		Integer sum = 0;
+		for(Integer entry : collect) sum += entry;
+		Map<Integer, Integer> tuple = new HashMap<Integer, Integer>();
+		tuple.put(1, sum);
+		outport.emit(tuple);
+	}
+}
