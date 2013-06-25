5cdad8c69aa9b009cdb3a56d2c7979b7df4be953
Amol Kekre <amol@datatorrent.com>
2013-06-25 11:39:10 -0700

    updated javadoc

--- demos/src/main/java/com/datatorrent/demos/twitter/TwitterTopCounterApplication.java
+++ demos/src/main/java/com/datatorrent/demos/twitter/TwitterTopCounterApplication.java
@@ -15,30 +15,28 @@
  */
 package com.datatorrent.demos.twitter;
 
-import java.net.URI;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.conf.Configuration;
-
-import com.datatorrent.api.StreamingApplication;
 import com.datatorrent.api.DAG;
 import com.datatorrent.api.Operator.InputPort;
+import com.datatorrent.api.StreamingApplication;
 import com.datatorrent.lib.algo.UniqueCounter;
 import com.datatorrent.lib.algo.WindowedTopCounter;
 import com.datatorrent.lib.io.ConsoleOutputOperator;
 import com.datatorrent.lib.io.PubSubWebSocketOutputOperator;
+import java.net.URI;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
 
 /**
  * Twitter Demo Application: <br>
  * This demo application samples random public status from twitter, send to url
  * extractor. <br>
- * Top 10 url(s) mentioned in tweets in last 1 mins are displayed oin every
- * window count (500ms). <br>
+ * Top 10 url(s) mentioned in tweets in last 5 mins are displayed on every
+ * window count (500ms).<br>
  * <br>
- * 
- * Real Time Calculation : <br>
- * This application calculates top 10 url mentioned in tweets in last one
- * minute across random tweet sampling. <br>
+ *
+ * Real Time Calculation :<br>
+ * This application calculates top 10 url mentioned in tweets in last 5
+ * minutes across a 1% random tweet sampling on a rolling window basis.<br>
  * <br>
  * Before running this application, you need to create a Twitter API account
  * and configure the Twitter authentication.
@@ -67,20 +65,20 @@ import com.datatorrent.lib.io.PubSubWebSocketOutputOperator;
  * <b>topCounts operator : <b>
  * <ul>
  * <li>Top Count : 10, number of top unique url to be reported.</li>
- * <li>Sliding window count : 300 , report over last 5 min</li>
+ * <li>Sliding window count : 600, report over last 5 min (600 * .5 / 60 mins)</li>
  * <li>window slide value : 1</li>
  * </ul>
  * <p>
  * Running Java Test or Main app in IDE:
- * 
+ *
  * <pre>
  * LocalMode.runApp(new Application(), 600000); // 10 min run
  * </pre>
- * 
+ *
  * Run Success : <br>
  * For successful deployment and run, user should see following output on
  * console:
- * 
+ *
  * <pre>
  * topURLs: {http://goo.gl/V0R05=2, http://etsy.me/10r1Yg3=6, http://tinyurl.com/88b5jqb=2, http://www.justunfollow.com=4, http://fllwrs.com=2, http://goo.gl/a9Sjp=2, http://goo.gl/iKeVH=2, http://Unfollowers.me=7, http://freetexthost.com/j3y03la4g3=2, http://uranaitter.com=4}
  * topURLs: {http://goo.gl/V0R05=2, http://etsy.me/10r1Yg3=6, http://tinyurl.com/88b5jqb=2, http://www.justunfollow.com=4, http://fllwrs.com=2, http://goo.gl/a9Sjp=2, http://goo.gl/iKeVH=2, http://Unfollowers.me=7, http://freetexthost.com/j3y03la4g3=2, http://uranaitter.com=4}
@@ -92,46 +90,44 @@ import com.datatorrent.lib.io.PubSubWebSocketOutputOperator;
  * 2013-06-17 14:38:55,201 [main] INFO  stram.StramLocalCluster run - Application finished.
  * 2013-06-17 14:38:55,201 [container-2] INFO  stram.StramChild processHeartbeatResponse - Received shutdown request
  * </pre>
- * 
+ *
  * Scaling Options : <br>
  * User can scale application by setting intial partition size > 1 on count
  * unique operator. <br>
  * <br>
- * 
+ *
  * Application DAG : <br>
  * <img src="doc-files/Application.gif" width=600px > <br>
  * <br>
- * 
+ *
  * Streaming Window Size : 500ms(default) <br>
  * Operator Details : <br>
  * <ul>
  * <li><b>The twitterFeed operator : </b> This operator samples random public
  * statues from twitter and emits to application. <br>
  * Class : com.datatorrent.demos.twitter.TwitterSampleInput <br>
- * State Less : YES, window count 1 <br>
+ * StateFull : No, window count 1 <br>
  * </li>
  * <li><b>The urlExtractor operator : </b> This operator extracts url from
  * random sampled statues from twitter. <br>
  * Class : {@link com.datatorrent.demos.twitter.TwitterStatusURLExtractor} <br>
- * State Less : Yes, window count 1 <br>
+ * StateFull : No, window count 1 <br>
  * </li>
  * <li><b>The uniqueCounter operator : </b> This operator aggregates count for each
  * url extracted from random samples. <br>
  * Class : {@link com.datatorrent.lib.algo.UniqueCounter} <br>
- * State Less : YES, window count 1 <br>
+ * StateFull : No, window count 1 <br>
  * </li>
  * <li><b> The topCounts operator : </b> This operator caluculates top url in last 1
  * min sliding window count 1. <br>
  * Class : com.datatorrent.lib.algo.WindowedTopCounter <br>
- * State Full : Yes, sliding window count 120 (1 min) <br>
+ * StateFull : Yes, sliding window count 120 (1 min) <br>
  * </li>
  * <li><b>The operator Console: </b> This operator just outputs the input tuples
  * to the console (or stdout). <br>
- * if you need to change write to HDFS,HTTP .. instead of console, <br>
- * Please refer to {@link com.datatorrent.lib.io.HttpOutputOperator} or
- * {@link com.datatorrent.lib.io.HdfsOutputOperator}.</li>
+ * </li>
  * </ul>
- * 
+ *
  */
 public class TwitterTopCounterApplication implements StreamingApplication
 {
@@ -162,7 +158,7 @@ public class TwitterTopCounterApplication implements StreamingApplication
     // Setup the operator to get the data from twitter sample stream injected into the system.
     TwitterSampleInput twitterFeed = new TwitterSampleInput();
     twitterFeed = dag.addOperator("TweetSampler", twitterFeed);
-    
+
     //  Setup the operator to get the URLs extracted from the twitter statuses
     TwitterStatusURLExtractor urlExtractor = dag.addOperator("URLExtractor", TwitterStatusURLExtractor.class);
 
