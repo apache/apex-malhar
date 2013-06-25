a31bfbd0a981e88fce510dc1bc4f14e619a1c336
Pramod Immaneni <pramod@datatorrent.com>
2013-06-25 07:33:31 -0700

    Set tuples blast to 200.

--- contrib/src/main/java/com/datatorrent/contrib/summit/mobile/Application.java
+++ contrib/src/main/java/com/datatorrent/contrib/summit/mobile/Application.java
@@ -74,7 +74,7 @@ public class Application implements StreamingApplication
     RandomEventGenerator phones = dag.addOperator("phonegen", RandomEventGenerator.class);
     phones.setMinvalue(this.phoneRange.lowerEndpoint());
     phones.setMaxvalue(this.phoneRange.upperEndpoint());
-    phones.setTuplesBlast(1000);
+    phones.setTuplesBlast(200);
     phones.setTuplesBlastIntervalMillis(5);
     dag.setOutputPortAttribute(phones.integer_data, PortContext.QUEUE_CAPACITY, 32 * 1024);
 
