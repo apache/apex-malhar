53f716efe4cfa5aaf5f2429ac55f64fbbab88d6e
Dinesh Prasad <dinesh@datatorrent.com>
2013-07-03 10:58:21 -0700

    Change name to more logical name

---
+++ library/src/main/java/com/datatorrent/lib/math/AbstractOutports.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.math;
+
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
+
+/**
+ *  Abstract base operator defining optional double/float/long/integer output ports.  <br>
+ *  Duplicate port declaration in operators. <br>
+ */
+public abstract class AbstractOutports extends BaseOperator
+{
+	/**
+	 * Double type output.
+	 */
+	@OutputPortFieldAnnotation(name = "doubleResult", optional = true)
+	public final transient DefaultOutputPort<Double> doubleResult = new DefaultOutputPort<Double>();
+
+	/**
+	 * Float type output.
+	 */
+	@OutputPortFieldAnnotation(name = "floatResult", optional = true)
+	public final transient DefaultOutputPort<Float> floatResult = new DefaultOutputPort<Float>();
+
+	/**
+	 * Long type output.
+	 */
+	@OutputPortFieldAnnotation(name = "longResult", optional = true)
+	public final transient DefaultOutputPort<Long> longResult = new DefaultOutputPort<Long>();
+
+	/**
+	 * Integer type output.
+	 */
+	@OutputPortFieldAnnotation(name = "integerResult", optional = true)
+	public final transient DefaultOutputPort<Integer> integerResult = new DefaultOutputPort<Integer>();
+}
