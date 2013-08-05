e257daedac51f846e0953812be3963629962d8c5
dineshmalhar <dinesh@malhar-inc.com>
2013-08-05 16:56:39 -0700

    Fixing sql operators

---
+++ library/src/main/java/com/datatorrent/lib/streamquery/SumAggregate.java
@@ -0,0 +1,30 @@
+package com.datatorrent.lib.streamquery;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class SumAggregate extends AggregateIndex
+{
+  public SumAggregate(String column, String alias) throws Exception
+  {
+    super(column, alias);
+  }
+
+  @Override
+  public Object compute(ArrayList<Map<String, Object>> rows) throws Exception
+  {
+    Double result = 0.0;
+    for (Map<String, Object> row : rows) {
+        if (!row.containsKey(column)) continue;
+        result += ((Number)row.get(column)).doubleValue();
+    }
+    return result;
+  }
+
+  @Override
+  protected String aggregateName()
+  {
+   return "Sum(" + column;
+  }
+
+}
