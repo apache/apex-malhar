bdbc34ff6b7df12572ebeff78a8ebe8167248770
Dinesh Prasad <dinesh@datatorrent.com>
2013-08-08 14:05:16 -0700

    pig operators

---
+++ library/src/main/java/com/datatorrent/lib/pigquery/PigFilterOperator.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.pigquery;
+
+import java.util.Map;
+
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.lib.streamquery.condition.Condition;
+
+
+/**
+ * Class to implement Apache Pig filter statement semantic. <br>
+ * Stream rows passing condition are emitted on output port stream. <br>
+ * <br>
+ * <b>StateFull : NO,</b> all row data is processed in current time window. <br>
+ * <b>Partitions : Yes, </b> No Input dependency among input rows. <br>
+ * <br>
+ * <b>Ports</b>:<br>
+ * <b> inport : </b> Input hash map(row) port, expects
+ * HashMap&lt;String,Object&gt;<<br>
+ * <b> outport : </b> Output hash map(row) port, emits
+ * HashMap&lt;String,Object&gt;<br>
+ * <br>
+ * <b> Properties : <b> <br>
+ * <b> condition : </b> Select condition for selecting rows. <br>
+ */
+public class PigFilterOperator  extends BaseOperator
+{
+  /**
+   * condition.
+   */
+  private Condition condition = null;
+
+  /**
+   * Get value for condition.
+   * @return Condition
+   */
+  public Condition getCondition()
+  {
+    return condition;
+  }
+
+  /**
+   * Set value for condition.
+   * @param condition set value for condition.
+   */
+  public void setCondition(Condition condition)
+  {
+    this.condition = condition;
+  }
+  
+  /**
+   * Input port.
+   */
+  public final transient DefaultInputPort<Map<String, Object>> inport = new DefaultInputPort<Map<String, Object>>()
+  {
+
+    @Override
+    public void process(Map<String, Object> tuple)
+    {
+      if ((condition != null) && (!condition.isValidRow(tuple))) return;
+      outport.emit(tuple);
+    }
+  };
+  
+  /**
+   * Output port.
+   */
+  public final transient DefaultOutputPort<Map<String, Object>> outport = new DefaultOutputPort<Map<String, Object>>();
+}
---
+++ library/src/main/java/com/datatorrent/lib/pigquery/PigGroupOperator.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.pigquery;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.constraints.NotNull;
+
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.lib.util.UnifierMap;
+
+
+/**
+ * Class implements Apache Pig operator sematic. <br>
+ * 
+ * <pre>
+ * B = GROUP A BY age;
+ * 
+ * DESCRIBE B;
+ * B: {group: int, A: {name: chararray,age: int,gpa: float}}
+ * 
+ * ILLUSTRATE B;
+ * etc ... 
+ * ----------------------------------------------------------------------
+ * | B     | group: int | A: bag({name: chararray,age: int,gpa: float}) |
+ * ----------------------------------------------------------------------
+ * |       | 18         | {(John, 18, 4.0), (Joe, 18, 3.8)}             |
+ * |       | 20         | {(Bill, 20, 3.9)}                             |
+ * ----------------------------------------------------------------------
+ * 
+ * DUMP B;
+ * (18,{(John,18,4.0F),(Joe,18,3.8F)})
+ * (19,{(Mary,19,3.8F)})
+ * (20,{(Bill,20,3.9F)})
+ * </pre>
+ * 
+ * 
+ */
+public class PigGroupOperator  extends BaseOperator
+{
+  /**
+   * Aggregate tuple list.
+   */
+  private ArrayList<Map<String, Object>> tuples;
+  
+  /**
+   * Group by condition.
+   */
+  @NotNull
+  private GroupByCondition groupByCondition;
+  
+  public PigGroupOperator(@NotNull GroupByCondition groupByCondition) {
+    this.groupByCondition = groupByCondition;
+  }
+  
+  /**
+   * Input port.
+   */
+  public final transient DefaultInputPort<Map<String, Object>> inport = new DefaultInputPort<Map<String, Object>>()
+  {
+    @Override
+    public void process(Map<String, Object> tuple)
+    {
+      tuples.add(tuple);
+    }
+  };
+  
+  /**
+   * Output port.
+   */
+  public final transient DefaultOutputPort<Map<Object, List<Map<String, Object>>>> outport = 
+      new DefaultOutputPort<Map<Object, List<Map<String, Object>>>>()
+  {
+    @Override
+    public Unifier<Map<Object, List<Map<String, Object>>>> getUnifier()
+    {
+      return new UnifierMap<Object, List<Map<String, Object>>>();
+    }
+  };
+
+  /**
+   * Get value for groupByCondition.
+   * @return GroupByCondition
+   */
+  public GroupByCondition getGroupByCondition()
+  {
+    return groupByCondition;
+  }
+
+  /**
+   * Set value for groupByCondition.
+   * @param groupByCondition set value for groupByCondition.
+   */
+  public void setGroupByCondition(@NotNull GroupByCondition groupByCondition)
+  {
+    this.groupByCondition = groupByCondition;
+  }
+  
+  @Override
+  public void beginWindow(long arg0)
+  {
+    tuples = new ArrayList<Map<String, Object>>();
+  }
+
+  @Override
+  public void endWindow()
+  {
+    Map<Object, List<Map<String, Object>>> result = new HashMap<Object, List<Map<String, Object>>>();
+    for (Map<String, Object> tuple : tuples) {
+      Object key = groupByCondition.compute(tuple);
+      List<Map<String, Object>> list;
+      if (result.containsKey(key)) {
+        list = result.get(key);
+      } else {
+        list = new ArrayList<Map<String, Object>>();
+        result.put(key, list);
+      }
+      list.add(tuple);
+    }
+    outport.emit(result);
+  }
+}
