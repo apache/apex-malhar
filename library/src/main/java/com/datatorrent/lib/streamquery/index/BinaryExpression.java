ea55bc6eca6b888d0a359db43e124c2f39bc0831
Dinesh Prasad <dinesh@datatorrent.com>
2013-08-07 17:32:34 -0700

    Adding advanced sql operation

---
+++ library/src/main/java/com/datatorrent/lib/streamquery/index/BinaryExpression.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.streamquery.index;
+
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * Abstract class to filter row by expression index.
+ * Sub class will implement filter/getExpressionName functions.
+ */
+abstract public class BinaryExpression  implements Index
+{
+  /**
+   * Left column name argument for expression.
+   */
+  @NotNull
+  protected String left;
+  
+  /**
+   * Right column name argument for expression.
+   */
+  @NotNull
+  protected String right;
+  
+  /**
+   *  Alias name for output field.
+   */
+  protected String alias;
+
+  /**
+   * @param Left column name argument for expression.
+   * @param Right column name argument for expression.
+   * @param Alias name for output field.
+   */
+  public BinaryExpression(@NotNull String left, @NotNull String right, String alias) 
+  {
+    this.left = left;
+    this.right = right;
+  }
+
+  public String getAlias()
+  {
+    return alias;
+  }
+
+  public void setAlias(String alias)
+  {
+    this.alias = alias;
+  }
+}
---
+++ library/src/main/java/com/datatorrent/lib/streamquery/index/UnaryExpression.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.streamquery.index;
+
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * Abstract class to filter row by expression index.
+ * Sub class will implement filter/getExpressionName functions.
+ */
+abstract public class UnaryExpression  implements Index
+{
+  /**
+   * Column name argument for unary expression.
+   */
+  @NotNull
+  protected String column;
+  
+  /**
+   *  Alias name for output field.
+   */
+  protected String alias;
+
+  /**
+   * @param Column name argument for unary expression.
+   * @param Alias name for output field.
+   */
+  public UnaryExpression(@NotNull String column, String alias) 
+  {
+    this.column = column;
+  }
+
+  public String getColumn()
+  {
+    return column;
+  }
+
+  public void setColumn(String column)
+  {
+    this.column = column;
+  }
+
+  public String getAlias()
+  {
+    return alias;
+  }
+
+  public void setAlias(String alias)
+  {
+    this.alias = alias;
+  }
+}
