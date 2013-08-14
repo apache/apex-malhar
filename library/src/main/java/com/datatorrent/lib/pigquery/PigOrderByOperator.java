1b31ced5dc9ffea29caebbdf7581a5f5fdb96e93
Dinesh Prasad <dinesh@datatorrent.com>
2013-08-14 14:03:47 -0700

    Added pig latin operator semantic

---
+++ library/src/main/java/com/datatorrent/lib/pigquery/PigOrderByOperator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.pigquery;
+
+/**
+ * This operator implements pig order by semantic on live steam.  <br>
+ * This semantic same as sql stream query  order by  operator. <br>
+ * Please use operator  {@link com.datatorrent.lib.streamquery.OrderByOperator}
+ */
+public class PigOrderByOperator 
+{
+  // Must not be used.
+  private PigOrderByOperator()
+  {
+    assert(false);
+  }
+}
---
+++ library/src/main/java/com/datatorrent/lib/pigquery/PigSampleOperator.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.pigquery;
+
+
+/**
+ * This operator semantic is same as   select top operator in stream query library. <br>
+ * Operator must be used with percentage flag on. <br>
+ * Please use operator : {@link com.datatorrent.lib.streamquery.SelectTopOperator}.
+ */
+public class PigSampleOperator
+{
+  // Must not be used.
+  private PigSampleOperator() 
+  {
+    assert(false);
+  }
+}
