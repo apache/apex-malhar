187901967db1a1604e872aa65b85df49e562da83
nickholub <nikholub@gmail.com>
2013-09-09 11:09:17 -0700

    #22 Node.js back-end for Ads Dimensions Demo

---
+++ contrib/src/main/apps/node-redis-ads-dimensions/app.js
@@ -0,0 +1,74 @@
+var async = require('async');
+var express = require('express');
+var redis = require("redis");
+var dateFormat = require('dateformat');
+var config = require('./config');
+
+var app = express();
+var client = redis.createClient(config.redis.port, config.redis.host);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/data', getData);
+
+function getData(req, res, next) {
+    getMinutes(req.query, function(err, result) {
+        res.json(result);
+    });
+}
+
+function getMinutes(query, resCallback) {
+    var lookbackMinutes = query.lookbackMinutes;
+    var endTime = query.endTime;
+    var publisher = query.publisher;
+    var advertiser = query.advertiser;
+    var adunit = query.adunit;
+
+    if (!endTime) {
+        endTime = Date.now();
+    }
+
+    var keyTemplate = 'm|$date';
+    if (publisher) keyTemplate += '|0:' + publisher;
+    if (advertiser) keyTemplate += '|1:' + advertiser;
+    if (adunit) keyTemplate += '|2:' + adunit;
+
+    //console.log('getData ' + keyTemplate);
+
+    var minute = (60 * 1000);
+    var result = [];
+    var time = endTime - lookbackMinutes * minute;
+
+    async.whilst(
+        function () { return time <= endTime; },
+        function (callback) {
+            var date = dateFormat(time, 'UTC:yyyymmddHHMM');
+            var key = keyTemplate
+                .replace('$date', date);
+            console.log('key ' + key);
+            client.hgetall(key, function(err, hash) {
+                //console.log(hash)
+                if (hash) {
+                    var minuteItem = {
+                        timestamp: time,
+                        keyPattern: keyTemplate,
+                        cost: parseFloat(hash[1]),
+                        revenue: parseFloat(hash[2]),
+                        impressions: parseFloat(hash[3]),
+                        clicks: parseFloat(hash[4])
+                    }
+                    result.push(minuteItem);
+                }
+                callback();
+            });
+
+            time += minute;
+        },
+        function (err) {
+            resCallback(err, result);
+        }
+    );
+}
+
+app.listen(3000);
+console.log('Express started on port 3000');
---
+++ contrib/src/main/apps/node-redis-ads-dimensions/config.js
@@ -0,0 +1,7 @@
+var config = {};
+config.redis = {};
+
+config.redis.host = 'localhost';
+config.redis.port = 6379;
+
+module.exports = config
