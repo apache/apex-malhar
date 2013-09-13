1cc745d2967f5b2a3529ad81b7a0711fc7dd6fdf
nickholub <nikholub@gmail.com>
2013-09-13 14:57:49 -0700

    #34 Move Twitter, Mobile and Ads Demo to Node.js

--- webdemos/config.js
+++ webdemos/config.js
@@ -5,8 +5,8 @@ config.web = {};
 config.redis = {};
 
 config.web.port = process.env.PORT || 3000;
-config.web.webSocketUrl = null;
-config.redis.host = null;
-config.redis.port = null;
+config.web.webSocketUrl = null; // 'ws://localhost:9090/pubsub' for local Daemon
+config.redis.host = null; // 'localhost' for local Redis server
+config.redis.port = null; // 6379 is default Redis port
 
 module.exports = config
