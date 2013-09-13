515b46e99e76c2f8d631aca20cc1724541beb20d
nickholub <nikholub@gmail.com>
2013-09-13 12:01:20 -0700

    #34 Move Twitter, Mobile and Ads Demo to Node.js

---
+++ webdemos/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var fs = require('fs');
+var config = require('./config');
+
+var main = require('./routes/index');
+var twitter = require('./routes/twitter');
+var mobile = require('./routes/mobile');
+var ads = require('./routes/ads');
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', main.index);
+app.get('/twitter', twitter.index);
+app.get('/mobile', mobile.index);
+app.get('/ads', ads.index);
+app.get('/ads/data', ads.data);
+
+app.listen(config.web.port, function() {
+    console.log('Node.js server started on port ' + config.web.port);
+});
