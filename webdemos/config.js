4811a98c4b61447089f1280084f6b5ae23736c00
nickholub <nikholub@gmail.com>
2013-09-16 16:41:49 -0700

    #45 Move SiteOps Demo to Node.js

--- webdemos/config.js
+++ webdemos/config.js
@@ -3,10 +3,14 @@ var path = require('path');
 var config = {};
 config.web = {};
 config.redis = {};
+config.siteops = {};
+config.siteops.redis = {};
 
 config.web.port = process.env.PORT || 3000;
 config.web.webSocketUrl = null; // 'ws://localhost:9090/pubsub' for local Daemon
 config.redis.host = null; // 'localhost' for local Redis server
 config.redis.port = null; // 6379 is default Redis port
+config.siteops.redis.host = null; // 'localhost' for local Redis server
+config.siteops.redis.port = null; // 6379 is default Redis port
 
 module.exports = config
