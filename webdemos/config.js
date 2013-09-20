557a6e741eacecce07f4a787ad646eaa973a58f4
nickholub <nikholub@gmail.com>
2013-09-19 17:43:34 -0700

    #65 Ads Dimensions Demo - Redis Configuration

--- webdemos/config.js
+++ webdemos/config.js
@@ -3,13 +3,15 @@ var path = require('path');
 var config = {};
 config.web = {};
 config.redis = {};
+config.adsdimensions = {};
+config.adsdimensions.redis = {};
 config.siteops = {};
 config.siteops.redis = {};
 
 config.web.port = process.env.PORT || 3000;
 config.web.webSocketUrl = null; // 'ws://localhost:9090/pubsub' for local Daemon
-config.redis.host = null; // 'localhost' for local Redis server
-config.redis.port = null; // 6379 is default Redis port
+config.adsdimensions.redis.host = null; // 'localhost' for local Redis server
+config.adsdimensions.redis.port = null; // 6379 is default Redis port
 config.siteops.redis.host = null; // 'localhost' for local Redis server
 config.siteops.redis.port = null; // 6379 is default Redis port
 
