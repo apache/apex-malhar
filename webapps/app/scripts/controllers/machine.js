a7a70a334ea8c3a0b1b71bc6ea2d354a18ee0ee0
Nick Holub <nikholub@gmail.com>
2013-10-31 17:08:18 -0700

    fix #235, fix #236, fix #237, fix #238 instant reload, load indicator, server polling stat, empty chart options

--- webapps/app/scripts/controllers/machine.js
+++ webapps/app/scripts/controllers/machine.js
@@ -24,6 +24,19 @@ var chartOptions = {
   chartArea: { top: 20, height: 240 }
 };
 
+function getEmptyChartOptionsFn(scope) {
+  return function () {
+    var now = Date.now();
+    var max = new Date(now);
+    var min = new Date(now - scope.lookback * 60 * 1000);
+    var options = jQuery.extend({}, chartOptions, {
+      vAxis: { minValue: 0, maxValue: 100 },
+      hAxis: { viewWindow: { min: min, max: max }}
+    });
+    return options;
+  };
+}
+
 function chartData(data, property) {
   return _.map(data, function (obj) {
     return {
@@ -33,9 +46,104 @@ function chartData(data, property) {
   });
 }
 
+function getRequestParams($scope) {
+  return {
+    customer: $scope.customer.value,
+    product: $scope.product.value,
+    os: $scope.os.value,
+    software1: $scope.software1.value,
+    software2: $scope.software2.value,
+    deviceId: $scope.deviceId.value,
+    lookback: $scope.lookback
+  };
+}
+
+function PollRequest (rest, scope, callback) {
+  this.rest = rest;
+  this.scope = scope;
+  this.max = scope.lookback;
+  this.dataCache = null;
+  this.callback = callback;
+  this.cancelled = false;
+  this.interval = null;
+  this.timeout = null;
+  this.params = getRequestParams(this.scope);
+}
+
+PollRequest.prototype = {
+  cancel: function () {
+    this.cancelled = true;
+    this.scope.requestProgress = 0;
+    clearInterval(this.interval);
+    clearTimeout(this.timeout);
+  },
+
+  responseStatus: function () {
+    this.scope.requestProgress = Math.round((Date.now() - this.requestStartTime) / 1000);
+    this.scope.$apply();
+  },
+
+  fetchMachineData: function () {
+    if (this.dataCache && this.dataCache.length) {
+      this.params.lastTimestamp = _.last(this.dataCache).timestamp;
+    }
+
+    var max = this.max;
+
+    this.requestStartTime = Date.now();
+
+    this.interval = setInterval(this.responseStatus.bind(this), 250);
+
+    var that = this;
+
+    this.rest.getMachineData(this.params).then(function (response) {
+      if (that.cancelled) {
+        return;
+      }
+
+      that.scope.response = response;
+      var minutes = response.minutes;
+      that.scope.requestProgress = 0;
+      clearInterval(that.interval);
+
+      var now = Date.now();
+      that.scope.lastResponse = new Date(now);
+      that.scope.responseTime = now - that.requestStartTime;
+      if (!that.dataCache) {
+        that.dataCache = minutes;
+
+        that.scope.minutesCached = 0;
+        that.scope.minutesReceived = minutes.length;
+
+      } else if (minutes.length > 0) {
+        that.scope.minutesCached = that.dataCache.length;
+        that.scope.minutesReceived = minutes.length;
+
+        that.dataCache.pop(); // remove last element since response should have new values for the last minute
+
+        var newlength = that.dataCache.length + minutes.length;
+
+        if (newlength > max) {
+          that.dataCache.splice(0, newlength - max);
+        }
+        that.dataCache.push.apply(that.dataCache, minutes); // add all elements
+      }
+
+      that.callback(that.dataCache);
+
+      var nextTimeout = settings.machine.pollInterval - (Date.now() - that.requestStartTime);
+      nextTimeout = Math.max(0, nextTimeout);
+
+      that.timeout = setTimeout(that.fetchMachineData.bind(that), nextTimeout);
+    });
+  }
+};
+
+
 angular.module('machine')
   .controller('MachineController', ['$scope', '$timeout', '$location', '$routeParams', 'rest', function ($scope, $timeout, $location, $routeParams, rest) {
     var queryParams = new URI(window.location.href).query(true);
+    var emptyChartOptionsFn = getEmptyChartOptionsFn($scope);
 
     $scope.app = rest.getApp(settings.machine.appName);
     $scope.$watch('app', function (app) {
@@ -87,26 +195,9 @@ angular.module('machine')
     setupSelect('deviceId', 'Device ID');
     $scope.lookback = queryParams.lookback ? parseInt(queryParams.lookback, 10) : settings.machine.lookback;
 
-    function getParams() {
-      return {
-        customer: $scope.customer.value,
-        product: $scope.product.value,
-        os: $scope.os.value,
-        software1: $scope.software1.value,
-        software2: $scope.software2.value,
-        deviceId: $scope.deviceId.value,
-        lookback: $scope.lookback
-      };
-    }
-
     $scope.reload = function () {
-      //$location.path('/home/2/customer/5');
-      //console.log(window.location);
-      //TODO
-      window.location.href = window.location.pathname + '?' + jQuery.param(getParams());
-
-      //window.location.href = window.location.pathname + '#/?customer=' + $scope.customer.value
-      //    + '&product=' + $scope.product.value;
+      //TODO have Angular route instead of reloading the page
+      window.location.href = window.location.pathname + '?' + jQuery.param(getRequestParams($scope));
     };
 
     function updateCharts(data) {
@@ -115,56 +206,60 @@ angular.module('machine')
         $scope.cpu = parseFloat(current.cpu);
         $scope.ram = parseFloat(current.ram);
         $scope.hdd = parseFloat(current.hdd);
-
-        $scope.cpuChart = {
-          data: chartData(data, 'cpu'),
-          options: chartOptions
-        };
-        $scope.ramChart = {
-          data: chartData(data, 'ram'),
-          options: chartOptions
-        };
-        $scope.hddChart = {
-          data: chartData(data, 'hdd'),
-          options: chartOptions
-        };
       }
+      $scope.cpuChart = {
+        data: chartData(data, 'cpu'),
+        options: chartOptions,
+        emptyChartOptions: emptyChartOptionsFn
+      };
+      $scope.ramChart = {
+        data: chartData(data, 'ram'),
+        options: chartOptions,
+        emptyChartOptions: emptyChartOptionsFn
+      };
+      $scope.hddChart = {
+        data: chartData(data, 'hdd'),
+        options: chartOptions,
+        emptyChartOptions: emptyChartOptionsFn
+      };
     }
 
-    var dataCache;
+    $scope.cpuChart = {
+      emptyChartOptions: emptyChartOptionsFn
+    };
+    $scope.ramChart = {
+      emptyChartOptions: emptyChartOptionsFn
+    };
+    $scope.hddChart = {
+      emptyChartOptions: emptyChartOptionsFn
+    };
 
-    function fetchMachineData() {
-      var params = getParams();
-
-      if (dataCache && dataCache.length) {
-        params.lastTimestamp = _.last(dataCache).timestamp;
+    var request = null;
+    function reloadCharts() {
+      //TODO check if lookback is valid
+      if (request) {
+        request.cancel();
       }
-
-      var max = $scope.lookback;
-
-      rest.getMachineData(params).then(function (response) {
-        if (!dataCache) {
-          dataCache = response;
-        } else if (response.length > 0) {
-          //console.log(dataCache.length + ' ' + response.length);
-
-          dataCache.pop(); // remove last element since response should have new values for the last minute
-
-          var newlength = dataCache.length + response.length;
-
-          if (newlength > max) {
-            dataCache.splice(0, newlength - max);
-          }
-          dataCache.push.apply(dataCache, response); // add all elements
-        }
-
-        updateCharts(dataCache);
-
-        $timeout(fetchMachineData, 1000);
-      });
+      request = new PollRequest(rest, $scope, updateCharts);
+      request.fetchMachineData();
     }
 
-    fetchMachineData();
+    $scope.$watch('[customer, product, os, software1, software1, deviceId]', function () {
+      reloadCharts();
+    }, true);
+
+    var lookbackTimeout;
+
+    var firstUpdate = true;
+
+    $scope.$watch('lookback', function () {
+      if (!firstUpdate) { // skip first change since there is a watch for select fields
+        clearTimeout(lookbackTimeout);
+        lookbackTimeout = setTimeout(reloadCharts, 500);
+      } else {
+        firstUpdate = false;
+      }
+    });
   }]);
 
 })();
--- webapps/config.js
+++ webapps/config.js
@@ -12,6 +12,7 @@ config.daemon.port = 3490;
 config.machine.redis.host = 'localhost';
 config.machine.redis.port = 9379;
 config.machine.redis.dbIndex = 2;
+//config.machine.forcedDelay = 5000; // for testing purpose only
 config.fraud.mongo.host = 'localhost';
 config.fraud.mongo.port = 27017;
 module.exports = config
