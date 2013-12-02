40379402b9250862fb872baed848571a3dfc023c
Chetan Narsude <chetan@datatorrent.com>
2013-12-02 13:08:51 -0800

    Gaurav, can you document this interface?

--- flume/src/main/java/com/datatorrent/storage/Storage.java
+++ flume/src/main/java/com/datatorrent/storage/Storage.java
@@ -2,32 +2,41 @@ package com.datatorrent.storage;
 
 public interface Storage
 {
-  
-  
   /**
    * This stores the bytes and returns the unique identifier to retrieve these bytes
+   *
    * @param bytes
    * @return
    */
   public long store(byte[] bytes);
-  
+
   /**
    * This returns the bytes identified by the identifier
+   *
+   * @param identifier
    * @return
    */
   public RetrievalObject retrieve(long identifier);
-  
+
   /**
-   * This returns the the bytes 
+   * This returns the the bytes
+   *
    * @return
    */
   public RetrievalObject retrieveNext();
-  
+
   /**
-   * This is used to clean up of the bytes identified by identifier 
+   * This is used to clean up of the bytes identified by identifier
+   *
+   * @param identifier
+   * @return
    */
   public boolean clean(long identifier);
-  
+
+  /**
+   *
+   * @return
+   */
   public boolean flush();
 
 }
