c471191f239e31a3bfc9529f73a50364b797bd26
Chetan Narsude <chetan@datatorrent.com>
2013-12-04 11:21:50 -0800

    Implement example of Configurable through DiskStorage.

--- flume/src/test/java/com/datatorrent/storage/DiskStorageTest.java
+++ flume/src/test/java/com/datatorrent/storage/DiskStorageTest.java
@@ -2,6 +2,7 @@ package com.datatorrent.storage;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import org.apache.flume.Context;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -10,26 +11,30 @@ import org.slf4j.LoggerFactory;
 public class DiskStorageTest
 {
   public static final String STORAGE_DIRECTORY = "target/testdata/local";
+
   @Test
   public void testStorage() throws IOException
   {
-    Storage storage = DiskStorage.getInstance(STORAGE_DIRECTORY, true);
-    if (storage == null) {
-      return;
-    }
+    Context context = new Context();
+    context.put("baseDir", STORAGE_DIRECTORY);
+    context.put("restore", Boolean.toString(false));
+
+    DiskStorage storage = new DiskStorage();
+    storage.configure(context);
     RandomAccessFile r = new RandomAccessFile("src/test/resources/TestInput.txt", "r");
     r.seek(0);
     storage.store(r.readLine().getBytes());
-    //logger.debug(new String(storage.retrieveNext()));
   }
 
   @Test
   public void testCleanup() throws IOException
   {
-    Storage storage = DiskStorage.getInstance(STORAGE_DIRECTORY, false);
-    if (storage == null) {
-      return;
-    }
+    Context context = new Context();
+    context.put("baseDir", STORAGE_DIRECTORY);
+    context.put("restore", Boolean.toString(false));
+
+    DiskStorage storage = new DiskStorage();
+    storage.configure(context);
     storage.clean(1);
   }
 
