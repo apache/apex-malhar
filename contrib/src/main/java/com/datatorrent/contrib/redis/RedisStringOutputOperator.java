0042b0775da29cc32724886bb1e3ead619041548
gaurav <gaurav@datatorrent.com>
2013-12-09 11:08:38 -0800

    added license and comments

--- contrib/src/main/java/com/datatorrent/contrib/redis/RedisStringOutputOperator.java
+++ contrib/src/main/java/com/datatorrent/contrib/redis/RedisStringOutputOperator.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.contrib.redis;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+/**
+ * <p>
+ * RedisStringOutputOperator class.
+ * This class is used to optimized for writing Map<String,String> to Redis in a single set call instead of different set call for each of the keys in map 
+ * </p>
+ *
+ * @since 0.3.2
+ */
 public class RedisStringOutputOperator<K> extends RedisOutputOperator<K,Map<String,String>>
 {
 
@@ -14,18 +35,7 @@ public class RedisStringOutputOperator<K> extends RedisOutputOperator<K,Map<Stri
       Object value = entry.getValue();
       if (value instanceof Map) {        
         redisConnection.hmset(entry.getKey().toString(), (Map) value);
-      } else if (value instanceof Set) {
-        for (Object o : (Set) value) {
-          redisConnection.sadd(entry.getKey().toString(), o.toString());
-        }
-      } else if (value instanceof List) {
-        int i = 0;
-        for (Object o : (List) value) {
-          redisConnection.lset(entry.getKey().toString(), i++, o.toString());
-        }
-      } else {
-        redisConnection.set(entry.getKey().toString(), value.toString());
-      }
+      } 
       if (keyExpiryTime != -1) {
         redisConnection.expire(entry.getKey().toString(), keyExpiryTime);
       }
