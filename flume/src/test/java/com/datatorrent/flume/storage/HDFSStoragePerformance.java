9302ad4e245a1bdd251053f0f352d1f2a5a1f048
gaurav <gaurav@datatorrent.com>
2013-12-09 16:49:53 -0800

    added logging

--- flume/src/test/java/com/datatorrent/flume/storage/HDFSStoragePerformance.java
+++ flume/src/test/java/com/datatorrent/flume/storage/HDFSStoragePerformance.java
@@ -2,11 +2,15 @@ package com.datatorrent.flume.storage;
 
 import org.apache.flume.Context;
 import org.apache.flume.conf.Configurable;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HDFSStoragePerformance
 {
 
-  public static void main(String[] args)
+  @Test
+  public void testPerformance()
   {
     Context ctx = new Context();
     ctx.put(HDFSStorage.BASE_DIR_KEY, ".");
@@ -14,9 +18,13 @@ public class HDFSStoragePerformance
     Storage storage = new HDFSStorage();
     ((Configurable) storage).configure(ctx);
 
+    logger.debug(" start time {}",System.currentTimeMillis());
     byte[] b = new byte[1024];
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < 1000000; i++)
       storage.store(b);
-
+    storage.close();
+    logger.debug(" end time {}",System.currentTimeMillis());
   }
+  
+  private static final Logger logger = LoggerFactory.getLogger(HDFSStoragePerformance.class);
 }
