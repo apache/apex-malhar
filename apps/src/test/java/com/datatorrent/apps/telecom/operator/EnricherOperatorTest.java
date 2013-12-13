fc3348fbb6bb0e8b77e2a10557e448261d33e5f0
gaurav <gaurav@datatorrent.com>
2013-12-13 11:45:31 -0800

    #426 : added license

--- apps/src/test/java/com/datatorrent/apps/telecom/operator/EnricherOperatorTest.java
+++ apps/src/test/java/com/datatorrent/apps/telecom/operator/EnricherOperatorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.apps.telecom.operator;
 
 import java.util.HashMap;
@@ -12,6 +27,10 @@ import org.slf4j.LoggerFactory;
 
 import com.datatorrent.lib.testbench.CollectorTestSink;
 
+/**
+*
+* @since 0.9.2
+*/
 public class EnricherOperatorTest
 {
   public static class TestEnricher implements EnricherInterface{
@@ -52,6 +71,7 @@ public class EnricherOperatorTest
     Assert.assertEquals("number emitted tuples", 1, sortSink.collectedTuples.size());
     for (Object o : sortSink.collectedTuples) {
       Assert.assertEquals("{prop2=b, prop1=a}", o.toString());
+      logger.debug(o.toString());
     }
   }
 }
