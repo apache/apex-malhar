847220741c8f73eaca0e7dea8f27c3a7d2cab61f
David Yan <david@datatorrent.com>
2013-12-13 16:50:26 -0800

    Squashed commit of the following:
    commit 54c57a73c2b5814ccfd33c7447323409a16b48b7
    Author: David Yan <david@datatorrent.com>
    Date:   Fri Dec 13 16:49:56 2013 -0800
    
        added javadoc
    
    commit f7050508c0743b9a6e5e2024cffc04e897e71746
    Merge: 6c7a054 689796b
    Author: David Yan <david@datatorrent.com>
    Date:   Fri Dec 13 16:48:53 2013 -0800
    
        Merge branch 'issue488_pull' into issue488_pull2
    
    commit 689796bb8e4a89b4b6eb8187957fbc3b49e8edca
    Author: David Yan <david@datatorrent.com>
    Date:   Fri Dec 13 15:33:41 2013 -0800
    
        fix #488 added delay before reconnection

--- library/src/main/java/com/datatorrent/lib/io/WebSocketOutputOperator.java
+++ library/src/main/java/com/datatorrent/lib/io/WebSocketOutputOperator.java
@@ -50,10 +50,6 @@ import org.slf4j.LoggerFactory;
 public class WebSocketOutputOperator<T> extends BaseOperator
 {
   private static final Logger LOG = LoggerFactory.getLogger(WebSocketOutputOperator.class);
-  /**
-   * Timeout interval for reading from server. 0 or negative indicates no timeout.
-   */
-  public int readTimeoutMillis = 0;
   @NotNull
   private URI uri;
   private transient AsyncHttpClient client;
@@ -61,24 +57,85 @@ public class WebSocketOutputOperator<T> extends BaseOperator
   protected transient final ObjectMapper mapper = new ObjectMapper(jsonFactory);
   protected transient WebSocket connection;
   private int ioThreadMultiplier = 1;
-  private int numRetries = 2;
+  private int numRetries = 3;
+  private int waitMillisRetry = 5000;
   private final transient AsyncHttpClientConfigBean config = new AsyncHttpClientConfigBean();
 
+  /**
+   * Gets the URI for WebSocket connection
+   * @return the URI
+   */
+  public URI getUri()
+  {
+    return uri;
+  }
+
+  /**
+   * Sets the URI for WebSocket connection
+   * @param uri
+   */
   public void setUri(URI uri)
   {
     this.uri = uri;
   }
 
+  /**
+   * Gets the milliseconds to wait before retrying connection
+   * @return wait in milliseconds
+   */
+  public int getWaitMillisRetry()
+  {
+    return waitMillisRetry;
+  }
+
+  /**
+   * Sets the milliseconds to wait before retrying connection
+   * @param waitMillisRetry
+   */
+  public void setWaitMillisRetry(int waitMillisRetry)
+  {
+    this.waitMillisRetry = waitMillisRetry;
+  }
+
+  /**
+   * Gets the IO Thread multiplier for AsyncWebSocket connection
+   * @return the IO thread multiplier
+   */
+  public int getIoThreadMultiplier()
+  {
+    return ioThreadMultiplier;
+  }
+
+  /**
+   * Sets the IO Thread multiplier for AsyncWebSocket connection
+   * @param ioThreadMultiplier
+   */
   public void setIoThreadMultiplier(int ioThreadMultiplier)
   {
     this.ioThreadMultiplier = ioThreadMultiplier;
   }
 
+  /**
+   * Gets the number of retries of connection before the giving up
+   * @return the number of retries
+   */
+  public int getNumRetries()
+  {
+    return numRetries;
+  }
+
+  /**
+   * Sets the number of retries of connection before the giving up
+   * @param numRetries
+   */
   public void setNumRetries(int numRetries)
   {
     this.numRetries = numRetries;
   }
 
+  /**
+   * The input port
+   */
   public final transient DefaultInputPort<T> input = new DefaultInputPort<T>()
   {
     @Override
@@ -100,6 +157,13 @@ public class WebSocketOutputOperator<T> extends BaseOperator
             LOG.debug("Caught exception", ex);
             LOG.warn("Send message failed ({}). Retrying ({}).", ex.getMessage(), countTries);
             connection.close();
+            if (waitMillisRetry > 0) {
+              try {
+                Thread.sleep(waitMillisRetry);
+              }
+              catch (InterruptedException ex1) {
+              }
+            }
           }
           else {
             throw new RuntimeException(ex);
