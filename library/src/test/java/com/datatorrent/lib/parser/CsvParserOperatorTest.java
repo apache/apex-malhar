f2b7eb50aca25e225a9faa61933bef815a41080d
Siyuan Hua <siyuan@datatorrent.com>
2013-12-18 11:01:01 -0800

    Fix Github #545

--- library/src/test/java/com/datatorrent/lib/parser/CsvParserOperatorTest.java
+++ library/src/test/java/com/datatorrent/lib/parser/CsvParserOperatorTest.java
@@ -15,6 +15,7 @@
  */
 package com.datatorrent.lib.parser;
 
+import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Map;
@@ -31,14 +32,18 @@ import com.datatorrent.lib.testbench.CollectorTestSink;
 import com.google.common.collect.Sets;
 
 /**
- * <p>Unit test for CsvParserOperator</p>
- *
+ * <p>
+ * Unit test for CsvParserOperator
+ * </p>
+ * 
  * @since 0.9.2
  */
 public class CsvParserOperatorTest
 {
-  
-  static String[] header = new String[]{"name", "accessible", "birth"};
+
+  static String[] header = new String[] { "name", "accessible", "birth" };
+
+  @SuppressWarnings("unchecked")
   @Test
   public void testOperator() throws ParseException
   {
@@ -51,30 +56,39 @@ public class CsvParserOperatorTest
     
     oper.beginWindow(0);
     
-    String testCsvString = "\"Doctor Who\",\"true\",\"1983-11-06\"\n \"Doctor WhoII\",\"FALSE\",\"1966-10-20\"";
+    String testCsvString = "\"Doctor Who\",\"true\",\"1983-11-06\"\n \"Doctor WhoII\",\"FALSE\",\"1966-10-20\"\n";
     
     oper.stringInput.process(testCsvString);
-    
+    String testCsvString2 = "\"Doctor WhoIII\",\"false\",\"1983-06-30\" \n\"Doctor WhoIV\",\"FALSE\",\"1966-10-20\"";
+    oper.stringInput.process(testCsvString2);
     oper.endWindow();
-    @SuppressWarnings("unchecked")
     Map<String, Object> collectedTuple = (Map<String, Object>) mapSink.collectedTuples.get(0);
+    Map<String, Object> collectedTuple2 = (Map<String, Object>) mapSink.collectedTuples.get(3);
+    System.out.println(collectedTuple2);
     assertTrue("The ", collectedTuple.keySet().equals(Sets.newHashSet(header)));
     Object[] expectedVal = new Object[]{"Doctor Who", Boolean.TRUE, new SimpleDateFormat("yyyy-MM-dd").parse("1983-11-06")};
+    Object[] expectedVal2 = new Object[]{"Doctor WhoIV", Boolean.FALSE, new SimpleDateFormat("yyyy-MM-dd").parse("1966-10-20")};
     int j = 0;
     for (String k : header) {
-      assertTrue("Expected: " + expectedVal[j] + ",  Actual:" + collectedTuple.get(k).toString(), collectedTuple.get(k).equals(expectedVal[j++]));
+      assertEquals(expectedVal[j], collectedTuple.get(k));
+      assertEquals(expectedVal2[j++], collectedTuple2.get(k));
     }
   }
-  
 
 }
 
-class TestHeaderMapping implements CSVHeaderMapping{
+class TestHeaderMapping implements CSVHeaderMapping, Serializable
+{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
 
   @Override
   public CellProcessor[] getProcessors()
   {
-    return new CellProcessor[]{new Optional(), new ParseBool(), new ParseDate("yyyy-MM-dd")};
+    return new CellProcessor[] { new Optional(), new ParseBool(), new ParseDate("yyyy-MM-dd") };
   }
 
   @Override
@@ -82,5 +96,5 @@ class TestHeaderMapping implements CSVHeaderMapping{
   {
     return CsvParserOperatorTest.header;
   }
-  
+
 }
