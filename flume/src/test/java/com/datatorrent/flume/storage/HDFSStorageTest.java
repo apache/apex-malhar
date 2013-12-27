586b160af593c3a428c462809560d4e1428a7922
gaurav <gaurav@datatorrent.com>
2013-12-26 17:24:42 -0800

    implemented failure handling for HDFSStorage

--- flume/src/test/java/com/datatorrent/flume/storage/HDFSStorageTest.java
+++ flume/src/test/java/com/datatorrent/flume/storage/HDFSStorageTest.java
@@ -17,12 +17,13 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.datatorrent.flume.sink.Server;
 import com.datatorrent.flume.storage.HDFSStorage;
 import com.datatorrent.flume.storage.Storage;
 
 /**
  * 
- * @author Gaurav Gupta  <gaurav@datatorrent.com>
+ * @author Gaurav Gupta <gaurav@datatorrent.com>
  */
 public class HDFSStorageTest
 {
@@ -32,10 +33,10 @@ public class HDFSStorageTest
   public void testStorage() throws IOException
   {
     Context ctx = new Context();
-    ctx.put(HDFSStorage.BASE_DIR_KEY,STORAGE_DIRECTORY);
-    ctx.put(HDFSStorage.RESTORE_KEY,Boolean.toString(false));    
+    ctx.put(HDFSStorage.BASE_DIR_KEY, STORAGE_DIRECTORY);
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(false));
     Storage storage = new HDFSStorage();
-    ((Configurable)storage).configure(ctx);
+    ((Configurable) storage).configure(ctx);
     byte[] b = new byte[1028];
     byte[] val = storage.store(b);
     storage.close();
@@ -43,35 +44,42 @@ public class HDFSStorageTest
     byte[] tempData = new byte[data.length - 8];
     System.arraycopy(data, 8, tempData, 0, tempData.length);
     Assert.assertEquals("matched the stored value with retrieved value", new String(b), new String(tempData));
+    byte[] identifier = new byte[8];
+    Server.writeLong(identifier, 0, calculateOffset(0l, 1l));
+    storage.clean(identifier);
     
+
   }
-  
+
   @Test
   public void testStorageWithRestore() throws IOException
   {
     testStorage();
     Context ctx = new Context();
-    ctx.put(HDFSStorage.BASE_DIR_KEY,STORAGE_DIRECTORY);
-    ctx.put(HDFSStorage.RESTORE_KEY,Boolean.toString(true));    
+    ctx.put(HDFSStorage.BASE_DIR_KEY, STORAGE_DIRECTORY);
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(true));
     Storage storage = new HDFSStorage();
-    ((Configurable)storage).configure(ctx);
+    ((Configurable) storage).configure(ctx);
     byte[] b = new byte[1028];
     storage.store(b);
     storage.close();
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(conf);
-    boolean exists =fs.exists(new Path(STORAGE_DIRECTORY+"/"+"1")); 
-    Assert.assertEquals("file shoule not exist", true, exists);
+    boolean exists = fs.exists(new Path(STORAGE_DIRECTORY + "/" + "1"));
+    Assert.assertEquals("file shoule exist", true, exists);
+    byte[] identifier = new byte[8];
+    Server.writeLong(identifier, 0, calculateOffset(0l, 2l));
+    storage.clean(identifier);
   }
 
   @Test
   public void testCleanup() throws IOException
   {
     Context ctx = new Context();
-    ctx.put(HDFSStorage.BASE_DIR_KEY,STORAGE_DIRECTORY);
-    ctx.put(HDFSStorage.RESTORE_KEY,Boolean.toString(false));    
+    ctx.put(HDFSStorage.BASE_DIR_KEY, STORAGE_DIRECTORY);
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(false));
     Storage storage = new HDFSStorage();
-    ((Configurable)storage).configure(ctx);
+    ((Configurable) storage).configure(ctx);
     RandomAccessFile r = new RandomAccessFile("src/test/resources/TestInput.txt", "r");
     r.seek(0);
     byte[] b = r.readLine().getBytes();
@@ -82,19 +90,22 @@ public class HDFSStorageTest
     storage.clean(val);
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(conf);
-    boolean exists =fs.exists(new Path(STORAGE_DIRECTORY+"/"+"0")); 
+    boolean exists = fs.exists(new Path(STORAGE_DIRECTORY + "/" + "0"));
     Assert.assertEquals("file shoule not exist", false, exists);
     r.close();
+    byte[] identifier = new byte[8];
+    Server.writeLong(identifier, 0, calculateOffset(0l, 2l));
+    storage.clean(identifier);
   }
-  
+
   @Test
   public void testNext() throws IOException
   {
     Context ctx = new Context();
-    ctx.put(HDFSStorage.BASE_DIR_KEY,STORAGE_DIRECTORY);
-    ctx.put(HDFSStorage.RESTORE_KEY,Boolean.toString(false));    
+    ctx.put(HDFSStorage.BASE_DIR_KEY, STORAGE_DIRECTORY);
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(false));
     Storage storage = new HDFSStorage();
-    ((Configurable)storage).configure(ctx);
+    ((Configurable) storage).configure(ctx);
     RandomAccessFile r = new RandomAccessFile("src/test/resources/TestInput.txt", "r");
     r.seek(0);
     byte[] b = r.readLine().getBytes();
@@ -116,16 +127,19 @@ public class HDFSStorageTest
     System.arraycopy(data, 8, tempData, 0, tempData.length);
     Assert.assertEquals("matched the stored value with retrieved value", new String(b), new String(tempData));
     r.close();
+    byte[] identifier = new byte[8];
+    Server.writeLong(identifier, 0, calculateOffset(0l, 1l));
+    storage.clean(identifier);
   }
-  
+
   @Test
   public void testNextWithFileClose() throws IOException
   {
     Context ctx = new Context();
-    ctx.put(HDFSStorage.BASE_DIR_KEY,STORAGE_DIRECTORY);
-    ctx.put(HDFSStorage.RESTORE_KEY,Boolean.toString(false));    
+    ctx.put(HDFSStorage.BASE_DIR_KEY, STORAGE_DIRECTORY);
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(false));
     Storage storage = new HDFSStorage();
-    ((Configurable)storage).configure(ctx);
+    ((Configurable) storage).configure(ctx);
     RandomAccessFile r = new RandomAccessFile("src/test/resources/TestInput.txt", "r");
     r.seek(0);
     byte[] b = r.readLine().getBytes();
@@ -143,16 +157,21 @@ public class HDFSStorageTest
     System.arraycopy(data, 8, tempData, 0, tempData.length);
     Assert.assertEquals("matched the stored value with retrieved value", new String(b1), new String(tempData));
     r.close();
+    byte[] identifier = new byte[8];
+    Server.writeLong(identifier, 0, calculateOffset(0l, 1l));
+    storage.clean(identifier);
+    Server.writeLong(identifier, 0, calculateOffset(0l, 2l));
+    storage.clean(identifier);
   }
 
   @Test
   public void testRetrieval() throws IOException
   {
     Context ctx = new Context();
-    ctx.put(HDFSStorage.BASE_DIR_KEY,STORAGE_DIRECTORY);
-    ctx.put(HDFSStorage.RESTORE_KEY,Boolean.toString(false));    
+    ctx.put(HDFSStorage.BASE_DIR_KEY, STORAGE_DIRECTORY);
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(false));
     Storage storage = new HDFSStorage();
-    ((Configurable)storage).configure(ctx);
+    ((Configurable) storage).configure(ctx);
     RandomAccessFile r = new RandomAccessFile("src/test/resources/TestInput.txt", "r");
     r.seek(0);
     byte[] b = r.readLine().getBytes();
@@ -167,20 +186,68 @@ public class HDFSStorageTest
     System.arraycopy(data, 8, tempData, 0, tempData.length);
     System.arraycopy(data, 0, identifier, 0, 8);
     Assert.assertEquals("matched the stored value with retrieved value", new String(b), new String(tempData));
-    
+
     data = storage.retrieve(identifier);
     tempData = new byte[data.length - 8];
     System.arraycopy(data, 8, tempData, 0, tempData.length);
     System.arraycopy(data, 0, identifier, 0, 8);
     Assert.assertEquals("matched the stored value with retrieved value", new String(b1), new String(tempData));
-    
+
     data = storage.retrieve(identifier);
     tempData = new byte[data.length - 8];
     System.arraycopy(data, 8, tempData, 0, tempData.length);
     System.arraycopy(data, 0, identifier, 0, 8);
     Assert.assertEquals("matched the stored value with retrieved value", new String(b), new String(tempData));
-    r.close();    
+    r.close();
+    
+    Server.writeLong(identifier, 0, calculateOffset(0l, 1l));
+    storage.clean(identifier);
   }
-  
+
+  @Test
+  public void testRetrievalWithFailure() throws IOException
+  {
+    Context ctx = new Context();
+    ctx.put(HDFSStorage.BASE_DIR_KEY, STORAGE_DIRECTORY);
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(false));
+    Storage storage = new HDFSStorage();
+    ((Configurable) storage).configure(ctx);
+    RandomAccessFile r = new RandomAccessFile("src/test/resources/TestInput.txt", "r");
+    r.seek(0);
+    byte[] b = r.readLine().getBytes();
+    storage.store(b);
+    storage.flush();
+    storage.close();
+    byte[] identifier = new byte[8];
+    Server.writeLong(identifier, 0, calculateOffset((long) 4 + b.length, 0l));
+    ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(true));
+    storage = new HDFSStorage();
+    ((Configurable) storage).configure(ctx);
+    try {
+      storage.retrieve(identifier);
+    } catch (Exception e) {
+      byte[] new_identifier = storage.store(b);
+      Assert.assertNull("identifier is null", new_identifier);
+      storage.close();
+      byte[] data = storage.retrieve(identifier);
+      byte[] tempData = new byte[data.length - 8];
+      System.arraycopy(data, 8, tempData, 0, tempData.length);
+      Assert.assertEquals("matched the stored value with retrieved value", new String(b), new String(tempData));
+      
+    }
+    r.close();
+    
+    Server.writeLong(identifier, 0, calculateOffset(0l, 1l));
+    storage.clean(identifier);
+    Server.writeLong(identifier, 0, calculateOffset(0l, 2l));
+    storage.clean(identifier);
+  }
+
   private static final Logger logger = LoggerFactory.getLogger(HDFSStorageTest.class);
+
+  private long calculateOffset(long fileOffset, long fileCounter)
+  {
+    return ((fileCounter << 32) | (fileOffset & 0xffffffffl));
+  }
+
 }
