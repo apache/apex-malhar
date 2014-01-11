09de8da0944efdefb749d02be43f179bcfe8e1a6
Chetan Narsude <chetan@datatorrent.com>
2014-01-11 11:37:16 -0800

    Storage.close is a detail which is best hidden from users.

--- flume/src/test/java/com/datatorrent/flume/storage/HDFSStoragePerformance.java
+++ flume/src/test/java/com/datatorrent/flume/storage/HDFSStoragePerformance.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 
+ *
  * @author Gaurav Gupta  <gaurav@datatorrent.com>
  */
 public class HDFSStoragePerformance
@@ -23,16 +23,17 @@ public class HDFSStoragePerformance
     Context ctx = new Context();
     ctx.put(HDFSStorage.BASE_DIR_KEY, ".");
     ctx.put(HDFSStorage.RESTORE_KEY, Boolean.toString(false));
-    Storage storage = new HDFSStorage();
-    ((Configurable) storage).configure(ctx);
+    HDFSStorage storage = new HDFSStorage();
+    storage.configure(ctx);
 
     logger.debug(" start time {}",System.currentTimeMillis());
     byte[] b = new byte[1024];
-    for (int i = 0; i < 1000000; i++)
+    for (int i = 0; i < 1000000; i++) {
       storage.store(b);
+    }
     storage.close();
     logger.debug(" end time {}",System.currentTimeMillis());
   }
-  
+
   private static final Logger logger = LoggerFactory.getLogger(HDFSStoragePerformance.class);
 }
