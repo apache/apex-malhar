8debd8f28b6323f5aa9c35838e43d16d54ff140f
David Yan <david@datatorrent.com>
2014-01-13 15:24:30 -0800

    Store is now a generic

---
+++ library/src/main/java/com/datatorrent/lib/db/AggregateKeyValPairTransactionableStoreOutputOperator.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2012-2014 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.datatorrent.lib.db;
+
+import com.datatorrent.api.Partitionable;
+import com.datatorrent.api.Partitionable.Partition;
+import com.datatorrent.lib.db.AbstractAggregateTransactionableStoreOutputOperator;
+import com.datatorrent.lib.db.TransactionableStore;
+import com.datatorrent.lib.util.KeyValPair;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ *
+ * @since 0.9.3
+ */
+public class AggregateKeyValPairTransactionableStoreOutputOperator<K, V, S extends TransactionableKeyValueStore>
+        extends AbstractAggregateTransactionableStoreOutputOperator<KeyValPair<K, V>, S>
+{
+  protected Map<Object, Object> dataMap;
+
+  @Override
+  public void storeAggregate()
+  {
+    store.putAll(dataMap);
+  }
+
+  @Override
+  protected long getCommittedWindowId(String appId, int operatorId)
+  {
+    Object value = store.get(getCommittedWindowKey(appId, operatorId));
+    return (value == null) ? -1 : Long.valueOf(value.toString());
+  }
+
+  @Override
+  protected void storeCommittedWindowId(String appId, int operatorId, long windowId)
+  {
+    store.put(getCommittedWindowKey(appId, operatorId), windowId);
+  }
+
+  protected Object getCommittedWindowKey(String appId, int operatorId)
+  {
+    return "_dt_wid:" + appId + ":" + operatorId;
+  }
+
+  @Override
+  public void processTuple(KeyValPair<K, V> tuple)
+  {
+    dataMap.put(tuple.getKey(), tuple.getValue());
+  }
+
+}
