979bbea24d4ed5093b1c8337fbfe6dcbc75f68a0
Andy Perlitch <andyperlitch@gmail.com>
2014-01-27 15:07:43 -0800

    added license agent model/collection

---
+++ front/js/datatorrent/LicenseAgentModel.js
@@ -0,0 +1,24 @@
+var BaseModel = require('./BaseModel');
+
+var LicenseAgentModel = BaseModel.extend({
+
+    debugName: 'License Agent',
+
+    defaults: {
+        id: '',
+        appId: '',
+        startedTime: '',
+        user: '',
+        remainingLicensedMB: '',
+        totalLicensedMB: ''
+    },
+
+    toJSON: function() {
+        var json = BaseModel.prototype.toJSON.call(this);
+        json.usedLicensedMB = json.totalLicensedMB * 1 - json.remainingLicensedMB * 1;
+        json.percentUsedLicenseMB = ((json.usedLicensedMB / json.totalLicensedMB) * 100).toFixed(1);
+        return json;
+    }
+
+});
+exports = module.exports = LicenseAgentModel;
