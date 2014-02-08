debaee28347a865cd1172b0e1ae8865a278eb619
Chandni Singh <chandni@datatorrent.com>
2014-02-07 16:16:46 -0800

    fixed a bug because of non breaking

--- flume/src/main/java/com/datatorrent/flume/source/DTFlumeSource.java
+++ flume/src/main/java/com/datatorrent/flume/source/DTFlumeSource.java
@@ -4,9 +4,12 @@
  */
 package com.datatorrent.flume.source;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -22,6 +25,8 @@ import org.apache.flume.source.AbstractSource;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
 
 public class DTFlumeSource extends AbstractSource implements EventDrivenSource, Configurable
 {
@@ -39,6 +44,7 @@ public class DTFlumeSource extends AbstractSource implements EventDrivenSource, 
   public transient String todayDate;
 
   private transient BufferedReader lineReader;
+  private transient List<Event> cache;
 
   public DTFlumeSource()
   {
@@ -61,11 +67,54 @@ public class DTFlumeSource extends AbstractSource implements EventDrivenSource, 
     rate = context.getInteger(RATE);
     emitTimer = new Timer();
     Preconditions.checkArgument(!Strings.isNullOrEmpty(filePath));
+    cache = Lists.newArrayListWithCapacity(rate);
     try {
       lineReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+      for (int i = 0; i < rate; i++) {
+        String line = lineReader.readLine();
+        if (line == null) {
+          lineReader.close();
+          lineReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+          line = lineReader.readLine();
+        }
+        byte[] row = line.getBytes();
+        final int rowsize = row.length;
+
+            /* guid */
+        int sliceLengh = -1;
+        while (++sliceLengh < rowsize) {
+          if (row[sliceLengh] == FIELD_SEPARATOR) {
+            break;
+          }
+        }
+            /* skip the next record */
+        int pointer = sliceLengh + 1;
+        while (pointer < rowsize) {
+          if (row[pointer++] == FIELD_SEPARATOR) {
+            break;
+          }
+        }
+
+           /* lets parse the date */
+        int dateStart = pointer;
+        while (pointer < rowsize) {
+          if (row[pointer++] == FIELD_SEPARATOR) {
+            String date = new String(row, dateStart, pointer - dateStart - 1);
+            if (date.indexOf(d1) >= 0) {
+              System.arraycopy(yesterdayDate.getBytes(), 0, row, dateStart, yesterdayDate.getBytes().length);
+            }
+            else {
+              System.arraycopy(todayDate.getBytes(), 0, row, dateStart, todayDate.getBytes().length);
+            }
+            break;
+          }
+        }
+        Event event = EventBuilder.withBody(row);
+        cache.add(event);
+      }
     }
-    catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
+    catch (Throwable t) {
+      Throwables.propagate(t);
     }
 
   }
@@ -79,51 +128,8 @@ public class DTFlumeSource extends AbstractSource implements EventDrivenSource, 
       @Override
       public void run()
       {
-        try {
-          for (int i = 0; i < rate; i++) {
-            String line = lineReader.readLine();
-            if (line == null) {
-              lineReader.close();
-              lineReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
-              line = lineReader.readLine();
-            }
-            byte[] row = line.getBytes();
-            final int rowsize = row.length;
-
-            /* guid */
-            int sliceLengh = -1;
-            while (++sliceLengh < rowsize) {
-              if (row[sliceLengh] == FIELD_SEPARATOR) {
-                break;
-              }
-            }
-
-            /* skip the next record */
-            int pointer = sliceLengh + 1;
-            while (pointer < rowsize) {
-              if (row[pointer++] == FIELD_SEPARATOR) {
-                break;
-              }
-            }
-
-           /* lets parse the date */
-            int dateStart = pointer;
-            while (pointer < rowsize) {
-              if (row[pointer++] == FIELD_SEPARATOR) {
-                String date = new String(row, dateStart, pointer - dateStart - 1);
-                if (date.contains(d1)) {
-                  System.arraycopy(yesterdayDate.getBytes(), 0, row, dateStart, yesterdayDate.getBytes().length);
-                }
-                else
-                  System.arraycopy(todayDate.getBytes(), 0, row, dateStart, todayDate.getBytes().length);
-              }
-            }
-            Event event = EventBuilder.withBody(row);
-            channel.processEvent(event);
-          }
-        }
-        catch (IOException e) {
-          throw new RuntimeException(e);
+        for (Event event : cache) {
+          channel.processEvent(event);
         }
       }
     }, 0, 1000);
