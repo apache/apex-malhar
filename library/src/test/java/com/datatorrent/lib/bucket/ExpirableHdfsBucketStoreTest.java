d3fdd82551d7163799b26dc8ee475c66b95373b5
Chandni Singh <chandni@datatorrent.com>
2014-04-11 12:46:49 -0700

    Tests for expirable hdfs store

--- library/src/test/java/com/datatorrent/lib/bucket/ExpirableHdfsBucketStoreTest.java
+++ library/src/test/java/com/datatorrent/lib/bucket/ExpirableHdfsBucketStoreTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,23 +16,21 @@
 package com.datatorrent.lib.bucket;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/**
- * Tests of {@link HdfsBucketStore}
- */
-public class HdfsBucketStoreTest
+public class ExpirableHdfsBucketStoreTest
 {
   static BucketStoreTests tests;
+  static ExpirableHdfsBucketStore<DummyEvent> expirableStore;
 
   @BeforeClass
   public static void setUp()
   {
     tests = new BucketStoreTests();
-    tests.setup(new HdfsBucketStore<DummyEvent>());
+    expirableStore = new ExpirableHdfsBucketStore<DummyEvent>();
+    tests.setup(expirableStore);
   }
 
   @AfterClass
@@ -45,20 +43,7 @@ public class HdfsBucketStoreTest
   public void test1() throws Exception
   {
     tests.testStoreBucketData();
+    expirableStore.deleteExpiredBuckets(1);
+    Assert.assertTrue(!tests.bucketExists(0));
   }
-
-  @Test
-  public void test2() throws Exception
-  {
-    tests.testFetchBucket();
-  }
-
-  @Test
-  public void test3() throws Exception
-  {
-    tests.testDeleteBucket();
-  }
-
-  private static final Logger logger = LoggerFactory.getLogger(HdfsBucketStore.class);
-
 }
