62402eb5bf4f2840b0e9ce0e51c1acba0cb15b52
Ashwin Chandra Putta <ashwin@datatorrent.com>
2014-04-28 11:47:19 -0700

    added license headers

--- apps/src/main/java/com/datatorrent/apps/etl/AggregationsOperator.java
+++ apps/src/main/java/com/datatorrent/apps/etl/AggregationsOperator.java
@@ -1,6 +1,17 @@
 /*
- *  Copyright (c) 2012-2014 Malhar, Inc.
- *  All Rights Reserved.
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.datatorrent.apps.etl;
 
@@ -12,6 +23,7 @@ import javax.annotation.Nonnull;
 /**
  * MapAggregateEvent aggregations operator
  */
+@ShipContainingJars(classes = {com.mongodb.DB.class})
 public class AggregationsOperator extends AggregationsOperatorBase<MapAggregateEvent, DataStoreWriter<MapAggregateEvent>>
 {
   @Nonnull
@@ -22,7 +34,7 @@ public class AggregationsOperator extends AggregationsOperatorBase<MapAggregateE
   {
     MapAggregateEvent aggregateEvent = (MapAggregateEvent)cache.getValueFor(event);
     if (aggregateEvent == null) {
-      aggregateEvent = retreiveFromStore(event);
+      aggregateEvent = event; //retreiveFromStore(event);
       if (aggregateEvent == null) {
         aggregateEvent = event;
       }
@@ -35,7 +47,7 @@ public class AggregationsOperator extends AggregationsOperatorBase<MapAggregateE
     }
 
     cache.setValueFor(aggregateEvent, aggregateEvent);
-    updateStore(aggregateEvent);
+    //updateStore(aggregateEvent);
 
     output.emit(aggregateEvent);
   }
