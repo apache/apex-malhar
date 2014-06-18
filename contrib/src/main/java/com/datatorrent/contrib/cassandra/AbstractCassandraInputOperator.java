7d91c018f9db718f0348cfe1d3a5925836cae131
Rohit Sharma <rohitsharma91@gmail.com>
2014-06-17 19:39:33 -0700

    MLHR-1172 and MLHR-1173 added cassandra input and output adapters

---
+++ contrib/src/main/java/com/datatorrent/contrib/cassandra/AbstractCassandraInputOperator.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.datatorrent.contrib.cassandra;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.exceptions.*;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
+
+
+import com.datatorrent.lib.db.AbstractStoreInputOperator;
+
+/**
+ * Base Cassandra input adapter operator, which reads data from persistence database through DATASTAX API
+ * and writes into output port(s).
+ *
+ * <p>
+ * This is an abstract class. Sub-classes need to implement {@link #queryToRetrieveData()} and {@link #getTuple(Row)}.
+ * </p>
+ */
+
+
+public abstract class AbstractCassandraInputOperator<T> extends AbstractStoreInputOperator<T, CassandraStore> {
+
+	private static final Logger logger = LoggerFactory.getLogger(AbstractCassandraInputOperator.class);
+	Statement queryStatement = null;
+
+	/**
+	 * Any concrete class has to override this method to convert a Database row into Tuple.
+	 *
+	 * @param result a single row that has been read from database.
+	 * @return Tuple a tuples created from row which can be any Java object.
+	 */
+	public abstract T getTuple(Row row);
+
+	/**
+	 * Any concrete class has to override this method to return the query string which will be used to
+	 * retrieve data from database.
+	 *
+	 * @return Query string
+	 */
+	public abstract String queryToRetrieveData();
+
+	/**
+	 * The output port that will emit tuple into DAG.
+	 */
+	@OutputPortFieldAnnotation(name = "outputPort")
+	public final transient DefaultOutputPort<T> outputPort = new DefaultOutputPort<T>();
+
+	/**
+	 * This executes the query to retrieve result from database.
+	 * It then converts each row into tuple and emit that into output port.
+	 */
+	@Override
+	public void emitTuples()
+	{
+		String query = queryToRetrieveData();
+		logger.debug(String.format("select statement: %s", query));
+
+		try {
+			ResultSet result = store.getSession().execute(query);
+			for(Row row: result) {
+				T tuple = getTuple(row);
+				outputPort.emit(tuple);
+			}
+		}
+		catch (NoHostAvailableException ex) {
+			store.disconnect();
+			throw new RuntimeException(String.format("Error while running query: %s", query), ex);
+		}
+		catch (QueryExecutionException ex) {
+			store.disconnect();
+			throw new RuntimeException(String.format("Error while running query: %s", query), ex);
+		}
+		catch (QueryValidationException ex) {
+			store.disconnect();
+			throw new RuntimeException(String.format("Error while running query: %s", query), ex);
+		}
+		catch(DriverException ex){
+			store.disconnect();
+			throw new RuntimeException(String.format("Error while running query: %s", query), ex);
+		}
+	}
+
+
+
+}
