068ced5f7aeaa1b2f593fd457a03b7ff4908b687
Rohit Sharma <rohitsharma91@gmail.com>
2014-06-25 17:07:59 -0700

    Squashed commit of the following:
    commit 77d0a427347ed0564dafee3ee00bfb226a0a4322
    Merge: b29b248 1000a99
    Author: Rohit Sharma <rohitsharma91@gmail.com>
    Date:   Wed Jun 25 17:00:58 2014 -0700
    
        Merge branch 'master' of github.com:DataTorrent/Malhar into cassandra_adapters
    
    commit b29b248d91e9f5036f29101e2971e99f5d31b286
    Author: Rohit Sharma <rohitsharma91@gmail.com>
    Date:   Wed Jun 25 16:59:57 2014 -0700
    
        Updated cassandra input and output adapters and added AbstractBatchTransactionableStoreOutputOperator
    
    commit 7d91c018f9db718f0348cfe1d3a5925836cae131
    Author: Rohit Sharma <rohitsharma91@gmail.com>
    Date:   Tue Jun 17 19:39:33 2014 -0700
    
        MLHR-1172 and MLHR-1173 added cassandra input and output adapters

---
+++ library/src/main/java/com/datatorrent/lib/db/AbstractBatchTransactionableStoreOutputOperator.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.db;
+
+import java.util.List;
+
+import com.datatorrent.api.Context.OperatorContext;
+import com.google.common.collect.Lists;
+
+/**
+ * This abstract class is for aggregate output (over one application window, all in one single batch) to a transactionable store with the transactional exactly-once feature.
+ *
+ * @param <T> The tuple type.
+ * @param <S> The store type.
+ */
+
+public abstract class AbstractBatchTransactionableStoreOutputOperator<T, S extends TransactionableStore> extends AbstractTransactionableStoreOutputOperator<T, S>{
+	protected final List<T> tuples;
+
+	public AbstractBatchTransactionableStoreOutputOperator(){
+		tuples = Lists.newArrayList();
+	}
+
+	@Override
+	public void setup(OperatorContext context)
+	{
+		super.setup(context);
+	}
+
+	@Override
+	public void processTuple(T tuple)
+	{
+		tuples.add(tuple);
+	}
+
+	@Override
+	public void endWindow()
+	{
+		store.beginTransaction();
+		processBatch();
+		store.storeCommittedWindowId(appId, operatorId, currentWindowId);
+		store.commitTransaction();
+		committedWindowId = currentWindowId;
+		super.endWindow();
+		tuples.clear();
+	}
+
+	/**
+	 * Processes the whole batch at the end window and writes to the store.
+	 *
+	 */
+	public abstract void processBatch();
+}
