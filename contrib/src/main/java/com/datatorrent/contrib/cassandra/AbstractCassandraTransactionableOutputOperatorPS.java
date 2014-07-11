52096dd018754ec517f3f712256e4f18bdba1371
Rohit Sharma <rohitsharma91@gmail.com>
2014-07-10 18:27:00 -0700

    added @ShipContatingJars and benchmark application

--- contrib/src/main/java/com/datatorrent/contrib/cassandra/AbstractCassandraTransactionableOutputOperatorPS.java
+++ contrib/src/main/java/com/datatorrent/contrib/cassandra/AbstractCassandraTransactionableOutputOperatorPS.java
@@ -20,6 +20,7 @@ import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.exceptions.DriverException;
 import com.datatorrent.api.Context;
+import com.datatorrent.api.annotation.ShipContainingJars;
 
 import javax.annotation.Nonnull;
 
@@ -45,6 +46,7 @@ import javax.annotation.Nonnull;
  * @param <T>type of tuple</T>
  * @since 1.0.2
  */
+@ShipContainingJars(classes = {com.datastax.driver.core.Cluster.class, com.codahale.metrics.Metric.class})
 public abstract class AbstractCassandraTransactionableOutputOperatorPS<T> extends AbstractCassandraTransactionableOutputOperator<T>{
 
 	private transient PreparedStatement updateCommand;
