045eeb077a5ead71228d4e163c1f273fa62fb422
Pramod Immaneni <pramod@datatorrent.com>
2014-07-28 16:12:51 -0700

    Fixed licensing and documentation

--- library/src/main/java/com/datatorrent/lib/xml/XmlDOMOperator.java
+++ library/src/main/java/com/datatorrent/lib/xml/XmlDOMOperator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.lib.xml;
 
 import com.datatorrent.api.BaseOperator;
@@ -12,7 +27,10 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 /**
- * Created by Pramod Immaneni <pramod@datatorrent.com> on 6/12/14.
+ * Operator that provides XML DOM parsing functionality.
+ *
+ * The operator is abstract and should be extended. It parses incoming tuples using the Java XML DOM parser and provides
+ * the resulting Document object along with the tuple to the extending operator for processing.
  *
  * @since 1.0.2
  */
