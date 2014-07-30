e61f7101737d036891bf5626befaaa792b2a951e
gaurav <gaurav@datatorrent.com>
2014-07-30 15:49:11 -0700

    MLHR-1253: added the documentation for keyhashvalpair

--- library/src/main/java/com/datatorrent/lib/util/KeyHashValPair.java
+++ library/src/main/java/com/datatorrent/lib/util/KeyHashValPair.java
@@ -18,36 +18,41 @@ package com.datatorrent.lib.util;
 import java.util.Map;
 
 /**
- * <p>KeyHashValPair class.</p>
+ * Unlike {@link KeyValPair} this computes hashcode based on just the key and not the value.<br/>
+ * Key and Value are to be treated as immutable objects.
  *
  * @since 0.9.0
  */
-public class KeyHashValPair<K, V> extends KeyValPair<K, V> {
 
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 7005592007894368002L;
+public class KeyHashValPair<K, V> extends KeyValPair<K, V>
+{
 
-	public KeyHashValPair(K k, V v) {
-		super(k, v);
-	}
+  private static final long serialVersionUID = 7005592007894368002L;
 
-	private KeyHashValPair() {
-		super(null, null);
-	}
+  public KeyHashValPair(K k, V v)
+  {
+    super(k, v);
+  }
 
-	@Override
-	public int hashCode() {
-		return (getKey() == null ? 0 : getKey().hashCode());
-	}
+  private KeyHashValPair()
+  {
+    super(null, null);
+  }
 
-	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof Map.Entry))
-			return false;
-		Map.Entry e = (Map.Entry) o;
-		return (this.getKey() == null ? e.getKey() == null : this.getKey().equals(e.getKey()));
-	}
+  @Override
+  public int hashCode()
+  {
+    return (getKey() == null ? 0 : getKey().hashCode());
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (!(o instanceof Map.Entry)) {
+      return false;
+    }
+    Map.Entry e = (Map.Entry) o;
+    return (this.getKey() == null ? e.getKey() == null : this.getKey().equals(e.getKey()));
+  }
 
 }
