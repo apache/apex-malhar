849853c5d53648f0523dcdb4bfee178c343c0cca
thomas <thomas@datatorrent.com>
2014-08-18 13:57:16 -0700

    test app with 2 partitions

---
+++ library/src/main/java/com/datatorrent/lib/hds/HDSOperator.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.hds;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datatorrent.api.Context.OperatorContext;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultPartition;
+import com.datatorrent.api.Partitioner;
+import com.datatorrent.api.StreamCodec;
+import com.datatorrent.common.util.DTThrowable;
+import com.datatorrent.lib.codec.KryoSerializableStreamCodec;
+import com.datatorrent.lib.util.KeyValPair;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.google.common.collect.Lists;
+
+public class HDSOperator extends HDSBucketManager implements Partitioner<HDSOperator>
+{
+  private static final Logger LOG = LoggerFactory.getLogger(HDSOperator.class);
+
+  /**
+   * Partition keys identify assigned bucket(s).
+   */
+  protected int partitionMask;
+  protected Set<Integer> partitions;
+  @NotNull
+  private String storeDir;
+
+  private transient StreamCodec<KeyValPair<byte[], byte[]>> streamCodec;
+  private transient FileSystem fs;
+
+  public final transient DefaultInputPort<KeyValPair<byte[], byte[]>> data = new DefaultInputPort<KeyValPair<byte[], byte[]>>() {
+    @Override
+    public void process(KeyValPair<byte[], byte[]> tuple)
+    {
+      int bucketKey = streamCodec.getPartition(tuple) & partitionMask;
+      try {
+        HDSOperator.this.put(bucketKey, tuple.getKey(), tuple.getValue());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public Class<? extends StreamCodec<KeyValPair<byte[], byte[]>>> getStreamCodec()
+    {
+      return getBucketKeyStreamCodec();
+    }
+  };
+
+  public static class BucketKeyStreamCodec extends KryoSerializableStreamCodec<KeyValPair<byte[], byte[]>>
+  {
+    @Override
+    public int getPartition(KeyValPair<byte[], byte[]> t)
+    {
+      int length = t.getKey().length;
+      int hash = 0;
+      for (int i = 0; i < 4 && i < length; i++) {
+        hash = hash << 8;
+        hash += t.getKey()[i];
+      }
+      return hash;
+    }
+  }
+
+  protected Class<? extends StreamCodec<KeyValPair<byte[], byte[]>>> getBucketKeyStreamCodec()
+  {
+    return BucketKeyStreamCodec.class;
+  }
+
+  @Override
+  public void setup(OperatorContext arg0)
+  {
+    super.setup(arg0);
+    try {
+      this.streamCodec = getBucketKeyStreamCodec().newInstance();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create streamCodec", e);
+    }
+
+    LOG.debug("Opening store at {} for partitions {} {}", this.storeDir, new PartitionKeys(this.partitionMask, this.partitions));
+    Path dataFilePath = new Path(storeDir);
+    try {
+      this.fs = FileSystem.newInstance(dataFilePath.toUri(), new Configuration());
+      super.bfs = new HDSFileAccessFSImpl(fs, storeDir);
+    } catch (IOException e) {
+      DTThrowable.rethrow(e);
+    }
+  }
+
+  @Override
+  public void teardown()
+  {
+    super.teardown();
+    try {
+      super.bfs.close();
+      this.fs.close();
+    } catch (IOException e) {
+      LOG.warn("Failed to close.", e);
+    }
+  }
+
+  @Override
+  public Collection<Partition<HDSOperator>> definePartitions(Collection<Partition<HDSOperator>> partitions, int incrementalCapacity)
+  {
+    boolean isInitialPartition = partitions.iterator().next().getStats() == null;
+
+    if (!isInitialPartition) {
+      // support for dynamic partitioning requires lineage tracking
+      LOG.warn("Dynamic partitioning not implemented");
+      return partitions;
+    }
+
+    int totalCount = partitions.size() + incrementalCapacity;
+    Kryo kryo = new Kryo();
+    Collection<Partition<HDSOperator>> newPartitions = Lists.newArrayListWithExpectedSize(totalCount);
+    for (int i = 0; i < totalCount; i++) {
+      // Kryo.copy fails as it attempts to clone transient fields (input port)
+      // TestStoreOperator oper = kryo.copy(this);
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      Output output = new Output(bos);
+      kryo.writeObject(output, this);
+      output.close();
+      Input input = new Input(bos.toByteArray());
+      HDSOperator oper = kryo.readObject(input, this.getClass());
+      newPartitions.add(new DefaultPartition<HDSOperator>(oper));
+    }
+
+    // assign the partition keys
+    DefaultPartition.assignPartitionKeys(newPartitions, data);
+
+    for (Partition<HDSOperator> p : newPartitions) {
+      PartitionKeys pks = p.getPartitionKeys().get(data);
+      p.getPartitionedInstance().partitionMask = pks.mask;
+      p.getPartitionedInstance().partitions = pks.partitions;
+    }
+
+    return newPartitions;
+  }
+
+  @Override
+  public void partitioned(Map<Integer, Partition<HDSOperator>> arg0)
+  {
+  }
+
+  public String getStoreDir()
+  {
+    return storeDir;
+  }
+
+  public void setStoreDir(String storeDir)
+  {
+    this.storeDir = storeDir;
+  }
+
+}
---
+++ library/src/test/java/com/datatorrent/lib/hds/HDSTestApp.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.hds;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.InputOperator;
+import com.datatorrent.api.LocalMode;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.annotation.ApplicationAnnotation;
+import com.datatorrent.lib.hds.HDSTest.MyDataKey;
+import com.datatorrent.lib.util.KeyValPair;
+
+@ApplicationAnnotation(name="HDSTestApp")
+public class HDSTestApp implements StreamingApplication
+{
+
+  public static class Generator extends BaseOperator implements InputOperator
+  {
+    public transient DefaultOutputPort<KeyValPair<byte[], byte[]>> output = new DefaultOutputPort<KeyValPair<byte[], byte[]>>();
+
+    @Override
+    public void emitTuples()
+    {
+      byte[] k0 = ByteBuffer.allocate(16).putLong(0x00000000).putLong(0).array();
+      output.emit(new KeyValPair<byte[], byte[]>(k0, "data0".getBytes()));
+      byte[] k1 = ByteBuffer.allocate(16).putLong(0xffffffff).putLong(0).array();
+      output.emit(new KeyValPair<byte[], byte[]>(k1, "data1".getBytes()));
+    }
+  }
+
+  @Override
+  public void populateDAG(DAG dag, Configuration conf)
+  {
+    Generator generator = dag.addOperator("Generator", new Generator());
+    HDSOperator store = dag.addOperator("Store", new HDSOperator());
+    store.setKeyComparator(new MyDataKey.SequenceComparator());
+
+    dag.addStream("Generator2Store", generator.output, store.data);
+  }
+
+  @Test
+  public void test() throws Exception
+  {
+    File file = new File("target/hds2");
+    FileUtils.deleteDirectory(file);
+
+    LocalMode lma = LocalMode.newInstance();
+    Configuration conf = new Configuration(false);
+    conf.set("dt.operator.Store.storeDir", file.toURI().toString());
+    conf.set("dt.operator.Store.attr.INITIAL_PARTITION_COUNT", "2");
+
+    lma.prepareDAG(new HDSTestApp(), conf);
+    LocalMode.Controller lc = lma.getController();
+    lc.setHeartbeatMonitoringEnabled(false);
+    lc.runAsync();
+
+    long tms = System.currentTimeMillis();
+    File f0 = new File(file, "0/_WAL");
+    File f1 = new File(file, "1/_WAL");
+
+    while (System.currentTimeMillis() - tms < 30000) {
+      if (f0.exists()) break;
+      if (f1.exists()) break;
+      Thread.sleep(100);
+    }
+    lc.shutdown();
+
+    Assert.assertTrue("exists " + f0, f0.exists() && f0.isFile());
+    Assert.assertTrue("exists " + f1, f1.exists() && f1.isFile());
+
+  }
+
+
+}
