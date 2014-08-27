3f00c93ef64d0f0a8ddab619b25e6d5493938493
gaurav <gaurav@datatorrent.com>
2014-08-27 16:00:00 -0700

    added more test cases

--- library/src/main/java/com/datatorrent/lib/algo/AbstractPatternMatcher.java
+++ library/src/main/java/com/datatorrent/lib/algo/AbstractPatternMatcher.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.lib.algo;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.validation.constraints.NotNull;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -10,26 +27,48 @@ import com.google.common.collect.Maps;
 import com.datatorrent.api.BaseOperator;
 import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.annotation.OperatorAnnotation;
 
 /**
+ * This operator searches the pattern in the input stream.
  *
+ * <br>
+ * <b> StateFull : Yes, </b> Count is aggregated over application window(s). <br>
+ * <b> Partitionable : No, </b> will yield wrong result. <br>
+ *
+ * <b>Ports</b>:<br>
+ * <b>inputPort</b>: the port to receive input<br>
+ *
+ * <b>Properties</b>:<br>
+ * <b>pattern</b>: The pattern that needs to be searched<br>
  */
 
-public class PatternMatcher<T> extends BaseOperator
+@OperatorAnnotation(partitionable = false)
+public abstract class AbstractPatternMatcher<T> extends BaseOperator
 {
-
-  private List<List<T>> matchedPatterns;
+  @NotNull
   private Pattern pattern;
+  private List<List<T>> matchedPatterns;
   private int patternLength;
 
-  public PatternMatcher(Pattern pattern)
+  public AbstractPatternMatcher()
+  {
+    matchedPatterns = Lists.newArrayList();
+  }
+
+  public void setPattern(Pattern pattern)
   {
     this.pattern = pattern;
     patternLength = pattern.getPattern().size();
-    matchedPatterns = Lists.newArrayList();
+    matchedPatterns.clear();
     for (int i = 0; i < patternLength; i++) {
       matchedPatterns.add(new ArrayList<T>());
     }
+  }
+
+  public Pattern getPattern()
+  {
+    return pattern;
   }
 
   public transient DefaultInputPort<T> inputPort = new DefaultInputPort<T>()
@@ -38,7 +77,10 @@ public class PatternMatcher<T> extends BaseOperator
     public void process(T t)
     {
       List<Integer> matchingPositions = pattern.getPosition(t);
-      if (matchingPositions == null || matchingPositions.isEmpty()) {
+      if (matchingPositions == null) {
+        for (int i = 0; i < patternLength; i++) {
+          matchedPatterns.get(i).clear();
+        }
         return;
       }
       int matchingPositionIndex = 0;
@@ -55,12 +97,12 @@ public class PatternMatcher<T> extends BaseOperator
         List<T> prev = matchedPatterns.get(i);
         if (!prev.isEmpty()) {
           prev.add(t);
-          matchedPatterns.get(i-1).addAll(prev);
+          matchedPatterns.get(i - 1).addAll(prev);
           prev.clear();
         }
         matchingPositionIndex++;
+        i++;
         if (matchingPositionIndex == matchingPositionLength) {
-          i++;
           for (; i < patternLength; i++) {
             matchedPatterns.get(i).clear();
           }
@@ -73,12 +115,15 @@ public class PatternMatcher<T> extends BaseOperator
       if (patternPosition == 0) {
         matchedPatterns.get(patternLength - 1).add(t);
       }
-      if (matchedPatterns.get(0) != null && !matchedPatterns.get(0).isEmpty()) {
-        outputPort.emit(matchedPatterns.get(0));
-        matchedPatterns.get(0).clear();
+      List<T> outputList = matchedPatterns.get(0);
+      if (!outputList.isEmpty()) {
+        processPatternFound(outputList);
+        outputList.clear();
       }
     }
   };
+
+  public abstract void processPatternFound(List<T> outputList);
 
   public transient DefaultOutputPort<List<T>> outputPort = new DefaultOutputPort<List<T>>();
 
@@ -87,7 +132,7 @@ public class PatternMatcher<T> extends BaseOperator
     private Map<T, List<Integer>> positionMap;
     private List<T> pattern;
 
-    private Pattern()
+    public Pattern()
     {
       positionMap = Maps.newHashMap();
     }
@@ -122,6 +167,7 @@ public class PatternMatcher<T> extends BaseOperator
     public void setPattern(List<T> pattern)
     {
       this.pattern = pattern;
+      positionMap.clear();
       populatePositionMap();
     }
   }
