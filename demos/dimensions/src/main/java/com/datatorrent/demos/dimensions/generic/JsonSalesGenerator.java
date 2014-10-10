9a95e06536e095bda47a1d7c4017d3b55d9a5ccf
sashadt <sasha@datatorrent.com>
2014-10-10 00:45:40 -0700

    Updating documentation and default settings for dimensions apps; SPOI-3305 Adding JSON Sales events generator and tests; SPOI-3306 Changing default schema to sales demo; Adding dimensions query operators, replacing productCategory type; Renaming adsdimensions package

---
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/generic/JsonSalesGenerator.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.demos.dimensions.generic;
+
+import com.datatorrent.api.Context;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.InputOperator;
+import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import javax.validation.constraints.Min;
+import java.util.Random;
+
+/**
+ * Generates sales events data and sends them out as JSON encoded byte arrays.
+ * <p>
+ * Sales events are JSON string encoded as byte arrays.  All id's are expected to be positive integers, and default to 1.
+ * Transaction amounts are double values with two decimal places.  Timestamp is unix epoch in milliseconds.
+ * Product categories are not assigned by default.  They are expected to be added by the Enrichment operator, but can be
+ * enabled with addProductCategory override.
+ *
+ * Example Sales Event
+ *
+ * {
+ *    "productId": 1,
+ *    "customerId": 12345,
+ *    "productCategory": 0,
+ *    "channelId": 3,
+ *    "amount": 107.99,
+ *    "timestamp": 1412897574000
+ * }
+ *
+ * @displayName JSON Sales Event Generator
+ * @category Input
+ * @tags input, generator, json
+ */
+
+class SalesEvent {
+
+  /* dimension keys */
+  public int productId;
+  public int customerId;
+  public int channelId;
+  public int productCategory;
+  public long timestamp;
+  /* metrics */
+  public double amount;
+}
+
+public class JsonSalesGenerator implements InputOperator
+{
+  @Min(1)
+  private int maxProductId = 100;
+  @Min(1)
+  private int maxCustomerId = 1000000;
+  @Min(1)
+  private int maxChannelId = 5;
+  @Min(0)
+  private double minAmount = 0.99;
+  private double maxAmount = 1000.0;
+
+  // Should not be included by default - only used for testing when running without enrichment operator
+  private boolean addProductCategory = false;
+  @Min(1)
+  private int maxProductCategories = 5;
+
+  // Limit number of emitted tuples per window
+  @Min(0)
+  private long maxTuplesPerWindow = 40000;
+
+
+  /**
+   * Outputs sales event in JSON format as a byte array
+   */
+  @OutputPortFieldAnnotation(name = "jsonBytes")
+  public final transient DefaultOutputPort<byte[]> jsonBytes = new DefaultOutputPort<byte[]>();
+
+  private static final ObjectMapper mapper = new ObjectMapper().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+  private final Random random = new Random();
+
+  private long tuplesCounter = 0;
+
+  @Override
+  public void beginWindow(long windowId)
+  {
+    tuplesCounter = 0;
+  }
+
+  @Override
+  public void endWindow()
+  {
+  }
+
+  @Override
+  public void setup(Context.OperatorContext context)
+  {
+  }
+
+  @Override
+  public void teardown()
+  {
+  }
+
+  @Override
+  public void emitTuples()
+  {
+    while (tuplesCounter++ < maxTuplesPerWindow) {
+      try {
+
+        SalesEvent salesEvent = generateSalesEvent();
+        this.jsonBytes.emit(mapper.writeValueAsBytes(salesEvent));
+
+      } catch (Exception ex) {
+        throw new RuntimeException(ex);
+      }
+    }
+  }
+
+  SalesEvent generateSalesEvent() throws Exception {
+    SalesEvent salesEvent = new SalesEvent();
+    salesEvent.timestamp = System.currentTimeMillis();
+    salesEvent.productId = randomId(maxProductId);
+    salesEvent.customerId = randomId(maxCustomerId);
+    salesEvent.channelId = randomId(maxChannelId);
+    salesEvent.amount = randomAmount(minAmount, maxAmount);
+    if (addProductCategory) {
+      salesEvent.productCategory = 1 + (salesEvent.productId % maxProductCategories);
+    }
+    return salesEvent;
+  }
+
+  private int randomId(int max) {
+    // Provide safe default for invalid max
+    if (max < 1) return 1;
+    return 1 + random.nextInt(max);
+  }
+
+  // Generate random double with gaussian distribution between min and max and two decimal places
+  private double randomAmount(double min, double max) {
+    // Provide safe default for invalid min/max relationships
+    if (max <= min) return Math.floor(min * 100) / 100;
+    double mean = (min + max)/2.0;
+    double deviation = (max - min)/2.0/3.0;
+    double rawAmount;
+    do {
+      rawAmount = random.nextGaussian() * deviation + mean;
+    } while (rawAmount < minAmount || rawAmount > maxAmount);
+    return Math.floor(rawAmount * 100) / 100;
+  }
+
+
+  public long getMaxTuplesPerWindow() {
+    return maxTuplesPerWindow;
+  }
+
+  public void setMaxTuplesPerWindow(long maxTuplesPerWindow) {
+    this.maxTuplesPerWindow = maxTuplesPerWindow;
+  }
+
+  public int getMaxProductId() {
+    return maxProductId;
+  }
+
+  public void setMaxProductId(int maxProductId) {
+    this.maxProductId = maxProductId;
+  }
+
+  public int getMaxCustomerId() {
+    return maxCustomerId;
+  }
+
+  public void setMaxCustomerId(int maxCustomerId) {
+    this.maxCustomerId = maxCustomerId;
+  }
+
+  public int getMaxChannelId() {
+    return maxChannelId;
+  }
+
+  public void setMaxChannelId(int maxChannelId) {
+    this.maxChannelId = maxChannelId;
+  }
+
+  public double getMinAmount() {
+    return minAmount;
+  }
+
+  public void setMinAmount(double minAmount) {
+    this.minAmount = minAmount;
+  }
+
+  public double getMaxAmount() {
+    return maxAmount;
+  }
+
+  public void setMaxAmount(double maxAmount) {
+    this.maxAmount = maxAmount;
+  }
+
+  public boolean isAddProductCategory() {
+    return addProductCategory;
+  }
+
+  public void setAddProductCategory(boolean addProductCategory) {
+    this.addProductCategory = addProductCategory;
+  }
+
+  public int getMaxProductCategories() {
+    return maxProductCategories;
+  }
+
+  public void setMaxProductCategories(int maxProductCategories) {
+    this.maxProductCategories = maxProductCategories;
+  }
+}
---
+++ demos/dimensions/src/test/java/com/datatorrent/demos/dimensions/generic/JsonSalesGeneratorTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Tests {@link com.datatorrent.demos.dimensions.generic.JsonSalesGenerator}
+ */
+package com.datatorrent.demos.dimensions.generic;
+
+import com.datatorrent.lib.testbench.CollectorTestSink;
+import com.datatorrent.lib.util.TestUtils;
+import junit.framework.Assert;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+public class JsonSalesGeneratorTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(JsonSalesGeneratorTest.class);
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  @Test
+  public void testValidSettings() throws Exception
+  {
+    int minTuples = 1000;
+    int maxProductId = 100;
+    int maxCustomerId = 1000000;
+    int maxChannelId = 5;
+    double minAmount = 0.99;
+    double maxAmount = 100.00;
+
+
+    JsonSalesGenerator oper = new JsonSalesGenerator();
+    oper.setMaxTuplesPerWindow(minTuples);
+    oper.setMaxProductId(maxProductId);
+    oper.setMaxCustomerId(maxCustomerId);
+    oper.setMaxChannelId(maxChannelId);
+    oper.setMinAmount(minAmount);
+    oper.setMaxAmount(maxAmount);
+
+    CollectorTestSink<byte[]> sink = new CollectorTestSink<byte[]>();
+    TestUtils.setSink(oper.jsonBytes, sink);
+    oper.setup(null);
+    oper.beginWindow(0);
+    oper.emitTuples();
+    oper.endWindow();
+    oper.teardown();
+
+    // Validate emitted tuple count
+    LOG.debug("Emitted tuples count: {}", sink.collectedTuples.size());
+    Assert.assertTrue("Emitted tuples match minTuples", minTuples <= sink.collectedTuples.size());
+
+    int testSize = 20;
+    Random random = new Random();
+    for (int i=0; i<testSize; i++) {
+      // Select a JSON tuple at random for testing
+      int randomIndex = random.nextInt(sink.collectedTuples.size());
+      String jsonTuple =  new String(sink.collectedTuples.get(randomIndex));
+
+      LOG.debug("Validating tuple: {}", jsonTuple);
+      // Validate JSON structure
+      Assert.assertTrue("Data in valid JSON format",  isValidJSON(jsonTuple));
+
+      // Validate requested ranges
+      SalesEvent salesEvent = mapper.readValue(jsonTuple, SalesEvent.class);
+      Assert.assertTrue("customerId within range",  salesEvent.customerId > 0 && salesEvent.customerId <= maxCustomerId);
+      Assert.assertTrue("productId within range",  salesEvent.productId > 0 && salesEvent.productId <= maxProductId);
+      Assert.assertTrue("channelId within range",  salesEvent.channelId > 0 && salesEvent.channelId <= maxChannelId);
+      Assert.assertTrue("amount within range",  salesEvent.amount >= minAmount && salesEvent.amount <= maxAmount);
+      Assert.assertTrue("timestamp is valid", salesEvent.timestamp > 0 && salesEvent.timestamp <= System.currentTimeMillis());
+    }
+  }
+
+
+  @Test
+  public void testInvalidSettings() throws Exception
+  {
+    int minTuples = 1000;
+    int maxProductId = 1;
+    int maxCustomerId = -1; // Invalid value.  Should default to 1 for all customerId's
+    int maxChannelId = 0; // Invalid value.  Should default to 1 for all channelId's
+    double minAmount = 0.99;
+    double maxAmount = 0.98; // Invalid value.  Max should be bigger than min.  Should default to 0.99 for all amounts.
+
+
+    JsonSalesGenerator oper = new JsonSalesGenerator();
+    oper.setMaxTuplesPerWindow(minTuples);
+    oper.setMaxProductId(maxProductId);
+    oper.setMaxCustomerId(maxCustomerId);
+    oper.setMaxChannelId(maxChannelId);
+    oper.setMinAmount(minAmount);
+    oper.setMaxAmount(maxAmount);
+    oper.setAddProductCategory(true);
+
+    CollectorTestSink<byte[]> sink = new CollectorTestSink<byte[]>();
+    TestUtils.setSink(oper.jsonBytes, sink);
+    oper.setup(null);
+    oper.beginWindow(0);
+    oper.emitTuples();
+    oper.endWindow();
+    oper.teardown();
+
+    // Validate emitted tuple count
+    LOG.debug("Emitted tuples count: {}", sink.collectedTuples.size());
+    Assert.assertTrue("Emitted tuples match minTuples", minTuples <= sink.collectedTuples.size());
+
+    int testSize = 20;
+    Random random = new Random();
+    for (int i=0; i<testSize; i++) {
+      // Select a JSON tuple at random for testing
+      int randomIndex = random.nextInt(sink.collectedTuples.size());
+      String jsonTuple =  new String(sink.collectedTuples.get(randomIndex));
+
+      LOG.debug("Validating tuple: {}", jsonTuple);
+      // Validate JSON structure
+      Assert.assertTrue("Data in valid JSON format",  isValidJSON(jsonTuple));
+
+      // Validate requested ranges
+      SalesEvent salesEvent = mapper.readValue(jsonTuple, SalesEvent.class);
+      Assert.assertTrue("customerId within range",  salesEvent.customerId == 1);
+      Assert.assertTrue("productId within range",  salesEvent.productId == maxProductId);
+      Assert.assertTrue("channelId within range",  salesEvent.channelId == 1);
+      Assert.assertTrue("amount within range",  salesEvent.amount == minAmount);
+      Assert.assertTrue("timestamp is valid", salesEvent.timestamp > 0 && salesEvent.timestamp <= System.currentTimeMillis());
+    }
+  }
+
+  public boolean isValidJSON(final String json) {
+    boolean valid = false;
+    try {
+      final JsonParser parser = new ObjectMapper().getJsonFactory().createJsonParser(json);
+      while (parser.nextToken() != null) {
+        //Iterate over every available token
+      }
+      valid = true;
+    } catch (Exception e) {
+      LOG.error("Failed to parse json {}", json, e);
+    }
+    return valid;
+  }
+
+}
