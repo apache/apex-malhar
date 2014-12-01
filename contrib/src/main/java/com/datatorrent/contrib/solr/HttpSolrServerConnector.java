088004a0d4ce4d33799d1146af160f9fbc39775f
Priyanka Gugale <priyanka@datatorrent.com>
2014-12-01 14:54:33 +0530

    Adding Solr Operators: 1. Input Operator: Reads data from Solr server based on user defined queries. 2. Output Operator: Writes data to Solr server. 3. Unit tests which uses embedded Solr Server. 4. resources file which needs to initialize embedded solr server for unit testing.

---
+++ contrib/src/main/java/com/datatorrent/contrib/solr/HttpSolrServerConnector.java
@@ -0,0 +1,35 @@
+package com.datatorrent.contrib.solr;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+
+/**
+ * Initializes HttpSolrServer instance of Solr Server.<br>
+ * <br>
+ * properties:<br>
+ * baseURL - The URL of the Solr server.
+ */
+public class HttpSolrServerConnector extends SolrServerConnector
+{
+  @NotNull
+  private String solrServerURL;
+
+  @Override
+  public void connect()
+  {
+    solrServer = new HttpSolrServer(solrServerURL);
+  }
+
+  // set this property in dt-site.xml
+  public void setSolrServerURL(String solrServerURL)
+  {
+    this.solrServerURL = solrServerURL;
+  }
+
+  public String getSolrServerURL()
+  {
+    return solrServerURL;
+  }
+
+}
---
+++ contrib/src/main/java/com/datatorrent/contrib/solr/SolrInputOperator.java
@@ -0,0 +1,49 @@
+package com.datatorrent.contrib.solr;
+
+import java.util.Map;
+
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.servlet.SolrRequestParsers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Default Implementation of AbstractSolrInputOperator. Reads query from properties file <br>
+ */
+public class SolrInputOperator extends AbstractSolrInputOperator<Map<String, Object>, SolrServerConnector>
+{
+  private static final Logger logger = LoggerFactory.getLogger(SolrInputOperator.class);
+  private String solrQuery;
+
+  @Override
+  protected void emitTuple(SolrDocument document)
+  {
+    outputPort.emit(document.getFieldValueMap());
+  }
+
+  @Override
+  public SolrParams getQueryParams()
+  {
+    SolrParams solrParams;
+    if (solrQuery != null) {
+      solrParams = SolrRequestParsers.parseQueryString(solrQuery);
+    } else {
+      logger.debug("Solr document fetch query is not set, using wild card query for search.");
+      solrParams = SolrRequestParsers.parseQueryString("*");
+    }
+    return solrParams;
+  }
+
+  public String getSolrQuery()
+  {
+    return solrQuery;
+  }
+
+  // set this property in dt-site.xml
+  public void setSolrQuery(String solrQuery)
+  {
+    this.solrQuery = solrQuery;
+  }
+
+}
