6132341975234c09124ff37a57bf5abdf5dc1c94
yogidevendra <devendra@datatorrent.com>
2014-12-09 16:29:33 +0530

    Squashed commit for ElasticSearch input, output, percolate operator.

---
+++ contrib/src/main/java/com/datatorrent/contrib/elasticsearch/AbstractElasticSearchInputOperator.java
@@ -0,0 +1,92 @@
+/*
+ *  Copyright (c) 2012-2013 DataTorrent, Inc.
+ *  All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.*/
+
+package com.datatorrent.contrib.elasticsearch;
+
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.search.SearchHit;
+
+import com.datatorrent.api.Context.OperatorContext;
+import com.datatorrent.lib.db.AbstractStoreInputOperator;
+
+/**
+ * This is the base implementation for a non transactional input operator for ElasticSearch
+ * 
+ * <br>
+ * Ports:<br>
+ * <b>Input</b>: no input port<br>
+ * <b>Output</b>: You can have one output port<br>
+ * <br>
+ * Compile time checks:<br>
+ * None<br>
+ * <br>
+ * Run time checks:<br>
+ * hostName port<br>
+ * 
+ * <b>Benchmarks</b>: <br>
+ * 
+ * @displayName ElasticSearch input
+ * @category Store
+ * @tags input operator
+ */
+public abstract class AbstractElasticSearchInputOperator<T, S extends ElasticSearchConnectable> extends AbstractStoreInputOperator<T, S>
+{
+  protected transient SearchRequestBuilder searchRequestBuilder;
+
+  /**
+   * Initializing transient fields such as ElasticSearchConnectable, SearchRequestBuilder
+   * 
+   * @see com.datatorrent.lib.db.AbstractStoreInputOperator#setup(com.datatorrent.api.Context.OperatorContext)
+   */
+  @Override
+  public void setup(OperatorContext t1)
+  {
+    super.setup(t1);
+    this.searchRequestBuilder = new SearchRequestBuilder(store.client);
+  }
+
+  /**
+   * Emit one tuple per {@code SearchHit} for given search query
+   * 
+   * @see com.datatorrent.api.InputOperator#emitTuples()
+   */
+  @Override
+  public void emitTuples()
+  {
+    SearchResponse response = getSearchRequestBuilder().execute().actionGet();
+    for (SearchHit hit : response.getHits().hits()) {
+      outputPort.emit(convertToTuple(hit));
+    }
+  }
+
+  /**
+   * Converts SearchHit to Tuple
+   * 
+   * @param hit
+   * @return tuple constructed from <code>hit</code>
+   */
+  protected abstract T convertToTuple(SearchHit hit);
+
+  /**
+   * Set {@link SearchRequestBuilder} properties according to search query requirements. Properties which do not change
+   * for each window can be set in {@code setup()}. Properties which may change for each window should be set in this
+   * function
+   * 
+   * @return {@link SearchRequestBuilder}
+   */
+  protected abstract SearchRequestBuilder getSearchRequestBuilder();
+
+}
---
+++ contrib/src/test/java/com/datatorrent/contrib/elasticsearch/ElasticSearchPercolateTest.java
@@ -0,0 +1,137 @@
+/*
+ *  Copyright (c) 2012-2013 DataTorrent, Inc.
+ *  All Rights Reserved.
+ */
+package com.datatorrent.contrib.elasticsearch;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.percolate.PercolateResponse;
+import org.elasticsearch.action.percolate.PercolateResponse.Match;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datatorrent.lib.testbench.CollectorTestSink;
+
+/**
+ * Test class for percolate operator
+ */
+public class ElasticSearchPercolateTest
+{
+  private static final String INDEX_NAME = "mails";
+  private static final String DOCUMENT_TYPE = "emailText";
+  private static final String HOST_NAME = "localhost";
+  private static final int PORT = 9300;
+
+  private static final String GITHUB_TOPIC = "onGithub";
+  private static final String MALHAR_TOPIC = "onMalhar";
+
+  private static final Logger logger = LoggerFactory.getLogger(ElasticSearchPercolateTest.class);
+
+  private ElasticSearchPercolatorStore store;
+
+  @Before
+  public void setup() throws IOException
+  {
+    store = new ElasticSearchPercolatorStore(HOST_NAME, PORT);
+    store.connect();
+  }
+
+  @Test
+  public void testPercolate() throws IOException
+  {
+    registerPercolateQueries();
+    checkPercolateResponse();
+  }
+
+  /**
+   * Register percolate queries on ElasticSearch
+   * 
+   * @throws IOException
+   * 
+   */
+  private void registerPercolateQueries() throws IOException
+  {
+    store.registerPercolateQuery(INDEX_NAME, GITHUB_TOPIC, new TermQueryBuilder("content", "github"));
+    store.registerPercolateQuery(INDEX_NAME, MALHAR_TOPIC, new TermQueryBuilder("content", "malhar"));
+
+  }
+
+  /**
+   * 
+   */
+  private void checkPercolateResponse()
+  {
+    ElasticSearchPercolatorOperator oper = new ElasticSearchPercolatorOperator();
+    oper.hostName = HOST_NAME;
+    oper.port = PORT;
+    oper.indexName = INDEX_NAME;
+    oper.documentType = DOCUMENT_TYPE;
+    oper.setup(null);
+
+    String[] messages = { "{content:'This will match only with malhar'}",
+
+    "{content:'This will match only with github'}",
+
+    "{content:'This will match with both github and malhar'}",
+
+    "{content:'This will not match with any of them'}"
+
+    };
+
+    String[][] matches = {
+
+    { MALHAR_TOPIC },
+
+    { GITHUB_TOPIC },
+
+    { GITHUB_TOPIC, MALHAR_TOPIC },
+
+    {}
+
+    };
+
+    CollectorTestSink<PercolateResponse> sink = new CollectorTestSink<PercolateResponse>();
+    oper.outputPort.setSink((CollectorTestSink) sink);
+
+    for (String message : messages) {
+      oper.inputPort.process(message);
+    }
+
+    System.out.println(sink.collectedTuples);
+
+    int i = 0;
+    for (PercolateResponse response : sink.collectedTuples) {
+      List<String> matchIds = new ArrayList<String>();
+      for (Match match : response.getMatches()) {
+        matchIds.add(match.getId().toString());
+      }
+      Collections.sort(matchIds);
+      System.out.println("Matches for : "+ messages[i] + " = " + matchIds);
+      
+      Assert.assertArrayEquals(matchIds.toArray(), matches[i]);
+      i++;
+    }
+  }
+
+  @After
+  public void cleanup() throws IOException
+  {
+    DeleteIndexResponse delete = store.client.admin().indices().delete(new DeleteIndexRequest(INDEX_NAME)).actionGet();
+    if (!delete.isAcknowledged()) {
+      logger.error("Index wasn't deleted");
+    }
+
+    store.disconnect();
+  }
+}
