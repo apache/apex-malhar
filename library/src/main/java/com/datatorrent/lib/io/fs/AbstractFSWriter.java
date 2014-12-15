81a34f3f422e4ca67a81d697fb0893bcfd570ecf
Chandni Singh <chandni@datatorrent.com>
2014-12-15 00:33:26 -0800

    Lease expired exception with tmp files

--- library/src/main/java/com/datatorrent/lib/io/fs/AbstractFSWriter.java
+++ library/src/main/java/com/datatorrent/lib/io/fs/AbstractFSWriter.java
@@ -232,6 +232,10 @@ public abstract class AbstractFSWriter<INPUT> extends BaseOperator
       throw new RuntimeException(ex);
     }
 
+    if (replication <= 0) {
+      replication = fs.getDefaultReplication(new Path(filePath));
+    }
+
     LOG.debug("FS class {}", fs.getClass());
 
     //When an entry is removed from the cache, removal listener is notified and it closes the output stream.
@@ -263,9 +267,6 @@ public abstract class AbstractFSWriter<INPUT> extends BaseOperator
         Path lfilepath = new Path(filePath + File.separator + partFileName);
 
         FSDataOutputStream fsOutput;
-        if (replication <= 0) {
-          replication = fs.getDefaultReplication(lfilepath);
-        }
 
         boolean sawThisFileBefore = endOffsets.containsKey(filename);
 
@@ -349,7 +350,7 @@ public abstract class AbstractFSWriter<INPUT> extends BaseOperator
               LOG.info("file corrupted {} {} {}", seenFileNamePart, offset, status.getLen());
               byte[] buffer = new byte[COPY_BUFFER_SIZE];
 
-              Path tmpFilePath = new Path(seenFileNamePart + TMP_EXTENSION);
+              Path tmpFilePath = new Path(filePath, seenFileNamePart + TMP_EXTENSION);
               FSDataOutputStream fsOutput = fs.create(tmpFilePath, (short) replication);
               while (inputStream.getPos() < offset) {
                 long remainingBytes = offset - inputStream.getPos();
@@ -365,6 +366,7 @@ public abstract class AbstractFSWriter<INPUT> extends BaseOperator
               LOG.debug("temp file path {}, rolling file path {}", tmpFilePath.toString(), status.getPath().toString());
               fileContext.rename(tmpFilePath, status.getPath(), Options.Rename.OVERWRITE);
             }
+            inputStream.close();
           }
         }
       }
