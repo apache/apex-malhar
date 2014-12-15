623e6d68060a61fe46f3503adf42742e1115245e
thomas <thomas@datatorrent.com>
2014-12-15 15:17:47 -0800

    changes from 1.0.4

--- library/src/main/java/com/datatorrent/lib/io/fs/AbstractFSWriter.java
+++ library/src/main/java/com/datatorrent/lib/io/fs/AbstractFSWriter.java
@@ -16,7 +16,6 @@
 
 package com.datatorrent.lib.io.fs;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -264,7 +263,7 @@ public abstract class AbstractFSWriter<INPUT> extends BaseOperator
       public FSDataOutputStream load(String filename)
       {
         String partFileName = getPartFileNamePri(filename);
-        Path lfilepath = new Path(filePath + File.separator + partFileName);
+        Path lfilepath = new Path(filePath + Path.SEPARATOR + partFileName);
 
         FSDataOutputStream fsOutput;
 
@@ -339,7 +338,7 @@ public abstract class AbstractFSWriter<INPUT> extends BaseOperator
         for (String seenFileName: endOffsets.keySet()) {
           String seenFileNamePart = getPartFileNamePri(seenFileName);
           LOG.debug("seenFileNamePart: {}", seenFileNamePart);
-          Path seenPartFilePath = new Path(filePath + File.separator + seenFileNamePart);
+          Path seenPartFilePath = new Path(filePath + Path.SEPARATOR + seenFileNamePart);
           if (fs.exists(seenPartFilePath)) {
             LOG.debug("file exists {}", seenFileNamePart);
             long offset = endOffsets.get(seenFileName).longValue();
@@ -350,7 +349,7 @@ public abstract class AbstractFSWriter<INPUT> extends BaseOperator
               LOG.info("file corrupted {} {} {}", seenFileNamePart, offset, status.getLen());
               byte[] buffer = new byte[COPY_BUFFER_SIZE];
 
-              Path tmpFilePath = new Path(filePath + File.separator+ seenFileNamePart + TMP_EXTENSION);
+              Path tmpFilePath = new Path(filePath + Path.SEPARATOR + seenFileNamePart + TMP_EXTENSION);
               FSDataOutputStream fsOutput = fs.create(tmpFilePath, (short) replication);
               while (inputStream.getPos() < offset) {
                 long remainingBytes = offset - inputStream.getPos();
