cef6560ef095748662242fe07cfcdc06a4da83e8
gaurav <gaurav@datatorrent.com>
2014-12-29 17:27:48 -0800

    fixed the test cases

--- demos/mroperator/src/test/java/com/datatorrent/demos/mroperator/WordCountMRApplicationTest.java
+++ demos/mroperator/src/test/java/com/datatorrent/demos/mroperator/WordCountMRApplicationTest.java
@@ -15,15 +15,57 @@
  */
 package com.datatorrent.demos.mroperator;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import com.datatorrent.api.LocalMode;
 
+public class WordCountMRApplicationTest
+{
+  private static Logger LOG = LoggerFactory.getLogger(WordCountMRApplicationTest.class);
+  @Rule
+  public MapOperatorTest.TestMeta testMeta = new MapOperatorTest.TestMeta();
 
-public class WordCountMRApplicationTest {
-	@Test
-	public void testSomeMethod() throws Exception {
-		LocalMode.runApp(new NewWordCountApplication(), 20000000);
-	}
+  @Test
+  public void testSomeMethod() throws Exception
+  {
+    LocalMode lma = LocalMode.newInstance();
+    Configuration conf = new Configuration(false);
+    conf.set("dt.application.WordCountDemo.operator.Mapper.dirName", testMeta.testDir);
+    conf.setInt("dt.application.WordCountDemo.operator.Mapper.partitionCount", 1);
+    conf.set("dt.application.WordCountDemo.operator.Console.filePath", testMeta.testDir);
+    conf.set("dt.application.WordCountDemo.operator.Console.outputFileName", "output.txt");
+    lma.prepareDAG(new NewWordCountApplication(), conf);
+    LocalMode.Controller lc = lma.getController();
+    lc.setHeartbeatMonitoringEnabled(false);
+    lc.run(5000);
+    lc.shutdown();
+    List<String> readLines = FileUtils.readLines(new File(testMeta.testDir + "/output.txt"));
+    Map<String,Integer> readMap = Maps.newHashMap();
+    Iterator<String> itr = readLines.iterator();
+    while(itr.hasNext()){
+      String[] splits = itr.next().split("=");
+      readMap.put(splits[0],Integer.valueOf(splits[1]));
+    }
+    Map<String,Integer> expectedMap = Maps.newHashMap();
+    expectedMap.put("1",2);
+    expectedMap.put("2",2);
+    expectedMap.put("3",2);
+    Assert.assertEquals("expected reduced data ", expectedMap, readMap);
+    LOG.info("read lines {}", readLines);
+  }
 
 }
