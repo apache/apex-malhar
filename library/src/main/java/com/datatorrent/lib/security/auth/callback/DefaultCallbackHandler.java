d007faf57b6279a0bc91026ad8a57bae1467906c
Pramod Immaneni <pramod@datatorrent.com>
2015-01-18 10:22:06 -0800

    Added documentation and copyright

--- library/src/main/java/com/datatorrent/lib/security/auth/callback/DefaultCallbackHandler.java
+++ library/src/main/java/com/datatorrent/lib/security/auth/callback/DefaultCallbackHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.lib.security.auth.callback;
 
 import java.io.IOException;
@@ -13,7 +28,16 @@ import com.datatorrent.lib.security.SecurityContext;
 import com.datatorrent.api.Component;
 
 /**
-*/
+ * The default callback handler handles common callbacks and can be used with most common
+ * JAAS login modules. This handler can be extended to create custom callback handlers if
+ * needed. When developing a custom handler in this fashion most of time you would need to
+ * just override the processCallback method, handle the custom callback and delegate to the
+ * base class for the common callbacks.
+ * 
+ * This handler implements Component to interface to receive the login information via the
+ * SecurityContext. Custom handlers being implemented from scratch without extending this
+ * handler can also extend Component to receive security information.
+ */
 public class DefaultCallbackHandler implements CallbackHandler, Component<SecurityContext>
 {
   private static final Logger logger = LoggerFactory.getLogger(DefaultCallbackHandler.class);
