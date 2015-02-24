32480f1ab69280150303d4496c1622ba589d656d
Pramod Immaneni <pramod@datatorrent.com>
2015-02-24 14:22:21 -0800

    Fixed typo in pom and removed author

--- demos/echoserver/src/main/java/com/datatorrent/demos/echoserver/Message.java
+++ demos/echoserver/src/main/java/com/datatorrent/demos/echoserver/Message.java
@@ -17,9 +17,6 @@ package com.datatorrent.demos.echoserver;
 
 import java.net.SocketAddress;
 
-/**
- * Created by Pramod Immaneni <pramod@datatorrent.com> on 12/12/14.
- */
 public class Message
 {
   public String message;
