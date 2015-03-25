66565f5786f16106c7157e47a2d5b2a25c5de72b
Timothy Farkas <tim@datatorrent.com>
2015-03-25 13:11:49 -0700

    Using fixed timestamp.

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/generic/GenericInputItemGenerator.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/generic/GenericInputItemGenerator.java
@@ -90,7 +90,7 @@ public class GenericInputItemGenerator implements InputOperator
         //int adUnit = random.nextInt(AdsSchemaResult.LOCATIONS.length) + 1;
         int adUnit = random.nextInt(1) + 1;
 
-        timestamp = System.currentTimeMillis();
+        timestamp = 1000000L;//System.currentTimeMillis();
 
         if(i == blastCount - 1) {
           logger.info("advertiserId {}, publisherId {}, addUnit {}, timestamp {}",
