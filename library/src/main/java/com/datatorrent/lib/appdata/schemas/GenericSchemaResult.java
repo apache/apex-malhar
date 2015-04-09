49f598aeb5c9c281db468f349ac92b505d41f9a6
Timothy Farkas <tim@datatorrent.com>
2015-04-08 17:17:43 -0700

    Updated schemaData to schemaResult.

--- library/src/main/java/com/datatorrent/lib/appdata/schemas/GenericSchemaResult.java
+++ library/src/main/java/com/datatorrent/lib/appdata/schemas/GenericSchemaResult.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,7 +24,7 @@ import com.google.common.base.Preconditions;
 @DataSerializerInfo(clazz=GenericSchemaSerializer.class)
 public class GenericSchemaResult extends Result
 {
-  public static final String TYPE = "schemaData";
+  public static final String TYPE = "schemaResult";
 
   private GenericSchema genericSchema;
 
