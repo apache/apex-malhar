1b49b6a51f5e84adccd26e6963f63242dacf8eeb
Timothy Farkas <tim@datatorrent.com>
2015-04-15 14:56:25 -0700

    Debugging cache

--- contrib/src/main/java/com/datatorrent/contrib/dimensions/DimensionsStoreHDHT.java
+++ contrib/src/main/java/com/datatorrent/contrib/dimensions/DimensionsStoreHDHT.java
@@ -53,7 +53,7 @@ public abstract class DimensionsStoreHDHT extends AbstractSinglePortHDHTWriter<A
 {
   private static final Logger logger = LoggerFactory.getLogger(DimensionsStoreHDHT.class);
 
-  public static final int CACHE_SIZE = 50000;
+  public static final int CACHE_SIZE = Integer.MAX_VALUE;
   public static final int DEFAULT_KEEP_ALIVE_TIME = 20;
 
   //HDHT Aggregation parameters
@@ -334,6 +334,7 @@ public abstract class DimensionsStoreHDHT extends AbstractSinglePortHDHTWriter<A
     @Override
     public void onRemoval(RemovalNotification<EventKey, AggregateEvent> notification)
     {
+      logger.debug("REMOVING FROM CACHE.");
       AggregateEvent gae = notification.getValue();
 
       if(gae.isEmpty()) {
