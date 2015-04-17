96e84a5827a11297f3bbdb0fd177cafc9b5ecf85
Timothy Farkas <tim@datatorrent.com>
2015-04-17 15:29:29 -0700

    Added serialization test. Removed setting decimal format from properties file

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/generic/ApplicationWithHDHT.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/generic/ApplicationWithHDHT.java
@@ -139,6 +139,7 @@ public class ApplicationWithHDHT implements StreamingApplication
     }
 
     store.setFileStore(hdsFile);
+    store.getResultSerializerFactory().getAppDataFormatter().setDecimalFormatString("#.##");
     dag.setAttribute(store, Context.OperatorContext.COUNTERS_AGGREGATOR, new BasicCounters.LongAggregator< MutableLong >());
 
     String eventSchema = SchemaUtils.jarResourceFileToString(EVENT_SCHEMA);
