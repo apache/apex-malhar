40b0d0d8506945946fed532709e7b878e8d8d02b
Timothy Farkas <tim@datatorrent.com>
2015-04-23 18:30:55 -0700

    Added missing files

---
+++ library/src/main/java/com/datatorrent/lib/appdata/converter/MapGPOConverterSchema.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.datatorrent.lib.appdata.converter;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class MapGPOConverterSchema
+{
+  private Map<String, String> gpoFieldToMapField = Maps.newHashMap();
+
+  public MapGPOConverterSchema(String jsonString)
+  {
+    Preconditions.checkNotNull(jsonString);
+    JSONObject jo = null;
+
+    try {
+      jo = new JSONObject(jsonString);
+    }
+    catch(JSONException ex) {
+      throw new RuntimeException(ex);
+    }
+
+    Iterator keyIterator = jo.keys();
+
+    while(keyIterator.hasNext()) {
+      String key = (String) keyIterator.next();
+      String value = null;
+
+      try {
+        value = jo.getString((String) keyIterator.next());
+      }
+      catch(JSONException ex) {
+        throw new IllegalArgumentException(ex);
+      }
+
+      gpoFieldToMapField.put(key, value);
+    }
+  }
+
+  public String getMapField(String gpoField)
+  {
+    String mapField = gpoFieldToMapField.get(gpoField);
+
+    if(mapField == null) {
+      return gpoField;
+    }
+
+    return mapField;
+  }
+}
