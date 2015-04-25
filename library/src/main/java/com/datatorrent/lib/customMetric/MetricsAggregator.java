f1135f3e3ac7752a2430000815d49b78ffb7914b
Chandni Singh <chandni@datatorrent.com>
2015-04-25 13:35:46 -0700

    Added documentation to metrics aggregator

--- library/src/main/java/com/datatorrent/lib/customMetric/MetricsAggregator.java
+++ library/src/main/java/com/datatorrent/lib/customMetric/MetricsAggregator.java
@@ -31,16 +31,32 @@ import com.google.common.collect.Multimap;
 import com.datatorrent.api.CustomMetric;
 import com.datatorrent.api.annotation.Name;
 
+import com.datatorrent.lib.customMetric.max.DoubleMaxAggregator;
+import com.datatorrent.lib.customMetric.min.IntMinAggregator;
+import com.datatorrent.lib.customMetric.sum.LongSumAggregator;
+
+/**
+ * An easy to use {@link CustomMetric.Aggregator} that can be configured to perform multiple aggregations on each
+ * {@link CustomMetric} in an operator.
+ * <p/>
+ * The user needs to add an array of {@link SingleMetricAggregator}s for each metric. All the aggregators on each
+ * metric will be executed during aggregation.
+ * <p/>
+ * There are examples of {@link SingleMetricAggregator} provided in the library for common number
+ * aggregations- {@link LongSumAggregator}, {@link AvgAggregator}, {@link IntMinAggregator},
+ * {@link DoubleMaxAggregator}.
+ */
 public class MetricsAggregator implements CustomMetric.Aggregator, Serializable
 {
   protected static final String DEFAULT_SEPARATOR = "-";
 
-  protected final Map<String, List<AggregatorMeta>> metricsToAggregators;
+  //physical metric -> collection of logical metrics
+  protected final Map<String, List<LogicalMetricMeta>> metricLogicalAggregates;
   protected String aggregatorMetricSeparator;
 
   public MetricsAggregator()
   {
-    metricsToAggregators = Maps.newHashMap();
+    metricLogicalAggregates = Maps.newHashMap();
     aggregatorMetricSeparator = DEFAULT_SEPARATOR;
   }
 
@@ -57,12 +73,12 @@ public class MetricsAggregator implements CustomMetric.Aggregator, Serializable
 
     Map<String, Object> aggregates = Maps.newHashMap();
     for (String metric : metricValues.keySet()) {
-      List<AggregatorMeta> aggregatorMetas = metricsToAggregators.get(metric);
-      if (aggregatorMetas != null) {
-        for (AggregatorMeta aggregatorMeta : aggregatorMetas) {
+      List<LogicalMetricMeta> logicalMetricMetas = metricLogicalAggregates.get(metric);
+      if (logicalMetricMetas != null) {
+        for (LogicalMetricMeta logicalMetricMeta : logicalMetricMetas) {
 
-          Object aggregatedVal = aggregatorMeta.aggregator.aggregate(metricValues.get(metric));
-          aggregates.put(aggregatorMeta.aggregateKey, aggregatedVal);
+          Object aggregatedVal = logicalMetricMeta.aggregator.aggregate(metricValues.get(metric));
+          aggregates.put(logicalMetricMeta.name, aggregatedVal);
         }
       }
     }
@@ -70,13 +86,13 @@ public class MetricsAggregator implements CustomMetric.Aggregator, Serializable
   }
 
   /**
-   * This can be overridden to change name of aggregated key.
+   * This can be overridden to change logical metric name.
    *
    * @param metric     metric name
    * @param aggregator aggregator
-   * @return key of the aggregated value in the logical metrics.
+   * @return logical metric name which is assigned to the result of the aggregator.
    */
-  protected String deriveAggregateMetricKey(String metric, SingleMetricAggregator aggregator)
+  protected String deriveLogicalMetricName(String metric, SingleMetricAggregator aggregator)
   {
     Name aggregatorName = aggregator.getClass().getAnnotation(Name.class);
     String aggregatorDesc;
@@ -89,6 +105,14 @@ public class MetricsAggregator implements CustomMetric.Aggregator, Serializable
     return aggregatorDesc + aggregatorMetricSeparator + metric;
   }
 
+  /**
+   * Adds aggregators for a single physical metric.<br/>
+   * If there is only one aggregator, then the logical metric name is same as physical metric name.<br/>
+   * Otherwise logical metric names are derived using {@link #deriveLogicalMetricName}
+   *
+   * @param metric      physical metric name.
+   * @param aggregators aggregators for the metric.
+   */
   public void addAggregators(@NotNull String metric, @NotNull SingleMetricAggregator[] aggregators)
   {
     Preconditions.checkNotNull(metric, "metric");
@@ -96,27 +120,39 @@ public class MetricsAggregator implements CustomMetric.Aggregator, Serializable
     addAggregatorsHelper(metric, aggregators, null);
   }
 
+  /**
+   * Adds aggregators and logical metric names for a single physical metric.
+   *
+   * @param metric             physical metric name.
+   * @param aggregators        aggregators for the metric.
+   * @param logicalMetricNames logic metric names that will be used for an aggregated value. logicalMetricNames[i] will
+   *                           be used for the result of aggregators[i].
+   */
   public void addAggregators(@NotNull String metric, @NotNull SingleMetricAggregator[] aggregators,
-                             @NotNull String[] aggregateKeys)
+                             @NotNull String[] logicalMetricNames)
   {
     Preconditions.checkNotNull(metric, "metric");
     Preconditions.checkNotNull(aggregators, "aggregators");
-    Preconditions.checkNotNull(aggregateKeys, "result metric keys");
-    Preconditions.checkArgument(aggregators.length == aggregateKeys.length, "different length aggregators and aggregateKeys");
-    addAggregatorsHelper(metric, aggregators, aggregateKeys);
+    Preconditions.checkNotNull(logicalMetricNames, "logicalMetricNames");
+    Preconditions.checkArgument(aggregators.length == logicalMetricNames.length, "different length aggregators and" +
+      " logical names");
+    addAggregatorsHelper(metric, aggregators, logicalMetricNames);
   }
 
-  private void addAggregatorsHelper(String metric, SingleMetricAggregator[] aggregators, String[] aggregateKeys)
+  private void addAggregatorsHelper(String metric, SingleMetricAggregator[] aggregators, String[] logicalMetricNames)
   {
-    List<AggregatorMeta> laggregators = metricsToAggregators.get(metric);
+    List<LogicalMetricMeta> laggregators = metricLogicalAggregates.get(metric);
     if (laggregators == null) {
       laggregators = Lists.newArrayList();
-      metricsToAggregators.put(metric, laggregators);
+      metricLogicalAggregates.put(metric, laggregators);
     }
     for (int i = 0; i < aggregators.length; i++) {
 
-      laggregators.add(new AggregatorMeta(aggregators[i], (aggregateKeys == null || aggregateKeys[i] == null) ?
-        deriveAggregateMetricKey(metric, aggregators[i]) : aggregateKeys[i]));
+      String resultName = (logicalMetricNames == null || logicalMetricNames[i] == null) ?
+        (aggregators.length == 1 ? metric : deriveLogicalMetricName(metric, aggregators[i]))
+        : logicalMetricNames[i];
+
+      laggregators.add(new LogicalMetricMeta(aggregators[i], resultName));
     }
   }
 
@@ -130,16 +166,16 @@ public class MetricsAggregator implements CustomMetric.Aggregator, Serializable
     this.aggregatorMetricSeparator = aggregatorMetricSeparator;
   }
 
-  public static class AggregatorMeta implements Serializable
+  public static class LogicalMetricMeta implements Serializable
   {
     private SingleMetricAggregator aggregator;
 
-    private String aggregateKey;
+    private String name;
 
-    protected AggregatorMeta(@NotNull SingleMetricAggregator aggregator, @NotNull String aggregateKey)
+    protected LogicalMetricMeta(@NotNull SingleMetricAggregator aggregator, @NotNull String name)
     {
       this.aggregator = Preconditions.checkNotNull(aggregator, "aggregator");
-      this.aggregateKey = Preconditions.checkNotNull(aggregateKey, "result metric key");
+      this.name = Preconditions.checkNotNull(name, "logical metric name");
     }
 
     public SingleMetricAggregator getAggregator()
@@ -152,14 +188,14 @@ public class MetricsAggregator implements CustomMetric.Aggregator, Serializable
       this.aggregator = aggregator;
     }
 
-    public String getAggregateKey()
+    public String getName()
     {
-      return aggregateKey;
+      return name;
     }
 
-    protected void setAggregateKey(String aggregateKey)
+    protected void setName(String name)
     {
-      this.aggregateKey = aggregateKey;
+      this.name = name;
     }
 
     private static final long serialVersionUID = 201604231340L;
