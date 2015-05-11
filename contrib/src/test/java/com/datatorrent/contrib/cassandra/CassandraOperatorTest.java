c1bbd66f2603ee19515e0eabdd5418ab70935c86
Prerna Manaktala <prerna@node17.morado.com>
2015-05-11 09:40:03 -0700

    changes for test.

--- contrib/src/test/java/com/datatorrent/contrib/cassandra/CassandraOperatorTest.java
+++ contrib/src/test/java/com/datatorrent/contrib/cassandra/CassandraOperatorTest.java
@@ -23,13 +23,11 @@ import com.datatorrent.common.util.DTThrowable;
 import com.datatorrent.lib.helper.OperatorContextTestHelper;
 import com.datatorrent.lib.testbench.CollectorTestSink;
 import com.google.common.collect.Lists;
-import java.util.ArrayList;
+import java.util.*;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.UUID;
 import org.junit.AfterClass;
 
 /**
@@ -43,6 +41,8 @@ public class CassandraOperatorTest
   private static final String TABLE_NAME = "test";
   private static String APP_ID = "CassandraOperatorTest";
   private static int OPERATOR_ID = 0;
+  private static Cluster cluster = null;
+  private static Session session=null;
 
   private static class TestEvent
   {
@@ -52,16 +52,15 @@ public class CassandraOperatorTest
     {
       this.id = id;
     }
-
   }
 
   @BeforeClass
   public static void setup()
   {
     try {
-      Cluster cluster = Cluster.builder()
-              .addContactPoint(NODE).build();
-      Session session = cluster.connect(KEYSPACE);
+       cluster = Cluster.builder()
+          .addContactPoint(NODE).build();
+      session = cluster.connect(KEYSPACE);
 
       String createMetaTable = "CREATE TABLE IF NOT EXISTS " + CassandraTransactionalStore.DEFAULT_META_TABLE + " ( "
               + CassandraTransactionalStore.DEFAULT_APP_ID_COL + " TEXT, "
@@ -70,24 +69,37 @@ public class CassandraOperatorTest
               + "PRIMARY KEY (" + CassandraTransactionalStore.DEFAULT_APP_ID_COL + ", " + CassandraTransactionalStore.DEFAULT_OPERATOR_ID_COL + ") "
               + ");";
       session.execute(createMetaTable);
-      String createTable = "CREATE TABLE IF NOT EXISTS " + KEYSPACE + "." + TABLE_NAME + " (id uuid PRIMARY KEY,firstname text,lastname text);";
+      String createTable = "CREATE TABLE IF NOT EXISTS " + KEYSPACE + "." + TABLE_NAME + " (id uuid PRIMARY KEY,age int,lastname text,test boolean,floatvalue float,doubleValue double,set1 set<int>,list1 list<int>,map1 map<text,int>);";
       session.execute(createTable);
     }
     catch (Throwable e) {
-
       DTThrowable.rethrow(e);
     }
   }
 
-  //@AfterClass
-  public static void cleanTable()
+  @AfterClass
+  public static void cleanup()
+  {
+   if(session!=null)
+   {
+     session.execute("DROP TABLE " + CassandraTransactionalStore.DEFAULT_META_TABLE);
+     session.execute("DROP TABLE " +  KEYSPACE + "." + TABLE_NAME);
+     session.close();
+   }
+    if(cluster!=null)
+    {
+      cluster.close();
+    }
+  }
+
+  private static void cleanTable()
   {
     try {
       Cluster cluster = Cluster.builder()
-              .addContactPoint(NODE).build();
+          .addContactPoint(NODE).build();
       Session session = cluster.connect(KEYSPACE);
 
-      String cleanTable = "DROP " + TABLE_NAME + ";";
+      String cleanTable = "TRUNCATE " + TABLE_NAME + ";";
       session.execute(cleanTable);
     }
     catch (DriverException e) {
@@ -98,17 +110,23 @@ public class CassandraOperatorTest
   private static class TestOutputOperator extends CassandraOutputOperator
   {
 
+    TestOutputOperator()
+    {
+      //cleanTable();
+    }
+
     public long getNumOfEventsInStore()
     {
 
       try {
         Cluster cluster = Cluster.builder()
-                .addContactPoint(NODE).build();
+            .addContactPoint(NODE).build();
         Session session = cluster.connect(KEYSPACE);
 
         String countQuery = "SELECT count(*) from " + TABLE_NAME + ";";
-        ResultSet resultSet = session.execute(countQuery);
-        for (Row row: resultSet) {
+        ResultSet resultSetCount = session.execute(countQuery);
+        for(Row row: resultSetCount)
+        {
           return row.getLong(0);
         }
         return 0;
@@ -118,12 +136,31 @@ public class CassandraOperatorTest
       }
     }
 
+    public String getEventsInStore()
+    {
+      try {
+        Cluster cluster = Cluster.builder()
+            .addContactPoint(NODE).build();
+        Session session = cluster.connect(KEYSPACE);
+
+        String recordsQuery = "SELECT * from " + TABLE_NAME + ";";
+        ResultSet resultSetRecords = session.execute(recordsQuery);
+         for(Row row: resultSetRecords)
+        {
+          System.out.println("result is "+ row.getBool("test") + "," + row.getString("lastname"));
+        }
+        return null;
+      }
+      catch (DriverException e) {
+        throw new RuntimeException("fetching records", e);
+      }
+    }
   }
 
   private static class TestInputOperator extends AbstractCassandraInputOperator<TestEvent>
   {
 
-    private static final String retrieveQuery = "SELECT * FROM " + KEYSPACE + "." + TABLE_NAME + ";";
+    private static final String retrieveQuery = "SELECT * FROM " +KEYSPACE +"."+TABLE_NAME + ";";
 
     TestInputOperator()
     {
@@ -151,10 +188,10 @@ public class CassandraOperatorTest
     {
       try {
         Cluster cluster = Cluster.builder()
-                .addContactPoint(NODE).build();
+            .addContactPoint(NODE).build();
         Session session = cluster.connect(KEYSPACE);
 
-        String insert = "INSERT INTO " + TABLE_NAME + " (ID)" + " VALUES (?);";
+        String insert = "INSERT INTO " + TABLE_NAME +" (ID)"+ " VALUES (?);";
         PreparedStatement stmt = session.prepare(insert);
         BoundStatement boundStatement = new BoundStatement(stmt);
         Statement statement;
@@ -167,8 +204,8 @@ public class CassandraOperatorTest
         throw new RuntimeException(e);
       }
     }
-
   }
+
 
   @Test
   public void testCassandraOutputOperator()
@@ -186,14 +223,36 @@ public class CassandraOperatorTest
     outputOperator.setTablename(TABLE_NAME);
     ArrayList<String> columns = new ArrayList<String>();
     columns.add("id");
-    columns.add("firstname");
-    columns.add("lastname");
+    columns.add("age");
+   // columns.add("lastname");
+  //  columns.add("test");
+  //  columns.add("floatValue");
+    columns.add("doubleValue");
+    columns.add("floatValue");
+     columns.add("lastname");
+   // columns.add("date");
+   // columns.add("set1");
+    columns.add("list1");
+    columns.add("map1");
+    columns.add("set1");
+    columns.add("test");
     // columns.add("age");
     outputOperator.setColumns(columns);
     ArrayList<String> expressions = new ArrayList<String>();
     expressions.add("id");
-    expressions.add("firstname");
+    expressions.add("age");
+ //   expressions.add("lastname");
+ //   expressions.add("test");
+  //  expressions.add("floatValue");
+    expressions.add("doubleValue");
+    expressions.add("floatValue");
     expressions.add("lastname");
+    //expressions.add("date");
+    //expressions.add("set1");
+    expressions.add("list1");
+    expressions.add("map1");
+    expressions.add("set1");
+   expressions.add("test");
     // expressions.add("age");
     outputOperator.setExpressions(expressions);
     outputOperator.setStore(transactionalStore);
@@ -202,7 +261,13 @@ public class CassandraOperatorTest
 
     List<TestPojo> events = Lists.newArrayList();
     for (int i = 0; i < 10; i++) {
-      events.add(new TestPojo(UUID.randomUUID(), "abc" + i, "abclast" + i));
+      Set<Integer> set  = new HashSet<Integer>();
+      set.add(i);
+      List<Integer> list = new ArrayList<Integer>();
+      list.add(i);
+      Map<String,Integer> map = new HashMap<String, Integer>();
+      map.put("key"+i, i);
+      events.add(new TestPojo(UUID.randomUUID(), i, "abclast" + i,true,i,2.0,set,list,map));
     }
 
     outputOperator.beginWindow(0);
@@ -210,11 +275,18 @@ public class CassandraOperatorTest
       outputOperator.input.process(event);
     }
     outputOperator.endWindow();
-    System.out.println("rows in db are " + outputOperator.getNumOfEventsInStore());
+
     Assert.assertEquals("rows in db", 10, outputOperator.getNumOfEventsInStore());
+    outputOperator.getEventsInStore();
+    for (int i = 0; i < 10; i++) {
+
+      //StringBuilder firstlastname = new StringBuilder("abc"+i+","+"abclast"+i);
+      //Assert.assertEquals(firstlastname.toString(),outputOperator.getEventsInStore());
+     }
+
   }
 
- // @Test
+ //@Test
   public void TestCassandraInputOperator()
   {
     CassandraStore store = new CassandraStore();
@@ -242,9 +314,110 @@ public class CassandraOperatorTest
 
   public static class TestPojo
   {
-    public String firstname = "xyz";
-    public String lastname = "hello";
-    public UUID id;
+    private int age = 2;
+
+    private TestPojo(UUID randomUUID, int i, String string, boolean b, float d, double d0, Set<Integer> set1, List<Integer> list1, Map<String, Integer> map1)
+    {
+      this.id = randomUUID;
+      this.age = i;
+      this.lastname = string;
+      this.test = b;
+      this.floatValue = d;
+      this.doubleValue = d0;
+     // this.date = date;
+      this.set1 = set1;
+      this.list1 = list1;
+      this.map1 = map1;
+    }
+
+    public int getAge()
+    {
+      return age;
+    }
+
+    public void setAge(int age)
+    {
+      this.age = age;
+    }
+
+    public boolean isTest()
+    {
+      return test;
+    }
+
+    public void setTest(boolean test)
+    {
+      this.test = test;
+    }
+
+    public Set getSet1()
+    {
+      return set1;
+    }
+
+    public void setSet1(Set set)
+    {
+      this.set1 = set;
+    }
+
+    public List getList1()
+    {
+      return list1;
+    }
+
+    public void setList1(List list)
+    {
+      this.list1 = list;
+    }
+
+    public Map<String, Integer> getMap1()
+    {
+      return map1;
+    }
+
+    public void setMap1(Map<String, Integer> map)
+    {
+      this.map1 = map;
+    }
+
+    public Date getDate()
+    {
+      return date;
+    }
+
+    public void setDate(Date date)
+    {
+      this.date = date;
+    }
+
+    public Double getDoubleValue()
+    {
+      return doubleValue;
+    }
+
+    public void setDoubleValue(Double doubleValue)
+    {
+      this.doubleValue = doubleValue;
+    }
+
+    public Float getFloatValue()
+    {
+      return floatValue;
+    }
+
+    public void setFloatValue(Float floatValue)
+    {
+      this.floatValue = floatValue;
+    }
+    private String lastname = "hello";
+    private UUID id;
+    private boolean test;
+    private Set set1;
+    private List list1;
+    private Map<String,Integer> map1;
+    private Date date;
+    private Double doubleValue;
+    private Float  floatValue;
 
     public UUID getId()
     {
@@ -254,13 +427,6 @@ public class CassandraOperatorTest
     public void setId(UUID id)
     {
       this.id = id;
-    }
-
-    private TestPojo(UUID id, String string, String string0)
-    {
-      this.id = id;
-      firstname = string;
-      lastname = string0;
     }
 
     public String getLastname()
@@ -273,17 +439,10 @@ public class CassandraOperatorTest
       this.lastname = lastname;
     }
 
-    public String getFirstname()
-    {
-      return firstname;
-    }
 
-    public void setFirstname(String firstname)
-    {
-      this.firstname = firstname;
-    }
 
   }
 
 }
+
 
