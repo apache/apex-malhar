80deae9eb41dca83f0b0ec23564e674e21ef89b1
Pramod Immaneni <pramod@datatorrent.com>
2015-05-19 19:07:12 -0700

    Some name and package refactoring. Added copyright

--- contrib/src/test/java/com/datatorrent/contrib/io/LzoFilterStreamTest.java
+++ contrib/src/test/java/com/datatorrent/contrib/io/LzoFilterStreamTest.java
@@ -1,4 +1,20 @@
-package com.datatorrent.contrib.lib.io;
+/*
+ * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.datatorrent.contrib.io;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,7 +32,7 @@ import com.datatorrent.lib.helper.OperatorContextTestHelper;
 import com.datatorrent.lib.io.fs.AbstractFileOutputOperator;
 import com.datatorrent.lib.util.TestUtils.TestInfo;
 
-public class LzoStreamCodecTest
+public class LzoFilterStreamTest
 {
   public static OperatorContextTestHelper.TestIdOperatorContext testOperatorContext = new OperatorContextTestHelper.TestIdOperatorContext(0);
   @Rule
@@ -65,7 +81,7 @@ public class LzoStreamCodecTest
   public void testLZOCompression() throws Exception
   {
     FileOutputOperator writer = new FileOutputOperator("compressedData.txt.lzo");
-    writer.setFilterStreamProvider(new LzoStreamCodec.LZOFilterStreamProvider());
+    writer.setFilterStreamProvider(new LzoFilterStream.LZOFilterStreamProvider());
 
     writer.setFilePath(testMeta.getDir());
     writer.setup(testOperatorContext);
