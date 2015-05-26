ba7a89fb94c706565f94c208b6f5c17d92e1d069
Timothy Farkas <tim@datatorrent.com>
2015-05-25 19:38:04 -0700

    Fixed serialization

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/AdInfo.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/AdInfo.java
@@ -1,6 +1,17 @@
 /*
- *  Copyright (c) 2012-2015 Malhar, Inc.
- *  All Rights Reserved.
+ * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.datatorrent.demos.dimensions.ads;
@@ -10,15 +21,14 @@ import com.datatorrent.lib.dimensions.AbstractDimensionsComputation;
 import com.datatorrent.lib.dimensions.AbstractDimensionsComputation.UnifiableAggregate;
 import com.datatorrent.lib.statistics.DimensionsComputation;
 import com.datatorrent.lib.statistics.DimensionsComputation.Aggregator;
+import java.io.Serializable;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- *
- * @author Timothy Farkas: tim@datatorrent.com
- */
-public class AdInfo
+public class AdInfo implements Serializable
 {
+  private static final long serialVersionUID = 201505250652L;
+
   public String publisher;
   public String advertiser;
   public String location;
@@ -460,6 +470,4 @@ public class AdInfo
       this.aggregatorIndex = aggregateIndex;
     }
   }
-
-
 }
