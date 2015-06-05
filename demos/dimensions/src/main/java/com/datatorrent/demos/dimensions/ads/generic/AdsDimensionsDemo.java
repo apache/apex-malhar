37d70df8e155c1adb51c03481372476e567b2cac
Timothy Farkas <tim@datatorrent.com>
2015-06-05 09:20:47 -0700

    Made checkpoint window count the same.

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/generic/AdsDimensionsDemo.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/generic/AdsDimensionsDemo.java
@@ -140,6 +140,7 @@ public class AdsDimensionsDemo implements StreamingApplication
     DimensionsComputationFlexibleSingleSchemaPOJO dimensions = dag.addOperator("DimensionsComputation", DimensionsComputationFlexibleSingleSchemaPOJO.class);
     dimensions.setUnifier(new DimensionsComputationUnifierImpl<InputEvent, Aggregate>());
     dag.getMeta(dimensions).getAttributes().put(Context.OperatorContext.APPLICATION_WINDOW_COUNT, 4);
+    dag.getMeta(dimensions).getAttributes().put(Context.OperatorContext.CHECKPOINT_WINDOW_COUNT, 4);
     AppDataSingleSchemaDimensionStoreHDHT store = dag.addOperator("Store", AppDataSingleSchemaDimensionStoreHDHT.class);
 
     //Set operator properties
