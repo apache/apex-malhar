5f7fc1f1681a99976c4090dc4b75f14ecf57f95b
Timothy Farkas <tim@datatorrent.com>
2015-06-04 20:45:42 -0700

    Made the random data generation for ads better.

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/InputItemGenerator.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/InputItemGenerator.java
@@ -42,6 +42,8 @@ public class InputItemGenerator implements InputOperator
   public static final String COST = "cost";
   public static final String REVENUE = "revenue";
 
+  public static final double EXPECTED_CLICK_THROUGH_RATE = .015;
+
   private int publisherID;
   private int advertiserID;
   private int locationID;
@@ -50,7 +52,10 @@ public class InputItemGenerator implements InputOperator
   public List<Object> advertiserName;
   public List<Object> locationName;
 
-  private double expectedClickThruRate = 0.015;
+  private double dayOffset;
+  private double expectedClickThruRate;
+  private long currentMinute;
+  private long currentDay;
   @Min(1)
   private int blastCount = 30000;
   @Min(1)
@@ -59,9 +64,9 @@ public class InputItemGenerator implements InputOperator
   private final Random random = new Random(0);
   public final transient DefaultOutputPort<AdInfo> outputPort = new DefaultOutputPort<AdInfo>();
 
-  private double[] publisherScaleArray = new double[publisherID];
-  private double[] advertiserScaleArray = new double[advertiserID];
-  private double[] locationScaleArray = new double[locationID];
+  private double[] publisherScaleArray;
+  private double[] advertiserScaleArray;
+  private double[] locationScaleArray;
 
   public double getExpectedClickThruRate()
   {
@@ -116,6 +121,22 @@ public class InputItemGenerator implements InputOperator
       advertiserName = schema.getKeysToEnumValuesList().get(ADVERTISER);
     }
     locationName = schema.getKeysToEnumValuesList().get(LOCATION);
+
+    publisherScaleArray = new double[publisherID];
+    initializeScaleArray(publisherScaleArray);
+    advertiserScaleArray = new double[advertiserID];
+    initializeScaleArray(advertiserScaleArray);
+    locationScaleArray = new double[locationID];
+    initializeScaleArray(locationScaleArray);
+  }
+
+  private void initializeScaleArray(double[] scaleArray)
+  {
+    for(int index = 0;
+        index < scaleArray.length;
+        index++) {
+      scaleArray[index] = 1.0 + random.nextDouble() * 3;
+    }
   }
 
   @Override
@@ -126,6 +147,21 @@ public class InputItemGenerator implements InputOperator
   @Override
   public void emitTuples()
   {
+    long time = System.currentTimeMillis();
+    long nextMinute = time % 60000;
+    long nextDay = time % (24 * 60 * 60 * 1000);
+    double dayTimeOffset = .2 * (Math.cos(2.0 * Math.PI / (24.0 * 60.0 * 60.0 * 1000.0) * ((double) nextDay)) *.5 + 1.0);
+
+    if(nextDay != currentDay) {
+      currentDay = nextDay;
+      dayOffset = random.nextDouble() * .05;
+    }
+
+    if(nextMinute != currentMinute) {
+      expectedClickThruRate = random.nextDouble() * .01 + EXPECTED_CLICK_THROUGH_RATE;
+      currentMinute = nextMinute;
+    }
+
     try {
       long timestamp;
       for (int i = 0; i < blastCount && windowCount < numTuplesPerWindow; ++i, windowCount++) {
@@ -135,13 +171,16 @@ public class InputItemGenerator implements InputOperator
 
         timestamp = System.currentTimeMillis();
 
-        double cost = 0.5 + 0.25 * random.nextDouble();
+        double tempScale = publisherScaleArray[publisherId] *
+                           advertiserScaleArray[advertiserId] *
+                           locationScaleArray[adUnit];
 
+        double cost = 0.5 + 0.25 * random.nextDouble() * tempScale + dayTimeOffset + dayOffset;
         /* 0 (zero) is used as the invalid value */
         buildAndSend(false, publisherId, advertiserId, adUnit, cost, timestamp);
 
         if (random.nextDouble() < expectedClickThruRate) {
-          double revenue = 0.5 + 0.5 * random.nextDouble();
+          double revenue = 0.5 + 0.5 * random.nextDouble() * tempScale + dayTimeOffset + dayOffset;
           // generate fake click
           buildAndSend(true, publisherId, advertiserId, adUnit, revenue, timestamp);
         }
