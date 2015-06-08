5c5787ad5d964712c807864e8fa0793e88937b95
Timothy Farkas <tim@datatorrent.com>
2015-06-08 10:19:15 -0700

    Updated Sales demo to use correct dimensions computation operator.

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/sales/generic/GenericSalesDemoWithHDHT.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/sales/generic/GenericSalesDemoWithHDHT.java
@@ -15,7 +15,7 @@ import com.datatorrent.contrib.dimensions.AppDataSingleSchemaDimensionStoreHDHT;
 import com.datatorrent.contrib.hdht.tfile.TFileImpl;
 import com.datatorrent.lib.appdata.schemas.SchemaUtils;
 import com.datatorrent.lib.counters.BasicCounters;
-import com.datatorrent.lib.dimensions.DimensionsComputationFlexibleSingleSchemaPOJO;
+import com.datatorrent.lib.dimensions.DimensionsComputationFlexibleSingleSchemaMap;
 import com.datatorrent.lib.io.PubSubWebSocketAppDataQuery;
 import com.datatorrent.lib.io.PubSubWebSocketAppDataResult;
 import java.net.URI;
@@ -51,8 +51,8 @@ public class GenericSalesDemoWithHDHT implements StreamingApplication
   {
     JsonSalesGenerator input = dag.addOperator("InputGenerator", JsonSalesGenerator.class);
     JsonToMapConverter converter = dag.addOperator("Converter", JsonToMapConverter.class);
-    DimensionsComputationFlexibleSingleSchemaPOJO dimensions =
-    dag.addOperator("DimensionsComputation", DimensionsComputationFlexibleSingleSchemaPOJO.class);
+    DimensionsComputationFlexibleSingleSchemaMap dimensions =
+    dag.addOperator("DimensionsComputation", DimensionsComputationFlexibleSingleSchemaMap.class);
 
     dag.getMeta(dimensions).getAttributes().put(Context.OperatorContext.APPLICATION_WINDOW_COUNT, 4);
     AppDataSingleSchemaDimensionStoreHDHT store = dag.addOperator("Store", AppDataSingleSchemaDimensionStoreHDHT.class);
