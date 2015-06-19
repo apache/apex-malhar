0154d25e9d0f1babc9d1c8457d19532e6f9967bd
Chandni Singh <chandni@datatorrent.com>
2015-06-19 00:26:59 -0700

    SPOI-5148: custom metric

---
+++ library/src/main/java/com/datatorrent/lib/customMetric/min/LongMinAggregator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.lib.customMetric.min;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import com.datatorrent.api.annotation.Name;
+
+import com.datatorrent.lib.customMetric.SingleMetricAggregator;
+
+@Name("min")
+public class LongMinAggregator implements SingleMetricAggregator, Serializable
+{
+  @Override
+  public Object aggregate(Collection<Object> metricValues)
+  {
+    Long min = null;
+    for (Object value : metricValues) {
+      long lval = ((Number) value).longValue();
+      if (min == null || lval < min) {
+        min = lval;
+      }
+    }
+    return min;
+  }
+
+  private static final long serialVersionUID = 201504081319L;
+}
