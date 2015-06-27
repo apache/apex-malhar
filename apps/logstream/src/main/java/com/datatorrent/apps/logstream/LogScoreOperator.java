27cf2d1316d115ef8dbef2ec0944db97b041a779
CI Support <jenkins@datatorrent.com>
2015-06-26 20:03:02 -0700

    fixed the incorrect package omission

--- apps/logstream/src/main/java/com/datatorrent/apps/logstream/LogScoreOperator.java
+++ apps/logstream/src/main/java/com/datatorrent/apps/logstream/LogScoreOperator.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.datatorrent.apps.logstream;
+
 import com.datatorrent.common.util.BaseOperator;
 import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.lib.logs.DimensionObject;
