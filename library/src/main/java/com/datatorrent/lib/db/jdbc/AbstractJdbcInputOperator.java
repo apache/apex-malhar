bd84a40d1b11327cb39f226ee5a805dc2a31296b
Timothy Farkas <tim@datatorrent.com>
2015-07-28 13:24:26 -0700

    Remove unnecessary import

--- library/src/main/java/com/datatorrent/lib/db/jdbc/AbstractJdbcInputOperator.java
+++ library/src/main/java/com/datatorrent/lib/db/jdbc/AbstractJdbcInputOperator.java
@@ -23,7 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.datatorrent.api.Context.OperatorContext;
-import com.datatorrent.api.DefaultOutputPort;
 
 import com.datatorrent.lib.db.AbstractStoreInputOperator;
 
