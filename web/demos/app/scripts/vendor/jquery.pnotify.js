e999ce2cf73fe9f7547070ff1a2a7098534155a5
MalharJenkins <jenkins@datatorrent.com>
2015-10-03 22:30:34 -0700

    MLHR-1857 Auto format license headers.

--- web/demos/app/scripts/vendor/jquery.pnotify.js
+++ web/demos/app/scripts/vendor/jquery.pnotify.js
@@ -1,15 +1,21 @@
 /*
- * jQuery Pines Notify (pnotify) Plugin 1.2.0
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * http://pinesframework.org/pnotify/
- * Copyright (c) 2009-2012 Hunter Perrin
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Triple license under the GPL, LGPL, and MPL:
- *	  http://www.gnu.org/licenses/gpl.html
- *	  http://www.gnu.org/licenses/lgpl.html
- *	  http://www.mozilla.org/MPL/MPL-1.1.html
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 (function($) {
 	var history_handle_top,
 		timer,
--- web/demos/app/scripts/vendor/markerwithlabel.js
+++ web/demos/app/scripts/vendor/markerwithlabel.js
@@ -1,22 +1,21 @@
-/**
- * @name MarkerWithLabel for V3
- * @version 1.1.8 [February 26, 2013]
- * @author Gary Little (inspired by code from Marc Ridey of Google).
- * @copyright Copyright 2012 Gary Little [gary at luxcentral.com]
- * @fileoverview MarkerWithLabel extends the Google Maps JavaScript API V3
- *  <code>google.maps.Marker</code> class.
- *  <p>
- *  MarkerWithLabel allows you to define markers with associated labels. As you would expect,
- *  if the marker is draggable, so too will be the label. In addition, a marker with a label
- *  responds to all mouse events in the same manner as a regular marker. It also fires mouse
- *  events and "property changed" events just as a regular marker would. Version 1.1 adds
- *  support for the raiseOnDrag feature introduced in API V3.3.
- *  <p>
- *  If you drag a marker by its label, you can cancel the drag and return the marker to its
- *  original position by pressing the <code>Esc</code> key. This doesn't work if you drag the marker
- *  itself because this feature is not (yet) supported in the <code>google.maps.Marker</code> class.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 /*!
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
