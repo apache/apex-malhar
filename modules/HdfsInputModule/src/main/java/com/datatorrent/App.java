4f3d70b4bf55d4001c6bcc9ee2c8edb49da90bc8
Priyanka Gugale <priyanka@datatorrent.com>
2015-11-05 15:37:18 +0530

    Adding packages for modules

---
+++ modules/HdfsInputModule/src/main/java/com/datatorrent/App.java
@@ -0,0 +1,13 @@
+package com.datatorrent;
+
+/**
+ * Placeholder class
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ modules/HdfsInputModule/src/test/java/com/datatorrent/AppTest.java
@@ -0,0 +1,15 @@
+package com.datatorrent;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+  @Test
+  public void dummyTest()
+  {
+
+  }
+}
