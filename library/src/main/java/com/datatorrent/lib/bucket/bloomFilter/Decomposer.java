0178ce1578ca83cdcc1df7c975955808b7f9abb8
Chaitanya <chaitanya@datatorrent.com>
2015-11-17 14:32:39 +0530

    Adding Bloom Filter Implementation

---
+++ library/src/main/java/com/datatorrent/lib/bucket/bloomFilter/Decomposer.java
@@ -0,0 +1,46 @@
+package com.datatorrent.lib.bucket.bloomFilter;
+
+import java.nio.charset.Charset;
+
+/**
+ * Contract for implementations that wish to help in decomposing an object to a
+ * byte-array so that various hashes can be computed over the same.
+ *
+ * @param <T>
+ *          the type of object over which this decomposer works
+ */
+public interface Decomposer<T>
+{
+
+  /**
+   * Decompose the object into the byte-array
+   *
+   * @param object
+   *          the object to be decomposed
+   */
+  public byte[] decompose(T object);
+}
+
+class DefaultDecomposer<T> implements Decomposer<T>
+{
+  /**
+   * The default platform encoding
+   */
+  private static final Charset DEFAULT_CHARSET = Charset.defaultCharset();
+
+  /**
+   * Decompose the object
+   */
+  @Override
+  public byte[] decompose(T object)
+  {
+    if (object == null) {
+      return null;
+    }
+    if (object instanceof String) {
+      return (((String)object).getBytes(DEFAULT_CHARSET));
+    }
+    return (object.toString().getBytes(DEFAULT_CHARSET));
+  }
+
+}
