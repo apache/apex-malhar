ecfd683ef452a1a846ac73880ae6135bbe793bec
David Yan <david@datatorrent.com>
2015-12-08 09:11:56 -0800

    simulate write ahead log

--- demos/iteration/src/main/java/com/datatorrent/demos/iteration/Application.java
+++ demos/iteration/src/main/java/com/datatorrent/demos/iteration/Application.java
@@ -29,6 +29,11 @@ import com.datatorrent.common.util.BaseOperator;
 import com.datatorrent.common.util.SimpleDelayOperator;
 import com.datatorrent.lib.io.ConsoleOutputOperator;
 import com.datatorrent.lib.testbench.RandomEventGenerator;
+import com.datatorrent.stram.util.LRUCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 /**
  * Iteration demo : <br>
@@ -59,6 +64,8 @@ import com.datatorrent.lib.testbench.RandomEventGenerator;
 @ApplicationAnnotation(name="IterationDemo")
 public class Application implements StreamingApplication
 {
+  private final static Logger LOG = LoggerFactory.getLogger(Application.class);
+
   public static class FibonacciOperator extends BaseOperator
   {
     public long currentNumber = 1;
@@ -75,7 +82,7 @@ public class Application implements StreamingApplication
       @Override
       public void process(Long tuple)
       {
-        tempNum = tuple;
+        tempNum = (currentNumber == 1) ? 1 : tuple;
       }
     };
     public transient DefaultOutputPort<Long> output = new DefaultOutputPort<>();
@@ -86,7 +93,49 @@ public class Application implements StreamingApplication
     {
       output.emit(currentNumber);
       currentNumber += tempNum;
+      if (currentNumber <= 0) {
+        currentNumber = 1;
+      }
     }
+  }
+
+  public static class FibMap extends LRUCache<Long, Long>
+  {
+    public FibMap()
+    {
+      super(1000, true);
+    }
+  }
+
+  public static class FibDelayOperator extends SimpleDelayOperator<Long>
+  {
+
+    public Map<Long, Long> windowToData = new FibMap();
+    public long windowId;
+
+    @Override
+    public void firstWindow(long windowId)
+    {
+      if (windowToData.containsKey(windowId)) {
+        Long tuple = windowToData.get(windowId);
+        LOG.info("RECOVERING {} -> {}", windowId, tuple);
+        output.emit(tuple);
+      }
+    }
+
+    @Override
+    public void beginWindow(long windowId)
+    {
+      this.windowId = windowId;
+    }
+
+    @Override
+    protected void processTuple(Long tuple)
+    {
+      windowToData.put(windowId, tuple);
+      super.processTuple(tuple);
+    }
+
   }
 
   @Override
@@ -94,7 +143,7 @@ public class Application implements StreamingApplication
   {
     RandomEventGenerator rand = dag.addOperator("rand", new RandomEventGenerator());
     FibonacciOperator fib = dag.addOperator("FIB", FibonacciOperator.class);
-    SimpleDelayOperator opDelay = dag.addOperator("opDelay", SimpleDelayOperator.class);
+    SimpleDelayOperator opDelay = dag.addOperator("opDelay", FibDelayOperator.class);
     ConsoleOutputOperator console = dag.addOperator("console", new ConsoleOutputOperator());
     dag.addStream("dummy_to_operator", rand.integer_data, fib.dummyInputPort);
     dag.addStream("operator_to_delay", fib.output, opDelay.input, console.input);
