e65a5751746a38e4144d2973aba7093c9e081889
Timothy Farkas <tim@datatorrent.com>
2015-12-30 18:02:13 +0530

    MLHR-1919 #resolve #comment add dimension schema to malhar

---
+++ library/src/test/java/com/datatorrent/lib/dimensions/DimensionsEventTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2015 DataTorrent, Inc.
+ * All rights reserved.
+ */
+package com.datatorrent.lib.dimensions;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.datatorrent.lib.appdata.gpo.GPOMutable;
+import com.datatorrent.lib.appdata.schemas.FieldsDescriptor;
+import com.datatorrent.lib.appdata.schemas.Type;
+import com.datatorrent.lib.dimensions.DimensionsEvent.EventKey;
+
+public class DimensionsEventTest
+{
+  @Test
+  public void eventKeyEqualsHashCodeTest()
+  {
+    Map<String, Type> fieldToTypeA = Maps.newHashMap();
+    fieldToTypeA.put("a", Type.LONG);
+    fieldToTypeA.put("b", Type.STRING);
+
+    FieldsDescriptor fdA = new FieldsDescriptor(fieldToTypeA);
+
+    GPOMutable gpoA = new GPOMutable(fdA);
+    gpoA.setField("a", 1L);
+    gpoA.setField("b", "hello");
+
+    EventKey eventKeyA = new EventKey(1, 1, 1, gpoA);
+
+    Map<String, Type> fieldToTypeB = Maps.newHashMap();
+    fieldToTypeB.put("a", Type.LONG);
+    fieldToTypeB.put("b", Type.STRING);
+
+    FieldsDescriptor fdB = new FieldsDescriptor(fieldToTypeB);
+
+    GPOMutable gpoB = new GPOMutable(fdB);
+    gpoB.setField("a", 1L);
+    gpoB.setField("b", "hello");
+
+    EventKey eventKeyB = new EventKey(1, 1, 1, gpoB);
+
+    Assert.assertEquals("The two hashcodes should equal", eventKeyA.hashCode(), eventKeyB.hashCode());
+    Assert.assertEquals("The two event keys should equal", eventKeyA, eventKeyB);
+  }
+}
---
+++ library/src/test/java/com/datatorrent/lib/dimensions/aggregator/AggregatorRegistryTest.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2015 DataTorrent, Inc.
+ * All rights reserved.
+ */
+package com.datatorrent.lib.dimensions.aggregator;
+
+import java.util.List;
+import java.util.Map;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.datatorrent.lib.dimensions.aggregator.AggregatorAverage;
+import com.datatorrent.lib.dimensions.aggregator.AggregatorCount;
+import com.datatorrent.lib.dimensions.aggregator.AggregatorRegistry;
+import com.datatorrent.lib.dimensions.aggregator.AggregatorSum;
+import com.datatorrent.lib.dimensions.aggregator.IncrementalAggregator;
+import com.datatorrent.lib.dimensions.aggregator.OTFAggregator;
+import com.datatorrent.lib.util.TestUtils;
+
+public class AggregatorRegistryTest
+{
+  @Test
+  public void serializationTest() throws Exception
+  {
+    TestUtils.clone(new Kryo(), AggregatorRegistry.DEFAULT_AGGREGATOR_REGISTRY);
+  }
+
+  @Test
+  public void metaDataTest()
+  {
+    Map<String, IncrementalAggregator> nameToIncrementalAggregator = Maps.newHashMap();
+    nameToIncrementalAggregator.put("SUM", new AggregatorSum());
+    nameToIncrementalAggregator.put("COUNT", new AggregatorCount());
+
+    Map<String, OTFAggregator> nameToOTFAggregator = Maps.newHashMap();
+    nameToOTFAggregator.put("AVG", AggregatorAverage.INSTANCE);
+
+    Map<String, Integer> nameToID = Maps.newHashMap();
+    nameToID.put("SUM", 0);
+    nameToID.put("COUNT", 1);
+
+    AggregatorRegistry aggregatorRegistry = new AggregatorRegistry(nameToIncrementalAggregator,
+                                                                   nameToOTFAggregator,
+                                                                   nameToID);
+
+    aggregatorRegistry.setup();
+
+    Map<Class<? extends IncrementalAggregator>, String> classToStaticAggregator =
+    aggregatorRegistry.getClassToIncrementalAggregatorName();
+
+    Assert.assertEquals("Incorrect number of elements.", 2, classToStaticAggregator.size());
+    Assert.assertEquals(classToStaticAggregator.get(AggregatorSum.class), "SUM");
+    Assert.assertEquals(classToStaticAggregator.get(AggregatorCount.class), "COUNT");
+
+    Assert.assertEquals(AggregatorAverage.class, nameToOTFAggregator.get("AVG").getClass());
+
+    Map<String, List<String>> otfAggregatorToStaticAggregators =
+    aggregatorRegistry.getOTFAggregatorToIncrementalAggregators();
+
+    Assert.assertEquals("Only 1 OTF aggregator", 1, otfAggregatorToStaticAggregators.size());
+    Assert.assertEquals(otfAggregatorToStaticAggregators.get("AVG"), Lists.newArrayList("SUM","COUNT"));
+  }
+}
