24fdbdfcfe37ef211563173952a676a73110dcd6
Chaitanya <chaitanya@datatorrent.com>
2016-01-06 10:52:04 +0530

    MLHR-1956 Added POJO Kafka Output operator with autometrics and batch processing

--- contrib/src/test/java/com/datatorrent/contrib/kafka/KafkaOutputOperatorTest.java
+++ contrib/src/test/java/com/datatorrent/contrib/kafka/KafkaOutputOperatorTest.java
@@ -182,5 +182,72 @@ public class KafkaOutputOperatorTest extends KafkaOperatorTestBase
     listener.close();
   }
 
+  /**
+   * Test AbstractKafkaOutputOperator (i.e. an output adapter for Kafka, aka producer).
+   * This module sends data into kafka message bus.
+   *
+   * [Generate tuple] ==> [send tuple through Kafka output adapter(i.e. producer) into Kafka message bus]
+   * ==> [receive data in outside Kaka listener (i.e consumer)]
+   *
+   * @throws Exception
+   */
+  @Test
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  public void testPOJOKafkaOutputOperator() throws Exception
+  {
+    tupleCount = 0;
+    //initialize the latch to synchronize the threads
+    latch = new CountDownLatch(maxTuple);
+    // Setup a message listener to receive the message
+    KafkaTestConsumer listener = new KafkaTestConsumer("topic1");
+    listener.setLatch(latch);
+    new Thread(listener).start();
 
+    // Create DAG for testing.
+    LocalMode lma = LocalMode.newInstance();
+
+    StreamingApplication app = new StreamingApplication() {
+      @Override
+      public void populateDAG(DAG dag, Configuration conf)
+      {
+      }
+    };
+
+    DAG dag = lma.getDAG();
+
+    StringGeneratorInputOperator generator = dag.addOperator("TestStringGenerator", StringGeneratorInputOperator.class);
+    POJOKafkaOutputOperator node = dag.addOperator("KafkaMessageProducer", POJOKafkaOutputOperator.class);
+
+    Properties props = new Properties();
+    props.setProperty("serializer.class", "kafka.serializer.StringEncoder");
+    props.setProperty("producer.type", "async");
+    props.setProperty("queue.buffering.max.ms", "200");
+    props.setProperty("queue.buffering.max.messages", "10");
+
+    node.setConfigProperties(props);
+    node.setTopic("topic1");
+    node.setBrokerList("localhost:9092");
+    node.setBatchSize(5);
+
+    // Connect ports
+    dag.addStream("Kafka message", generator.outputPort, node.inputPort).setLocality(DAG.Locality.CONTAINER_LOCAL);
+
+    Configuration conf = new Configuration(false);
+    lma.prepareDAG(app, conf);
+
+    // Create local cluster
+    final LocalMode.Controller lc = lma.getController();
+    lc.runAsync();
+
+    // Immediately return unless latch timeout in 20 seconds
+    latch.await(20, TimeUnit.SECONDS);
+    lc.shutdown();
+
+    // Check values send vs received
+    Assert.assertEquals("Number of emitted tuples", maxTuple, listener.holdingBuffer.size());
+    logger.debug(String.format("Number of emitted tuples: %d", listener.holdingBuffer.size()));
+    Assert.assertEquals("First tuple", "testString 1", listener.getMessage(listener.holdingBuffer.peek()));
+
+    listener.close();
+  }
 }
