313ad0d08443290e34034c705dda099c40e3e6af
Priyanka Gugale <priyanka@datatorrent.com>
2016-04-06 18:15:50 +0530

    APEXMALHAR-2045: Adding bandwidth control feature to limit Input Operator bandwidth limit

---
+++ library/src/test/java/com/datatorrent/lib/bandwidth/ScheduledExecutorTestService.java
@@ -0,0 +1,140 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.datatorrent.lib.bandwidth;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class ScheduledExecutorTestService implements ScheduledExecutorService
+{
+  private Runnable command;
+  @Override
+  public void shutdown()
+  {
+
+  }
+
+  @Override
+  public List<Runnable> shutdownNow()
+  {
+    return null;
+  }
+
+  @Override
+  public boolean isShutdown()
+  {
+    return false;
+  }
+
+  @Override
+  public boolean isTerminated()
+  {
+    return false;
+  }
+
+  @Override
+  public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException
+  {
+    return false;
+  }
+
+  @Override
+  public <T> Future<T> submit(Callable<T> task)
+  {
+    return null;
+  }
+
+  @Override
+  public <T> Future<T> submit(Runnable task, T result)
+  {
+    return null;
+  }
+
+  @Override
+  public Future<?> submit(Runnable task)
+  {
+    return null;
+  }
+
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException
+  {
+    return null;
+  }
+
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException
+  {
+    return null;
+  }
+
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException
+  {
+    return null;
+  }
+
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException
+  {
+    return null;
+  }
+
+  @Override
+  public void execute(Runnable command)
+  {
+    this.command.run();
+  }
+
+  @Override
+  public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit)
+  {
+    this.command = command;
+    return null;
+  }
+
+  @Override
+  public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit)
+  {
+    return null;
+  }
+
+  @Override
+  public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit)
+  {
+    this.command = command;
+    return null;
+  }
+
+  @Override
+  public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit)
+  {
+    this.command = command;
+    return null;
+  }
+
+}
+
