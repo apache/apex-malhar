3df6715d7aec3094b7794ae7becd2e12dbeda4b3
shubham <shubham-pathak22@github.com>
2016-06-01 18:47:36 +0530

    APEXMALHAR-2105 enhancing CSV formatter to read field info from schema

--- contrib/src/main/java/com/datatorrent/contrib/formatter/CsvFormatter.java
+++ contrib/src/main/java/com/datatorrent/contrib/formatter/CsvFormatter.java
@@ -20,7 +20,7 @@ package com.datatorrent.contrib.formatter;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
@@ -34,7 +34,13 @@ import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
+import com.google.common.annotations.VisibleForTesting;
+
+import com.datatorrent.api.AutoMetric;
 import com.datatorrent.api.Context;
+import com.datatorrent.contrib.parser.DelimitedSchema;
+import com.datatorrent.contrib.parser.DelimitedSchema.Field;
+import com.datatorrent.contrib.parser.DelimitedSchema.FieldType;
 import com.datatorrent.lib.formatter.Formatter;
 import com.datatorrent.netlet.util.DTThrowable;
 
@@ -44,10 +50,14 @@ import com.datatorrent.netlet.util.DTThrowable;
  * java type.<br>
  * <br>
  * <b>Properties</b> <br>
- * <b>fieldInfo</b>:User need to specify fields and their types as a comma
- * separated string having format &lt;NAME&gt;:&lt;TYPE&gt;|&lt;FORMAT&gt; in
- * the same order as incoming data. FORMAT refers to dates with dd/mm/yyyy as
- * default e.g name:string,dept:string,eid:integer,dateOfJoining:date|dd/mm/yyyy
+ * <b>schema</b>:schema as a string specified in a json format as per
+ * {@link DelimitedSchema}. Currently only Date constraint (fmt) is supported
+ * <br>
+ * <b>clazz</b>:Pojo class <br>
+ * <b>Ports</b> <br>
+ * <b>in</b>:input tuple as a POJO. Each tuple represents a record<br>
+ * <b>out</b>:tuples are are converted to string are emitted on this port<br>
+ * <b>err</b>:tuples that could not be converted are emitted on this port<br>
  * 
  * @displayName CsvFormatter
  * @category Formatter
@@ -58,215 +68,152 @@ import com.datatorrent.netlet.util.DTThrowable;
 public class CsvFormatter extends Formatter<String>
 {
 
-  private ArrayList<Field> fields;
-  @NotNull
-  protected String classname;
-  @NotNull
-  protected int fieldDelimiter;
-  protected String lineDelimiter;
+  /**
+   * Names of all the fields in the same order that would appear in output
+   * records
+   */
+  private transient String[] nameMapping;
+  /**
+   * Cell processors are an integral part of reading and writing with Super CSV
+   * they automate the data type conversions, and enforce constraints.
+   */
+  private transient CellProcessor[] processors;
+  /**
+   * Writing preferences that are passed through schema
+   */
+  private transient CsvPreference preference;
 
+  /**
+   * Contents of the schema.Schema is specified in a json format as per
+   * {@link DelimitedSchema}
+   */
   @NotNull
-  protected String fieldInfo;
+  private String schema;
+  /**
+   * Schema is read into this object to access fields
+   */
+  private transient DelimitedSchema delimitedParserSchema;
 
-  public enum FIELD_TYPE
+  /**
+   * metric to keep count of number of tuples emitted on error port port
+   */
+  @AutoMetric
+  private long errorTupleCount;
+
+  /**
+   * metric to keep count of number of tuples emitted on out port
+   */
+  @AutoMetric
+  private long emittedObjectCount;
+
+  /**
+   * metric to keep count of number of tuples emitted on input port
+   */
+  @AutoMetric
+  private long incomingTuplesCount;
+
+  @Override
+  public void beginWindow(long windowId)
   {
-    BOOLEAN, DOUBLE, INTEGER, FLOAT, LONG, SHORT, CHARACTER, STRING, DATE
-  };
-
-  protected transient String[] nameMapping;
-  protected transient CellProcessor[] processors;
-  protected transient CsvPreference preference;
-
-  public CsvFormatter()
-  {
-    fields = new ArrayList<Field>();
-    fieldDelimiter = ',';
-    lineDelimiter = "\r\n";
-
+    super.beginWindow(windowId);
+    errorTupleCount = 0;
+    emittedObjectCount = 0;
+    incomingTuplesCount = 0;
   }
 
   @Override
   public void setup(Context.OperatorContext context)
   {
     super.setup(context);
-
-    //fieldInfo information
-    fields = new ArrayList<Field>();
-    String[] fieldInfoTuple = fieldInfo.split(",");
-    for (int i = 0; i < fieldInfoTuple.length; i++) {
-      String[] fieldTuple = fieldInfoTuple[i].split(":");
-      Field field = new Field();
-      field.setName(fieldTuple[0]);
-      String[] typeFormat = fieldTuple[1].split("\\|");
-      field.setType(typeFormat[0].toUpperCase());
-      if (typeFormat.length > 1) {
-        field.setFormat(typeFormat[1]);
-      }
-      getFields().add(field);
-    }
-    preference = new CsvPreference.Builder('"', fieldDelimiter, lineDelimiter).build();
-    int countKeyValue = getFields().size();
-    nameMapping = new String[countKeyValue];
-    processors = new CellProcessor[countKeyValue];
-    initialise(nameMapping, processors);
-
+    delimitedParserSchema = new DelimitedSchema(schema);
+    preference = new CsvPreference.Builder(delimitedParserSchema.getQuoteChar(),
+        delimitedParserSchema.getDelimiterChar(), delimitedParserSchema.getLineDelimiter()).build();
+    nameMapping = delimitedParserSchema.getFieldNames()
+        .toArray(new String[delimitedParserSchema.getFieldNames().size()]);
+    processors = getProcessor(delimitedParserSchema.getFields());
   }
 
-  private void initialise(String[] nameMapping, CellProcessor[] processors)
+  /**
+   * Returns array of cellprocessors, one for each field
+   */
+  private CellProcessor[] getProcessor(List<Field> fields)
   {
-    for (int i = 0; i < getFields().size(); i++) {
-      FIELD_TYPE type = getFields().get(i).type;
-      nameMapping[i] = getFields().get(i).name;
-      if (type == FIELD_TYPE.DATE) {
-        String dateFormat = getFields().get(i).format;
-        processors[i] = new Optional(new FmtDate(dateFormat == null ? "dd/MM/yyyy" : dateFormat));
+    CellProcessor[] processor = new CellProcessor[fields.size()];
+    int fieldCount = 0;
+    for (Field field : fields) {
+      if (field.getType() == FieldType.DATE) {
+        String format = field.getConstraints().get(DelimitedSchema.DATE_FORMAT) == null ? null
+            : (String)field.getConstraints().get(DelimitedSchema.DATE_FORMAT);
+        processor[fieldCount++] = new Optional(new FmtDate(format == null ? "dd/MM/yyyy" : format));
       } else {
-        processors[i] = new Optional();
+        processor[fieldCount++] = new Optional();
       }
     }
-
+    return processor;
   }
 
   @Override
   public String convert(Object tuple)
   {
+    incomingTuplesCount++;
+    if (tuple == null) {
+      errorTupleCount++;
+      logger.error(" Null tuple", tuple);
+      return null;
+    }
     try {
       StringWriter stringWriter = new StringWriter();
       ICsvBeanWriter beanWriter = new CsvBeanWriter(stringWriter, preference);
       beanWriter.write(tuple, nameMapping, processors);
       beanWriter.flush();
       beanWriter.close();
+      emittedObjectCount++;
       return stringWriter.toString();
     } catch (SuperCsvException e) {
-      logger.debug("Error while converting tuple {} {}",tuple,e.getMessage());
+      logger.error("Error while converting tuple {} {}", tuple, e.getMessage());
+      errorTupleCount++;
     } catch (IOException e) {
       DTThrowable.rethrow(e);
     }
     return null;
   }
 
-  public static class Field
+  /**
+   * Get the schema
+   * 
+   * @return schema
+   */
+  public String getSchema()
   {
-    String name;
-    String format;
-    FIELD_TYPE type;
-
-    public String getName()
-    {
-      return name;
-    }
-
-    public void setName(String name)
-    {
-      this.name = name;
-    }
-
-    public FIELD_TYPE getType()
-    {
-      return type;
-    }
-
-    public void setType(String type)
-    {
-      this.type = FIELD_TYPE.valueOf(type);
-    }
-
-    public String getFormat()
-    {
-      return format;
-    }
-
-    public void setFormat(String format)
-    {
-      this.format = format;
-    }
+    return schema;
   }
 
   /**
-   * Gets the array list of the fields, a field being a POJO containing the name
-   * of the field and type of field.
+   * Set the schema
    * 
-   * @return An array list of Fields.
+   * @param schema
    */
-  public ArrayList<Field> getFields()
+  public void setSchema(String schema)
   {
-    return fields;
+    this.schema = schema;
   }
 
-  /**
-   * Sets the array list of the fields, a field being a POJO containing the name
-   * of the field and type of field.
-   * 
-   * @param fields
-   *          An array list of Fields.
-   */
-  public void setFields(ArrayList<Field> fields)
+  @VisibleForTesting
+  protected long getErrorTupleCount()
   {
-    this.fields = fields;
+    return errorTupleCount;
   }
 
-  /**
-   * Gets the delimiter which separates fields in incoming data.
-   * 
-   * @return fieldDelimiter
-   */
-  public int getFieldDelimiter()
+  @VisibleForTesting
+  protected long getEmittedObjectCount()
   {
-    return fieldDelimiter;
+    return emittedObjectCount;
   }
 
-  /**
-   * Sets the delimiter which separates fields in incoming data.
-   * 
-   * @param fieldDelimiter
-   */
-  public void setFieldDelimiter(int fieldDelimiter)
+  @VisibleForTesting
+  protected long getIncomingTuplesCount()
   {
-    this.fieldDelimiter = fieldDelimiter;
-  }
-
-  /**
-   * Gets the delimiter which separates lines in incoming data.
-   * 
-   * @return lineDelimiter
-   */
-  public String getLineDelimiter()
-  {
-    return lineDelimiter;
-  }
-
-  /**
-   * Sets the delimiter which separates line in incoming data.
-   * 
-   * @param lineDelimiter
-   */
-  public void setLineDelimiter(String lineDelimiter)
-  {
-    this.lineDelimiter = lineDelimiter;
-  }
-
-  /**
-   * Gets the name of the fields with type and format in data as comma separated
-   * string in same order as incoming data. e.g
-   * name:string,dept:string,eid:integer,dateOfJoining:date|dd/mm/yyyy
-   * 
-   * @return fieldInfo
-   */
-  public String getFieldInfo()
-  {
-    return fieldInfo;
-  }
-
-  /**
-   * Sets the name of the fields with type and format in data as comma separated
-   * string in same order as incoming data. e.g
-   * name:string,dept:string,eid:integer,dateOfJoining:date|dd/mm/yyyy
-   * 
-   * @param fieldInfo
-   */
-  public void setFieldInfo(String fieldInfo)
-  {
-    this.fieldInfo = fieldInfo;
+    return incomingTuplesCount;
   }
 
   private static final Logger logger = LoggerFactory.getLogger(CsvFormatter.class);
