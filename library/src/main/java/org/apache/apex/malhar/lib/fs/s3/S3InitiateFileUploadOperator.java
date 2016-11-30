a5e8fa3facca750f5d7402c2c29e7cbabe53bd9e
chaitanya <chaithu@apache.org>
2016-11-30 10:47:36 +0530

    APEXMALHAR-2022 Development of S3 Output Module

---
+++ library/src/main/java/org/apache/apex/malhar/lib/fs/s3/S3InitiateFileUploadOperator.java
@@ -0,0 +1,378 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.apex.malhar.lib.fs.s3;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.apex.malhar.lib.wal.FSWindowDataManager;
+import org.apache.apex.malhar.lib.wal.WindowDataManager;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.classification.InterfaceStability;
+import org.apache.hadoop.fs.Path;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.google.common.base.Preconditions;
+
+import com.datatorrent.api.Context;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.Operator;
+import com.datatorrent.lib.io.fs.AbstractFileSplitter;
+
+/**
+ * This is an S3 Initiate file upload operator which can be used to initiate file upload and emits the upload id.
+ * Initiate the given file for upload only if the file contains more than one block.
+ * This operator is useful in context of S3 Output Module.
+ */
+@InterfaceStability.Evolving
+public class S3InitiateFileUploadOperator implements Operator, Operator.CheckpointNotificationListener
+{
+  @NotNull
+  private String bucketName;
+  @NotNull
+  private String accessKey;
+  @NotNull
+  private String secretAccessKey;
+  private String endPoint;
+  @NotNull
+  private String outputDirectoryPath;
+  private WindowDataManager windowDataManager = new FSWindowDataManager();
+  protected transient AmazonS3 s3Client;
+  protected transient long currentWindowId;
+  protected transient List<UploadFileMetadata> currentWindowRecoveryState;
+
+  public final transient DefaultOutputPort<UploadFileMetadata> fileMetadataOutput = new DefaultOutputPort<>();
+
+  public final transient DefaultOutputPort<UploadFileMetadata> uploadMetadataOutput = new DefaultOutputPort<>();
+
+  /**
+   * This input port receive file metadata and those files will be upload into S3.
+   */
+  public final transient DefaultInputPort<AbstractFileSplitter.FileMetadata> filesMetadataInput = new DefaultInputPort<AbstractFileSplitter.FileMetadata>()
+  {
+    @Override
+    public void process(AbstractFileSplitter.FileMetadata tuple)
+    {
+      processTuple(tuple);
+    }
+  };
+
+  /**
+   * For the input file, initiate the upload and emit the UploadFileMetadata through the fileMetadataOutput,
+   * uploadMetadataOutput ports.
+   * @param tuple given tuple
+   */
+  protected void processTuple(AbstractFileSplitter.FileMetadata tuple)
+  {
+    if (currentWindowId <= windowDataManager.getLargestCompletedWindow()) {
+      return;
+    }
+    String keyName = getKeyName(tuple.getFilePath());
+    String uploadId = "";
+    if (tuple.getNumberOfBlocks() > 1) {
+      InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucketName, keyName);
+      initRequest.setObjectMetadata(createObjectMetadata());
+      InitiateMultipartUploadResult initResponse = s3Client.initiateMultipartUpload(initRequest);
+      uploadId = initResponse.getUploadId();
+    }
+    UploadFileMetadata uploadFileMetadata = new UploadFileMetadata(tuple, uploadId, keyName);
+    fileMetadataOutput.emit(uploadFileMetadata);
+    uploadMetadataOutput.emit(uploadFileMetadata);
+    currentWindowRecoveryState.add(uploadFileMetadata);
+  }
+
+  /**
+   * Creates the empty object metadata for initiate multipart upload request.
+   * @return the ObjectMetadata
+   */
+  public ObjectMetadata createObjectMetadata()
+  {
+    return new ObjectMetadata();
+  }
+
+  @Override
+  public void setup(Context.OperatorContext context)
+  {
+    outputDirectoryPath = StringUtils.removeEnd(outputDirectoryPath, Path.SEPARATOR);
+    currentWindowRecoveryState = new ArrayList<>();
+    windowDataManager.setup(context);
+    s3Client = createClient();
+  }
+
+  /**
+   * Create AmazonS3 client using AWS credentials
+   * @return AmazonS3
+   */
+  protected AmazonS3 createClient()
+  {
+    AmazonS3 client = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretAccessKey));
+    if (endPoint != null) {
+      client.setEndpoint(endPoint);
+    }
+    return client;
+  }
+
+  /**
+   * Generates the key name from the given file path and output directory path.
+   * @param filePath file path to upload
+   * @return key name for the given file
+   */
+  private String getKeyName(String filePath)
+  {
+    return outputDirectoryPath + Path.SEPARATOR + StringUtils.removeStart(filePath, Path.SEPARATOR);
+  }
+
+  @Override
+  public void beginWindow(long windowId)
+  {
+    currentWindowId = windowId;
+    if (windowId <= windowDataManager.getLargestCompletedWindow()) {
+      replay(windowId);
+    }
+  }
+
+  @Override
+  public void endWindow()
+  {
+    if (currentWindowId > windowDataManager.getLargestCompletedWindow()) {
+      try {
+        windowDataManager.save(currentWindowRecoveryState, currentWindowId);
+      } catch (IOException e) {
+        throw new RuntimeException("Unable to save recovery", e);
+      }
+    }
+    currentWindowRecoveryState.clear();
+  }
+
+  @Override
+  public void teardown()
+  {
+    windowDataManager.teardown();
+  }
+
+  protected void replay(long windowId)
+  {
+    try {
+      @SuppressWarnings("unchecked")
+      List<UploadFileMetadata> recoveredData = (List<UploadFileMetadata>)windowDataManager.retrieve(windowId);
+      if (recoveredData != null) {
+        for (UploadFileMetadata upfm : recoveredData) {
+          uploadMetadataOutput.emit(upfm);
+          fileMetadataOutput.emit(upfm);
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void beforeCheckpoint(long windowId)
+  {
+
+  }
+
+  @Override
+  public void checkpointed(long windowId)
+  {
+
+  }
+
+  @Override
+  public void committed(long windowId)
+  {
+    try {
+      windowDataManager.committed(windowId);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Return the name of the bucket in which to create the multipart upload.
+   * @return bucket name
+   */
+  public String getBucketName()
+  {
+    return bucketName;
+  }
+
+  /**
+   * Set the name of the bucket in which to create the multipart upload.
+   * @param bucketName bucket name
+   */
+  public void setBucketName(@NotNull String bucketName)
+  {
+    this.bucketName = Preconditions.checkNotNull(bucketName);
+  }
+
+  /**
+   * Return the AWS access key
+   * @return AWS access key
+   */
+  public String getAccessKey()
+  {
+    return accessKey;
+  }
+
+  /**
+   * Sets the AWS access key
+   * @param accessKey given access key
+   */
+  public void setAccessKey(@NotNull String accessKey)
+  {
+    this.accessKey = Preconditions.checkNotNull(accessKey);
+  }
+
+  /**
+   * Return the AWS secret access key
+   * @return the AWS secret access key
+   */
+  public String getSecretAccessKey()
+  {
+    return secretAccessKey;
+  }
+
+  /**
+   * Sets the AWS secret access key
+   * @param secretAccessKey secret access key
+   */
+  public void setSecretAccessKey(@NotNull String secretAccessKey)
+  {
+    this.secretAccessKey = Preconditions.checkNotNull(secretAccessKey);
+  }
+
+  /**
+   * Output directory path for the files to upload
+   * @return the output directory path
+   */
+  public String getOutputDirectoryPath()
+  {
+    return outputDirectoryPath;
+  }
+
+  /**
+   * Sets the output directory path for uploading new files.
+   * @param outputDirectoryPath output directory path
+   */
+  public void setOutputDirectoryPath(@NotNull String outputDirectoryPath)
+  {
+    this.outputDirectoryPath = Preconditions.checkNotNull(outputDirectoryPath);
+  }
+
+  /**
+   * Returns the window data manager.
+   * @return the windowDataManager
+   */
+  public WindowDataManager getWindowDataManager()
+  {
+    return windowDataManager;
+  }
+
+  /**
+   * Sets the window data manager
+   * @param windowDataManager given windowDataManager
+   */
+  public void setWindowDataManager(@NotNull WindowDataManager windowDataManager)
+  {
+    this.windowDataManager = Preconditions.checkNotNull(windowDataManager);
+  }
+
+  /**
+   * Returns the AWS S3 end point
+   * @return the S3 end point
+   */
+  public String getEndPoint()
+  {
+    return endPoint;
+  }
+
+  /**
+   * Sets the AWS S3 end point
+   * @param endPoint S3 end point
+   */
+  public void setEndPoint(String endPoint)
+  {
+    this.endPoint = endPoint;
+  }
+
+  /**
+   * A file upload metadata which contains file metadata, upload id, key name.
+   */
+  public static class UploadFileMetadata
+  {
+    private AbstractFileSplitter.FileMetadata fileMetadata;
+    private String uploadId;
+    private String keyName;
+
+    // For Kryo
+    public UploadFileMetadata()
+    {
+    }
+
+    public UploadFileMetadata(AbstractFileSplitter.FileMetadata fileMetadata, String uploadId, String keyName)
+    {
+      this.fileMetadata = fileMetadata;
+      this.uploadId = uploadId;
+      this.keyName = keyName;
+    }
+
+    @Override
+    public int hashCode()
+    {
+      return keyName.hashCode();
+    }
+
+    /**
+     * Returns the name of the key generated from file path.
+     * @return the key name
+     */
+    public String getKeyName()
+    {
+      return keyName;
+    }
+
+    /**
+     * Return the file metadata of a file.
+     * @return the fileMetadata
+     */
+    public AbstractFileSplitter.FileMetadata getFileMetadata()
+    {
+      return fileMetadata;
+    }
+
+    /**
+     * Returns the unique upload id of a file.
+     * @return the upload Id of a file
+     */
+    public String getUploadId()
+    {
+      return uploadId;
+    }
+  }
+}
---
+++ library/src/test/java/org/apache/apex/malhar/lib/fs/s3/S3OutputModuleMockTest.java
@@ -0,0 +1,171 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.apex.malhar.lib.fs.s3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadResult;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.UploadPartRequest;
+import com.amazonaws.services.s3.model.UploadPartResult;
+
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.LocalMode;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.lib.helper.OperatorContextTestHelper;
+import com.datatorrent.lib.io.fs.FSInputModule;
+import com.datatorrent.stram.StramLocalCluster;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+/**
+ * Verifies the S3OutputModule using the application. This reads the data from local file system
+ * "input" directory and uploads the files into "output" directory.
+ */
+public class S3OutputModuleMockTest
+{
+  private String uploadId = "uploadfile";
+  private static final String APPLICATION_PATH_PREFIX = "target/s3outputmocktest/";
+  private static final String FILE_DATA = "Testing the S3OutputModule. This File has more data hence more blocks.";
+  private static final String FILE = "file.txt";
+  private String inputDir;
+  private String outputDir;
+  private String applicationPath;
+  private File inputFile;
+  @Mock
+  public static AmazonS3 client;
+
+  @Before
+  public void beforeTest() throws IOException
+  {
+    applicationPath = OperatorContextTestHelper.getUniqueApplicationPath(APPLICATION_PATH_PREFIX);
+    inputDir = applicationPath + File.separator + "input";
+    outputDir = applicationPath + File.separator + "output";
+    inputFile = new File(inputDir + File.separator + FILE);
+    FileUtils.writeStringToFile(inputFile, FILE_DATA);
+  }
+
+  @After
+  public void afterTest()
+  {
+    Path root = new Path(applicationPath);
+    try {
+      FileSystem fs = FileSystem.newInstance(root.toUri(), new Configuration());
+      fs.delete(root, true);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private CompleteMultipartUploadResult completeMultiPart() throws IOException
+  {
+    FileUtils.copyFile(inputFile, new File(outputDir + File.separator + FILE));
+    CompleteMultipartUploadResult result = new CompleteMultipartUploadResult();
+    result.setETag(outputDir);
+    return result;
+  }
+
+  @Test
+  public void testS3OutputModule() throws Exception
+  {
+    InitiateMultipartUploadResult result = new InitiateMultipartUploadResult();
+    result.setUploadId(uploadId);
+
+    PutObjectResult objResult = new PutObjectResult();
+    objResult.setETag("SuccessFullyUploaded");
+
+    UploadPartResult partResult = new UploadPartResult();
+    partResult.setPartNumber(1);
+    partResult.setETag("SuccessFullyPartUploaded");
+
+    MockitoAnnotations.initMocks(this);
+    when(client.initiateMultipartUpload(any(InitiateMultipartUploadRequest.class))).thenReturn(result);
+    when(client.putObject(any(PutObjectRequest.class))).thenReturn(objResult);
+    when(client.uploadPart(any(UploadPartRequest.class))).thenReturn(partResult);
+    when(client.completeMultipartUpload(any(CompleteMultipartUploadRequest.class))).thenReturn(completeMultiPart());
+
+    Application app = new S3OutputModuleMockTest.Application();
+    Configuration conf = new Configuration();
+    conf.set("dt.operator.HDFSInputModule.prop.files", inputDir);
+    conf.set("dt.operator.HDFSInputModule.prop.blockSize", "10");
+    conf.set("dt.operator.HDFSInputModule.prop.blocksThreshold", "1");
+    conf.set("dt.attr.CHECKPOINT_WINDOW_COUNT","20");
+
+    conf.set("dt.operator.S3OutputModule.prop.accessKey", "accessKey");
+    conf.set("dt.operator.S3OutputModule.prop.secretAccessKey", "secretKey");
+    conf.set("dt.operator.S3OutputModule.prop.bucketName", "bucketKey");
+    conf.set("dt.operator.S3OutputModule.prop.outputDirectoryPath", outputDir);
+
+    Path outDir = new Path("file://" + new File(outputDir).getAbsolutePath());
+    final Path outputFilePath =  new Path(outDir.toString() + File.separator + FILE);
+    final FileSystem fs = FileSystem.newInstance(outDir.toUri(), new Configuration());
+    LocalMode lma = LocalMode.newInstance();
+    lma.prepareDAG(app, conf);
+    LocalMode.Controller lc = lma.getController();
+    lc.setHeartbeatMonitoringEnabled(true);
+
+    ((StramLocalCluster)lc).setExitCondition(new Callable<Boolean>()
+    {
+      @Override
+      public Boolean call() throws Exception
+      {
+        return fs.exists(outputFilePath);
+      }
+    });
+    lc.run(10000);
+
+    Assert.assertTrue("output file exist", fs.exists(outputFilePath));
+  }
+
+  private static class Application implements StreamingApplication
+  {
+    @Override
+    public void populateDAG(DAG dag, Configuration conf)
+    {
+      FSInputModule inputModule = dag.addModule("HDFSInputModule", new FSInputModule());
+      S3OutputTestModule outputModule = dag.addModule("S3OutputModule", new S3OutputTestModule());
+
+      dag.addStream("FileMetaData", inputModule.filesMetadataOutput, outputModule.filesMetadataInput);
+      dag.addStream("BlocksMetaData", inputModule.blocksMetadataOutput, outputModule.blocksMetadataInput)
+        .setLocality(DAG.Locality.CONTAINER_LOCAL);
+      dag.addStream("BlocksData", inputModule.messages, outputModule.blockData).setLocality(DAG.Locality.CONTAINER_LOCAL);
+
+    }
+  }
+}
