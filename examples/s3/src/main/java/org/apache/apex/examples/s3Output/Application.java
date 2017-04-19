c830f5e4f5390e93202b3b99f5a0398cb44592b0
Apex Dev <dev@apex.apache.org>
2017-04-19 14:07:32 -0700

    APEXMALHAR-2480 Fixed checkstyle and Log4j Properties.

--- examples/s3/src/main/java/org/apache/apex/examples/s3Output/Application.java
+++ examples/s3/src/main/java/org/apache/apex/examples/s3Output/Application.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.apex.examples.s3Output;
 
 import org.apache.apex.malhar.lib.fs.s3.S3OutputModule;
@@ -11,7 +29,7 @@ import com.datatorrent.lib.io.fs.FSInputModule;
 /**
  * Application illustrating copy files from HDFS to S3 bucket.
  */
-@ApplicationAnnotation(name="HDFSToS3App")
+@ApplicationAnnotation(name = "HDFSToS3App")
 public class Application implements StreamingApplication
 {
   @Override
--- examples/s3/src/main/java/org/apache/apex/examples/s3TupleOutput/Application.java
+++ examples/s3/src/main/java/org/apache/apex/examples/s3TupleOutput/Application.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.apex.examples.s3TupleOutput;
 
 import org.apache.apex.malhar.lib.fs.FSRecordReaderModule;
@@ -11,7 +29,7 @@ import com.datatorrent.api.annotation.ApplicationAnnotation;
 /**
  * Simple application illustrating file copy from S3
  */
-@ApplicationAnnotation(name="s3-output-line")
+@ApplicationAnnotation(name = "s3-output-line")
 public class Application implements StreamingApplication
 {
 
@@ -20,7 +38,7 @@ public class Application implements StreamingApplication
     FSRecordReaderModule recordReader = dag.addModule("lineInput", FSRecordReaderModule.class);
     S3BytesOutputModule s3StringOutputModule = dag.addModule("s3TupleOutput", S3BytesOutputModule.class);
     dag.addStream("data", recordReader.records, s3StringOutputModule.input);
-    
+
   }
 
 }
