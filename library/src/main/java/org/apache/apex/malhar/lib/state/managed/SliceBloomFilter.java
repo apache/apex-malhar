07869c82986dd721346aeb6c7e552175db27141d
Pramod Immaneni <pramod@datatorrent.com>
2017-06-01 14:02:26 -0700

    APEXMALHAR-2366 Added license references, this closes #521

--- library/src/main/java/org/apache/apex/malhar/lib/state/managed/SliceBloomFilter.java
+++ library/src/main/java/org/apache/apex/malhar/lib/state/managed/SliceBloomFilter.java
@@ -16,6 +16,49 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+/**
+ *
+ * bloomfilter - Bloom filters for Java
+ * Copyright (c) 2014-2015, Sandeep Gupta
+ *
+ * http://sangupta.com/projects/bloomfilter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ *
+ * murmurhash - Pure Java implementation of the Murmur Hash algorithms.
+ * Copyright (c) 2014, Sandeep Gupta
+ *
+ * http://sangupta.com/projects/murmur
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.apex.malhar.lib.state.managed;
 
 import java.util.BitSet;
@@ -24,6 +67,9 @@ import com.datatorrent.netlet.util.Slice;
 
 /**
  * This class implemented BloomFilter algorithm, the key is Slice
+ * Bloom filter and murmur hash implementations originally from Sandeep Gupta
+ * <a href="https://github.com/sangupta/bloomfilter/blob/master/src/main/java/com/sangupta/bloomfilter/BloomFilter.java">BloomFilter.java</a>
+ * <a href="https://github.com/sangupta/murmur/blob/master/src/main/java/com/sangupta/murmur/Murmur3.java">Murmur3.java</a>
  *
  */
 public class SliceBloomFilter
