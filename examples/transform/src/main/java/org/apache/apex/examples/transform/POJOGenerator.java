30bfac5a83b54b5909640db41c1e1db9844672ac
Ananth <ananthg.apex@gmail.com>
2017-10-07 06:04:57 +1100

    APEXMALHAR-2532-TransformApplication-AlterConsoleOutputToUseFunctionOperator

--- examples/transform/src/main/java/org/apache/apex/examples/transform/POJOGenerator.java
+++ examples/transform/src/main/java/org/apache/apex/examples/transform/POJOGenerator.java
@@ -19,6 +19,7 @@
 
 package org.apache.apex.examples.transform;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
@@ -37,16 +38,26 @@ import com.datatorrent.api.InputOperator;
  */
 public class POJOGenerator implements InputOperator
 {
+  public static final int MAX_YEAR_DIFF_RANGE = 30;
+
   @Min(1)
   private int maxCustomerId = 100000;
   @Min(1)
   private int maxNameLength = 10;
   @Min(1)
   private int maxAddressLength = 15;
-  private long tuplesCounter;
+  private long tuplesCounter = 0;
+  private long currentWindowTuplesCounter = 0;
+
   // Limit number of emitted tuples per window
   @Min(1)
   private long maxTuplesPerWindow = 100;
+
+  @Min(1)
+  private long maxTuples = 500;
+
+  private Date currentDate;
+
   private final Random random = new Random();
   private final RandomStringUtils rRandom = new RandomStringUtils();
   public final transient DefaultOutputPort<Object> output = new DefaultOutputPort<>();
@@ -54,7 +65,7 @@ public class POJOGenerator implements InputOperator
   @Override
   public void beginWindow(long windowId)
   {
-    tuplesCounter = 0;
+    currentWindowTuplesCounter = 0;
   }
 
   @Override
@@ -65,6 +76,7 @@ public class POJOGenerator implements InputOperator
   @Override
   public void setup(Context.OperatorContext context)
   {
+    currentDate = new Date();
   }
 
   @Override
@@ -79,9 +91,10 @@ public class POJOGenerator implements InputOperator
     customerEvent.setCustomerId(randomId(maxCustomerId));
     customerEvent.setFirstName(rRandom.randomAlphabetic(randomId(maxNameLength)));
     customerEvent.setLastName(rRandom.randomAlphabetic(randomId(maxNameLength)));
-    long val = random.nextLong();
-    long diff1 = val % System.currentTimeMillis();
-    customerEvent.setDateOfBirth(new Date(diff1));
+    Calendar calendar = Calendar.getInstance();
+    // Subtract a random year within a given range to set Date of birth. Used in asserts later
+    calendar.add(Calendar.YEAR, (-1 * random.nextInt(MAX_YEAR_DIFF_RANGE)));
+    customerEvent.setDateOfBirth(calendar.getTime());
     customerEvent.setAddress(rRandom.randomAlphabetic(randomId(maxAddressLength)));
     return customerEvent;
   }
@@ -97,24 +110,26 @@ public class POJOGenerator implements InputOperator
   @Override
   public void emitTuples()
   {
-    while (tuplesCounter++ < maxTuplesPerWindow) {
+    while ( ( currentWindowTuplesCounter < maxTuplesPerWindow) && (tuplesCounter < maxTuples) ) {
       try {
         CustomerEvent event = generateCustomersEvent();
         this.output.emit(event);
+        tuplesCounter++;
+        currentWindowTuplesCounter++;
       } catch (Exception ex) {
         throw new RuntimeException(ex);
       }
     }
   }
 
-  public long getMaxTuplesPerWindow()
+  public long getMaxTuples()
   {
-    return maxTuplesPerWindow;
+    return maxTuples;
   }
 
-  public void setMaxTuplesPerWindow(long maxTuplesPerWindow)
+  public void setMaxTuples(long maxTuples)
   {
-    this.maxTuplesPerWindow = maxTuplesPerWindow;
+    this.maxTuples = maxTuples;
   }
 
   public int getMaxAddressLength()
@@ -145,5 +160,15 @@ public class POJOGenerator implements InputOperator
   public void setMaxCustomerId(int maxCustomerId)
   {
     this.maxCustomerId = maxCustomerId;
+  }
+
+  public long getMaxTuplesPerWindow()
+  {
+    return maxTuplesPerWindow;
+  }
+
+  public void setMaxTuplesPerWindow(long maxTuplesPerWindow)
+  {
+    this.maxTuplesPerWindow = maxTuplesPerWindow;
   }
 }
